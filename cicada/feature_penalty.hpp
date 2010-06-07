d0e8e48ec49b223445ac7fc63b2deeac59e3cb23
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-06-07 17:44:38 +0900

    new file:   cicada/feature_penalty.hpp 		added missed file

---
+++ cicada/feature_penalty.hpp
@@ -0,0 +1,70 @@
+// -*- mode: c++ -*-
+
+#ifndef __CICADA__FEATURE__PENALTY__HPP__
+#define __CICADA__FEATURE__PENALTY__HPP__ 1
+
+namespace cicada
+{
+
+  namespace feature
+  {
+    class TargetWordPenalty : public FeatureFunction
+    {
+    public:
+      TargetWordPenalty() : FeatureFunction(0, "target-word-penalty") { }
+      
+      void operator()(state_ptr_type& state,
+		      const state_ptr_set_type& states,
+		      const edge_type& edge,
+		      feature_set_type& features,
+		      feature_set_type& estimates) const
+      {
+	int count = 0;
+	rule_type::symbol_set_type::const_iterator titer_end = edge.rule->target.end();
+	for (rule_type::symbol_set_type::const_iterator titer = edge.rule->target.begin(); titer != titer_end; ++ titer)
+	  count -= (titer->is_terminal() && *titer != vocab_type::EPSILON);
+	
+	features[feature_name()] = count;
+      }
+    };
+
+    class SourceWordPenalty : public FeatureFunction
+    {
+    public:
+      SourceWordPenalty() : FeatureFunction(0, "source-word-penalty") { }
+      
+      void operator()(state_ptr_type& state,
+		      const state_ptr_set_type& states,
+		      const edge_type& edge,
+		      feature_set_type& features,
+		      feature_set_type& estimates) const
+      {
+	int count = 0;
+	rule_type::symbol_set_type::const_iterator titer_end = edge.rule->source.end();
+	for (rule_type::symbol_set_type::const_iterator titer = edge.rule->source.begin(); titer != titer_end; ++ titer)
+	  count -= (titer->is_terminal() && *titer != vocab_type::EPSILON);
+	
+	features[feature_name()] = count;
+      }
+    };
+    
+    class RulePenalty : public FeatureFunction
+    {
+    public:
+      RulePenalty() : FeatureFunction(0, "rule-penalty") { }
+      
+      void operator()(state_ptr_type& state,
+		      const state_ptr_set_type& states,
+		      const edge_type& edge,
+		      feature_set_type& features,
+		      feature_set_type& estimates) const
+      {
+	features[feature_name()] = -1;
+      }
+      
+    };
+    
+  };
+};
+
+#endif
