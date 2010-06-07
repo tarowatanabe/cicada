48a4f1a138cae194fcc42660ecd680d35ea7ea28
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-06-07 17:42:32 +0900

    new file:   cicada/eval/score.cpp 	new file:   cicada/feature_function.cpp 		added missed files

---
+++ cicada/eval/score.cpp
@@ -0,0 +1,36 @@
+
+#include "eval/score.hpp"
+#include "eval/bleu.hpp"
+
+#include "parameter.hpp"
+
+namespace cicada
+{
+  namespace eval
+  {
+
+    std::string Scorer::lists()
+    {
+      return "bleu,order=<order, default=4>\n";
+    }
+    
+    Scorer::scorer_ptr_type Scorer::create(const std::string& parameter)
+    {
+      typedef cicada::Parameter parameter_type;
+
+      const parameter_type param(parameter);
+      
+      if (param.name() == "bleu") {
+	int order = 4;
+	parameter_type::const_iterator iter = param.find("order");
+	if (iter != param.end())
+	  order = boost::lexical_cast<int>(iter->second);
+	
+	return scorer_ptr_type(new BleuScorer(order));
+      } else
+	throw std::runtime_error("unknown scorer" + param.name());
+      
+    }
+    
+  };
+};
---
+++ cicada/feature_function.cpp
@@ -0,0 +1,42 @@
+
+#include "feature_function.hpp"
+#include "parameter.hpp"
+
+#include "feature_ngram.hpp"
+#include "feature_penalty.hpp"
+
+namespace cicada
+{
+  
+  std::string FeatureFunction::lists()
+  {
+    static const char* desc = "\
+ngram,file=<file>,order=<order>,name=feature-name(default: ngram)\n\
+target-word-penalty\n\
+soruce-word-penalty\n\
+rule-penalty\n\
+";
+      return desc;
+    
+  }
+
+  FeatureFunction::feature_function_ptr_type FeatureFunction::create(const std::string& parameter)
+  {
+    typedef cicada::Parameter parameter_type;
+    
+    const parameter_type param(parameter);
+    
+    if (param.name() == "ngram")
+      return feature_function_ptr_type(new feature::NGram(parameter));
+    else if (param.name() == "target-word-penalty")
+      return feature_function_ptr_type(new feature::TargetWordPenalty());
+    else if (param.name() == "source-word-penalty")
+      return feature_function_ptr_type(new feature::SourceWordPenalty());
+    else if (param.name() == "rule-penalty")
+      return feature_function_ptr_type(new feature::RulePenalty());
+    else
+      throw std::runtime_error("unknown featuer: " + parameter);
+    
+  }
+  
+};
