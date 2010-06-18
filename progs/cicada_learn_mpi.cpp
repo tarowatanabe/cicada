8b3cb10dd25c44e7da7fd3f99c0f74a0bda31c74
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-06-18 17:08:41 +0900

    modified:   progs/cicada_learn_mpi.cpp 		abort

--- progs/cicada_learn_mpi.cpp
+++ progs/cicada_learn_mpi.cpp
@@ -156,7 +156,7 @@ int main(int argc, char ** argv)
   }
   catch (const std::exception& err) {
     std::cerr << "error: " << err.what() << std::endl;
-    MPI::Abort(1);
+    MPI::COMM_WORLD.Abort(1);
     return 1;
   }
   return 0;
