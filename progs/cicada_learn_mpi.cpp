d5ec7c02399fb22a3f74d48e0b3ac3d506d921ce
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-06-18 14:20:39 +0900

    modified:   progs/cicada_learn_mpi.cpp 		added an option to load training data on-memory

--- progs/cicada_learn_mpi.cpp
+++ progs/cicada_learn_mpi.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <deque>
 
 #include "cicada_impl.hpp"
 
@@ -52,6 +53,8 @@ typedef rule_type::feature_set_type    feature_set_type;
 typedef feature_set_type::feature_type feature_type;
 typedef cicada::WeightVector<double>   weight_set_type;
 
+typedef std::deque<hypergraph_type, std::allocator<hypergraph_type> > hypergraph_set_type;
+
 
 path_type forest_path;
 path_type intersected_path;
@@ -63,14 +66,21 @@ bool regularize_l1 = false;
 bool regularize_l2 = false;
 double C = 1.0;
 
+bool load = false;
+
 int debug = 0;
 
 
 void options(int argc, char** argv);
 double optimize(const path_type& forest_path,
 		const path_type& intersected_path,
+		const hypergraph_set_type& graphs_forest,
+		const hypergraph_set_type& graphs_intersected,
 		weight_set_type& weights);
-void enumerate_forest(const path_type& path);
+void enumerate_forest(const path_type& forest_path,
+		      const path_type& intersected_path,
+		      hypergraph_set_type& graphs_forest,
+		      hypergraph_set_type& graphs_intersected);
 
 
 void bcast_weights(const int rank, weight_set_type& weights);
@@ -122,15 +132,18 @@ int main(int argc, char ** argv)
 
     if (! boost::filesystem::exists(intersected_path) || ! boost::filesystem::is_directory(intersected_path))
       throw std::runtime_error("no intersected forest?");
+
+    hypergraph_set_type graphs_forest;
+    hypergraph_set_type graphs_intersected;
         
-    enumerate_forest(forest_path);
+    enumerate_forest(forest_path, intersected_path, graphs_forest, graphs_intersected);
 
     if (debug && mpi_rank == 0)
       std::cerr << "# of features: " << feature_type::allocated() << std::endl;
 
     weight_set_type weights;
     
-    const double objective = optimize(forest_path, intersected_path, weights);
+    const double objective = optimize(forest_path, intersected_path, graphs_forest, graphs_intersected, weights);
 
     if (debug && mpi_rank == 0)
       std::cerr << "objective: " << objective << std::endl;
@@ -153,9 +166,13 @@ struct OptimizeLBFGS
 
   OptimizeLBFGS(const path_type& __forest_path,
 		const path_type& __intersected_path,
+		const hypergraph_set_type& __graphs_forest,
+		const hypergraph_set_type& __graphs_intersected,
 		weight_set_type& __weights)
     : forest_path(__forest_path),
       intersected_path(__intersected_path),
+      graphs_forest(__graphs_forest),
+      graphs_intersected(__graphs_intersected),
       weights(__weights) {}
 
   double operator()()
@@ -191,10 +208,14 @@ struct OptimizeLBFGS
 
     Task(const weight_set_type& __weights,
 	 const path_type& __dir_forest,
-	 const path_type& __dir_intersected)
+	 const path_type& __dir_intersected,
+	 const hypergraph_set_type& __graphs_forest,
+	 const hypergraph_set_type& __graphs_intersected)
       : weights(__weights),
 	dir_forest(__dir_forest),
-	dir_intersected(__dir_intersected) {}
+	dir_intersected(__dir_intersected),
+	graphs_forest(__graphs_forest),
+	graphs_intersected(__graphs_intersected) {}
     
     struct gradients_type
     {
@@ -274,80 +295,27 @@ struct OptimizeLBFGS
       g.clear();
       objective = 0.0;
       
-      for (int i = mpi_rank;/**/; i += mpi_size) {
-	const std::string file_name = boost::lexical_cast<std::string>(i) + ".gz";
-	
-	const path_type path_forest      = dir_forest / file_name;
-	const path_type path_intersected = dir_intersected / file_name;
-	
-	if (! boost::filesystem::exists(path_forest)) break;
-	if (! boost::filesystem::exists(path_intersected)) continue;
-	
-	gradients.clear();
-	gradients_intersected.clear();
-	
-	inside.clear();
-	inside_intersected.clear();
+      if (! graphs_forest.empty()) {
+	for (int i = 0; i < graphs_forest.size(); ++ i) {
+	  const hypergraph_type& hypergraph_forest      = graphs_forest[i];
+	  const hypergraph_type& hypergraph_intersected = graphs_intersected[i];
 
-	bool valid_forest = true;
-	bool valid_intersected = true;
+	  if (! hypergraph_forest.is_valid() || ! hypergraph_intersected.is_valid()) continue;
+	  
+	  gradients.clear();
+	  gradients_intersected.clear();
+	  
+	  inside.clear();
+	  inside_intersected.clear();
 
-	{
-	  utils::compress_istream is(path_forest);
-	  std::getline(is, line);
-
-	  std::string::const_iterator iter = line.begin();
-	  std::string::const_iterator end = line.end();
+	  inside.reserve(hypergraph_forest.nodes.size());
+	  inside.resize(hypergraph_forest.nodes.size(), weight_type());
+	  inside_outside(hypergraph_forest, inside, gradients, weight_function(weights), feature_function(weights));
 	  
-	  if (! parse_id(id_forest, iter, end))
-	    throw std::runtime_error("invalid id input");
+	  inside_intersected.reserve(hypergraph_intersected.nodes.size());
+	  inside_intersected.resize(hypergraph_intersected.nodes.size(), weight_type());
+	  inside_outside(hypergraph_intersected, inside_intersected, gradients_intersected, weight_function(weights), feature_function(weights));
 	  
-	  if (! hypergraph.assign(iter, end))
-	    throw std::runtime_error("invalid graph format");
-	  
-	  if (iter != end)
-	    throw std::runtime_error("invalid id ||| graph format");
-	  
-	  valid_forest = hypergraph.is_valid();
-	}
-	
-	if (valid_forest) {
-	  inside.reserve(hypergraph.nodes.size());
-	  inside.resize(hypergraph.nodes.size(), weight_type());
-	  
-	  inside_outside(hypergraph, inside, gradients, weight_function(weights), feature_function(weights));
-	}
-	
-	{
-	  utils::compress_istream is(path_intersected);
-	  std::getline(is, line);
-
-	  std::string::const_iterator iter = line.begin();
-	  std::string::const_iterator end = line.end();
-	  
-	  if (! parse_id(id_intersected, iter, end))
-	    throw std::runtime_error("invalid id input");
-	  
-	  if (! hypergraph.assign(iter, end))
-	    throw std::runtime_error("invalid graph format");
-	  
-	  if (iter != end)
-	    throw std::runtime_error("invalid id ||| graph format");
-	  
-	  valid_intersected = hypergraph.is_valid();
-	}
-	
-	if (valid_intersected) {
-	  inside_intersected.reserve(hypergraph.nodes.size());
-	  inside_intersected.resize(hypergraph.nodes.size(), weight_type());
-	  
-	  inside_outside(hypergraph, inside_intersected, gradients_intersected, weight_function(weights), feature_function(weights));
-	}
-
-	if (id_forest != id_intersected)
-	  throw std::runtime_error("different segment id?");
-	  
-	if (valid_forest && valid_intersected) {
 	  gradient_type& gradient = gradients.gradient;
 	  weight_type& Z = inside.back();
 	  
@@ -365,7 +333,103 @@ struct OptimizeLBFGS
 	  objective -= margin;
 	  
 	  if (debug >= 3)
-	    std::cerr << "id: " << id_forest << " margin: " << margin << std::endl;
+	    std::cerr << "margin: " << margin << std::endl;
+	}
+	
+      } else {
+	for (int i = mpi_rank;/**/; i += mpi_size) {
+	  const std::string file_name = boost::lexical_cast<std::string>(i) + ".gz";
+	
+	  const path_type path_forest      = dir_forest / file_name;
+	  const path_type path_intersected = dir_intersected / file_name;
+	
+	  if (! boost::filesystem::exists(path_forest)) break;
+	  if (! boost::filesystem::exists(path_intersected)) continue;
+	
+	  gradients.clear();
+	  gradients_intersected.clear();
+	
+	  inside.clear();
+	  inside_intersected.clear();
+
+	  bool valid_forest = true;
+	  bool valid_intersected = true;
+
+	  {
+	    utils::compress_istream is(path_forest);
+	    std::getline(is, line);
+
+	    std::string::const_iterator iter = line.begin();
+	    std::string::const_iterator end = line.end();
+	  
+	    if (! parse_id(id_forest, iter, end))
+	      throw std::runtime_error("invalid id input");
+	  
+	    if (! hypergraph.assign(iter, end))
+	      throw std::runtime_error("invalid graph format");
+	  
+	    if (iter != end)
+	      throw std::runtime_error("invalid id ||| graph format");
+	  
+	    valid_forest = hypergraph.is_valid();
+	  }
+	
+	  if (valid_forest) {
+	    inside.reserve(hypergraph.nodes.size());
+	    inside.resize(hypergraph.nodes.size(), weight_type());
+	  
+	    inside_outside(hypergraph, inside, gradients, weight_function(weights), feature_function(weights));
+	  }
+	
+	  {
+	    utils::compress_istream is(path_intersected);
+	    std::getline(is, line);
+
+	    std::string::const_iterator iter = line.begin();
+	    std::string::const_iterator end = line.end();
+	  
+	    if (! parse_id(id_intersected, iter, end))
+	      throw std::runtime_error("invalid id input");
+	  
+	    if (! hypergraph.assign(iter, end))
+	      throw std::runtime_error("invalid graph format");
+	  
+	    if (iter != end)
+	      throw std::runtime_error("invalid id ||| graph format");
+	  
+	    valid_intersected = hypergraph.is_valid();
+	  }
+	
+	  if (valid_intersected) {
+	    inside_intersected.reserve(hypergraph.nodes.size());
+	    inside_intersected.resize(hypergraph.nodes.size(), weight_type());
+	  
+	    inside_outside(hypergraph, inside_intersected, gradients_intersected, weight_function(weights), feature_function(weights));
+	  }
+
+	  if (id_forest != id_intersected)
+	    throw std::runtime_error("different segment id?");
+	  
+	  if (valid_forest && valid_intersected) {
+	    gradient_type& gradient = gradients.gradient;
+	    weight_type& Z = inside.back();
+	  
+	    gradient_type& gradient_intersected = gradients_intersected.gradient;
+	    weight_type& Z_intersected = inside_intersected.back();
+	  
+	    gradient /= Z;
+	    gradient_intersected /= Z_intersected;
+	  
+	    feature_expectations -= gradient_intersected;
+	    feature_expectations += gradient;
+	  
+	    const double margin = log(Z_intersected) - log(Z);
+	  
+	    objective -= margin;
+	  
+	    if (debug >= 3)
+	      std::cerr << "id: " << id_forest << " margin: " << margin << std::endl;
+	  }
 	}
       }
       
@@ -378,6 +442,9 @@ struct OptimizeLBFGS
     const weight_set_type& weights;
     const path_type        dir_forest;
     const path_type        dir_intersected;
+
+    const hypergraph_set_type& graphs_forest;
+    const hypergraph_set_type& graphs_intersected;
     
     double          objective;
     weight_set_type g;
@@ -403,7 +470,7 @@ struct OptimizeLBFGS
         
     bcast_weights(0, optimizer.weights);
     
-    task_type task(optimizer.weights, optimizer.forest_path, optimizer.intersected_path);
+    task_type task(optimizer.weights, optimizer.forest_path, optimizer.intersected_path, optimizer.graphs_forest, optimizer.graphs_intersected);
     task();
     
     // collect all the objective and gradients...
@@ -434,18 +501,24 @@ struct OptimizeLBFGS
   
   path_type forest_path;
   path_type intersected_path;
+  
+  const hypergraph_set_type& graphs_forest;
+  const hypergraph_set_type& graphs_intersected;
+  
   weight_set_type& weights;
 };
 
 double optimize(const path_type& forest_path,
 		const path_type& intersected_path,
+		const hypergraph_set_type& graphs_forest,
+		const hypergraph_set_type& graphs_intersected,
 		weight_set_type& weights)
 {
   const int mpi_rank = MPI::COMM_WORLD.Get_rank();
   const int mpi_size = MPI::COMM_WORLD.Get_size();
   
   if (mpi_rank == 0) {
-    const double objective = OptimizeLBFGS(forest_path, intersected_path, weights)();
+    const double objective = OptimizeLBFGS(forest_path, intersected_path, graphs_forest, graphs_intersected, weights)();
     
     // send termination!
     for (int rank = 1; rank < mpi_size; ++ rank)
@@ -477,7 +550,7 @@ double optimize(const path_type& forest_path,
 
 	bcast_weights(0, weights);
 	
-	task_type task(weights, forest_path, intersected_path);
+	task_type task(weights, forest_path, intersected_path, graphs_forest, graphs_intersected);
 	task();
 	
 	send_weights(task.g);
@@ -496,35 +569,80 @@ double optimize(const path_type& forest_path,
 
 // I know, it is very stupid...
 
-void enumerate_forest(const path_type& dir)
+void enumerate_forest(const path_type& forest_path,
+		      const path_type& intersected_path,
+		      hypergraph_set_type& graphs_forest,
+		      hypergraph_set_type& graphs_intersected)
 {
   const int mpi_rank = MPI::COMM_WORLD.Get_rank();
   const int mpi_size = MPI::COMM_WORLD.Get_size();
   
-  size_t          id;
-  hypergraph_type graph;
+  size_t id_forest;
+  size_t id_intersected;
   
   std::string line;
   
   for (int i = mpi_rank; /**/; i += mpi_size) {
-    path_type path = dir / (boost::lexical_cast<std::string>(i) + ".gz");
-    if (! boost::filesystem::exists(path)) break;
+    const std::string file_name = boost::lexical_cast<std::string>(i) + ".gz";
     
-    utils::compress_istream is(path);
-    std::getline(is, line);
+    const path_type path_forest      = forest_path / file_name;
+    const path_type path_intersected = intersected_path / file_name;
     
-    std::string::const_iterator iter = line.begin();
-    std::string::const_iterator end = line.end();
+    if (! boost::filesystem::exists(path_forest)) break;
+    if (! boost::filesystem::exists(path_intersected)) continue;
     
-    if (! parse_id(id, iter, end))
-      throw std::runtime_error("invalid id input");
+    {
+      utils::compress_istream is(path_forest);
+      std::getline(is, line);
+      
+      std::string::const_iterator iter = line.begin();
+      std::string::const_iterator end = line.end();
+      
+      if (! parse_id(id_forest, iter, end))
+	throw std::runtime_error("invalid id input");
+      
+      graphs_forest.push_back(hypergraph_type());
+      
+      if (! graphs_forest.back().assign(iter, end))
+	throw std::runtime_error("invalid graph format");
+      
+      if (! load)
+	graphs_forest.clear();
+      
+      if (iter != end)
+	throw std::runtime_error("invalid id ||| graph format");
+    }
+
+    {
+      utils::compress_istream is(path_intersected);
+      std::getline(is, line);
+      
+      std::string::const_iterator iter = line.begin();
+      std::string::const_iterator end = line.end();
+      
+      if (! parse_id(id_intersected, iter, end))
+	throw std::runtime_error("invalid id input");
+      
+      graphs_intersected.push_back(hypergraph_type());
+      
+      if (! graphs_intersected.back().assign(iter, end))
+	throw std::runtime_error("invalid graph format");
+      
+      if (! load)
+	graphs_intersected.clear();
+
+      
+      if (iter != end)
+	throw std::runtime_error("invalid id ||| graph format");
+    }
     
-    if (! graph.assign(iter, end))
-      throw std::runtime_error("invalid graph format");
-    
-    if (iter != end)
-      throw std::runtime_error("invalid id ||| graph format");
+    if (id_forest != id_intersected)
+      throw std::runtime_error("id do not match");
   }
+  
+  if (graphs_intersected.size() != graphs_forest.size())
+    throw std::runtime_error("# of hypergraphs do not match");
+
   
   // collect features...
   for (int rank = 0; rank < mpi_size; ++ rank) {
@@ -670,6 +788,8 @@ void options(int argc, char** argv)
     ("regularize-l1", po::bool_switch(&regularize_l1), "L1-regularization")
     ("regularize-l2", po::bool_switch(&regularize_l2), "L2-regularization")
     ("C"            , po::value<double>(&C),           "regularization constant")
+
+    ("load", po::bool_switch(&load), "load training data on-memory")
     
     ("debug", po::value<int>(&debug)->implicit_value(1), "debug level")
     ("help", "help message");
