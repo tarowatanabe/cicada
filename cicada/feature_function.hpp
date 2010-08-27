49a15217dc6ccd0daf8f7fef90772b726867fa43
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-08-27 17:40:12 +0900

    modified:   README 	modified:   README.features 	modified:   cicada/feature_function.hpp 		comments

--- cicada/feature_function.hpp
+++ cicada/feature_function.hpp
@@ -60,12 +60,24 @@ namespace cicada
     static std::string               lists();
     
   public:
+    // feature application 
+    // state:  is the result of application (concequence)
+    // states: are the states from antecedents
+    // edge:   is the current edge (tails will be the tails of new graph, head is the head of old graph)
+    //         acces only (*edge.rule) if you are not sure...
+    // features: accumulated features.
+    //           You should assign by features[feature-name] = score (see feature/ngram.cc), not like features[feature-name] += score
+    //           since "apply" can be applied many times for the same edge...
+    // estiamtes: upper bound estiamtes
+    // final:     final flag indicating that the edge's head is goal
     virtual void apply(state_ptr_type& state,
 		       const state_ptr_set_type& states,
 		       const edge_type& edge,
 		       feature_set_type& features,
 		       feature_set_type& estimates,
 		       const bool final) const = 0;
+    
+    // similar to apply, but used for coarse-heuristic functions used in cube-growing
     virtual void apply_coarse(state_ptr_type& state,
 			      const state_ptr_set_type& states,
 			      const edge_type& edge,
@@ -73,27 +85,45 @@ namespace cicada
 			      feature_set_type& estimates,
 			      const bool final) const = 0;
         
+    // cloning.. You should be careful for copying objects so that different threads 
+    // should not access at the same object. If not sure, "always" gives deep copy.
     virtual feature_function_ptr_type clone() const = 0;
     
+    // initialization will be called before and after feature application
+    // so that you can clean-up internal data
     virtual void initialize() {}
     
-    virtual void assign(const hypergraph_type& hypergraph) {}
-    virtual void assign(const lattice_type& lattice) {}
-    virtual void assign(const span_set_type& spans) {}
+    // You can collect additional data, if necessary.
+    // This will be called before feature application.
+    virtual void assign(const hypergraph_type& hypergraph) {} // hypergraph before feature application
+    virtual void assign(const lattice_type& lattice) {}       // input-lattice (may be empty)
+    virtual void assign(const span_set_type& spans) {}        // input-spans (may be empty)
     
     size_type state_size() const { return __state_size; }
     const feature_type& feature_name() const { return __feature_name; }
     bool sparse_feature() const { return __sparse_feature; }
     
+    // set by feature application algorithms...
+    // This will control the # of binary features...
     bool& apply_feature() { return __apply_feature; }
     bool  apply_feature() const { return __apply_feature; }
     
   protected:
-    size_type    __state_size;
-    feature_type __feature_name;
+    //
+    // You have to set up following parameters:
+    //
     
-    bool         __sparse_feature;
-    bool         __apply_feature;
+    // the state size used by the feature function. Zero implies state-less. Must be fixed value
+    size_type    __state_size;   
+    
+    // name of the feature. It is used to distinguish feature-functions
+    feature_type __feature_name; 
+    
+    // whether this is a sparse "binary" feature or not
+    bool         __sparse_feature; 
+    
+    // This value will be set by feature application algorithms via apply_feature()
+    bool         __apply_feature;  
   };
   
 };
