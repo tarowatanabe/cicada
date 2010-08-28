658809eb83b6f74a25e1ab350802672e0dc02fec
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-08-28 16:44:04 +0900

    fixed typo

--- cicada/stemmer/prefix.hpp
+++ cicada/stemmer/prefix.hpp
@@ -17,7 +17,7 @@ namespace cicada
       typedef std::vector<symbol_type, std::allocator<symbol_type> > symbol_set_type;
     
     public:
-      Prefix(const size_type __size) size(__size) {}
+      Prefix(const size_type __size) : size(__size) {}
     
     public:
       symbol_type operator[](const symbol_type& x) const;
