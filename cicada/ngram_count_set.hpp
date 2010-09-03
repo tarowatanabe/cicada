509098c5141cc413b427897fe4b48aa8338879cd
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-09-04 08:45:25 +0900

    added ngram-count-set

---
+++ cicada/ngram_count_set.hpp
@@ -0,0 +1,68 @@
+// -*- mode: c++ -*-
+
+#ifndef __CICADA__NGRAM_COUNT_SET__HPP__
+#define __CICADA__NGRAM_COUNT_SET__HPP__ 1
+
+#include <cicada/symbol.hpp>
+#include <cicada/symbol_vector.hpp>
+
+#include <utils/sgi_hash_map.hpp>
+
+namespace cicada
+{
+  
+  class NGramCountSet
+  {
+  public:
+    typedef Symbol       word_type;
+    typedef SymbolVector ngram_type;
+    typedef double       count_type;
+
+  private:
+#ifdef HAVE_TR1_UNORDERED_MAP
+    typedef std::tr1::unordered_map<ngram_type, count_type, boost::hash<ngram_type>, std::equal_to<ngram_type>,
+				    std::allocator<std::pair<ngram_type, count_type> > > ngram_set_type;
+#else
+    typedef sgi::hash_map<ngram_type, count_type, boost::hash<ngram_type>, std::equal_to<ngram_type>,
+			  std::allocator<std::pair<ngram_type, count_type> > > ngram_set_type;
+#endif
+    
+public:
+    typedef ngram_set_type::size_type        size_type;
+    typedef ngram_set_type::difference_type  difference_type;
+    
+    typedef ngram_set_type::value_type  value_type;
+    typedef ngram_set_type::key_type    key_type;
+    typedef ngram_set_type::data_type   data_type;
+    typedef ngram_set_type::mapped_type mapped_type;
+
+    typedef ngram_set_type::const_iterator  const_iterator;
+    typedef ngram_set_type::iterator        iterator;
+
+  public:
+    NGramCountSet() : ngrams() {}
+    NGramCountSet(size_type hint) : ngrams(hint) {}
+
+  public:
+    size_type size() const { return ngrams.size(); }
+    bool empty() const { return ngrams.empty(); }
+    
+  public:
+    count_type& operator[](const ngram_type& ngram) { return ngrams[ngram]; }
+    
+    void clear() { ngrams.clear(); }
+    
+  public:
+    const_iterator begin() const { return ngrams.begin(); }
+    iterator begin() { return ngrams.begin(); }
+
+    const_iterator end() const { return ngrams.end(); }
+    iterator end() { return ngrams.end(); }
+    
+private:
+  ngram_set_type ngrams;
+  };
+  
+};
+
+#endif
