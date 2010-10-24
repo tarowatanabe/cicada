f50b2d0642d985ec7f97959571ac3b6f1a1ba776
Taro Watanabe <taro@monet.local>
2010-10-24 10:16:47 +0900

    added f-measure based score added string-kernel scorer (but incomplete)

---
+++ cicada/eval/f.hpp
@@ -0,0 +1,101 @@
+// -*- mode: c++ -*-
+
+#ifndef __CICADA__EVAL__F__HPP__
+#define __CICADA__EVAL__F__HPP__ 1
+
+#include <cicada/eval/score.hpp>
+
+namespace cicaa
+{
+  namespace eval
+  {
+    class F : public Score
+    {
+    public:
+      typedef double count_type;
+      
+    public:
+      F() : match_ref(0), match_hyp(0), norm_ref(0), norm_hyp(0) {}
+      
+    public:
+      std::pair<double, double> score() const
+      {
+	const double precision = (norm_hyp != 0.0 ? match_hyp / norm_hyp : 0.0);
+	const double recall    = (norm_ref != 0.0 ? match_ref / norm_ref : 0.0);
+	
+	return std::make_pair(precision * recall / (0.5 * recall + 0.5 * precision), 0.0);
+      }
+
+      void assign(const score_type& score)
+      {
+	const F* rhs = dynamic_cast<const F*>(&score);
+	if (! rhs)
+	  throw std::runtime_error("invalid F measure");
+
+	match_ref = rhs->match_ref;
+	match_hyp = rhs->match_hyp;
+	norm_ref  = rhs->norm_ref;
+	norm_hyp  = rhs->norm_hyp;
+      }
+
+      void plus_equal(const score_type& score)
+      {
+	const F* rhs = dynamic_cast<const F*>(&score);
+	if (! rhs)
+	  throw std::runtime_error("invalid F measure");
+
+	match_ref += rhs->match_ref;
+	match_hyp += rhs->match_hyp;
+	norm_ref  += rhs->norm_ref;
+	norm_hyp  += rhs->norm_hyp;
+      }
+      
+      void minus_equal(const score_type& score)
+      {
+	const F* rhs = dynamic_cast<const F*>(&score);
+	if (! rhs)
+	  throw std::runtime_error("invalid F measure");
+
+	match_ref -= rhs->match_ref;
+	match_hyp -= rhs->match_hyp;
+	norm_ref  -= rhs->norm_ref;
+	norm_hyp  -= rhs->norm_hyp;
+      }
+
+      void multiplies_equal(const double& scale)
+      {
+	match_ref *= scale;
+	match_hyp *= scale;
+	norm_ref  *= scale;
+	norm_hyp  *= scale;
+      }
+      
+      void divides_equal(const double& scale)
+      {
+	match_ref /= scale;
+	match_hyp /= scale;
+	norm_ref  /= scale;
+	norm_hyp  /= scale;
+      }
+
+      score_ptr_type zero() const
+      {
+	return score_ptr_type(new F());
+      }
+
+      score_ptr_type clone() const
+      {
+	return score_ptr_type(new F(*this));
+      }
+      
+    protected:
+      count_type match_ref;
+      count_type match_hyp;
+      count_type norm_ref;
+      count_type norm_hyp;
+    };
+    
+  };
+};
+
+#endif
---
+++ cicada/eval/sk.hpp
@@ -0,0 +1,54 @@
+// -*- mode: c++ -*-
+
+#ifndef __CICADA__EVAL__SK__HPP__
+#define __CICADA__EVAL__SK__HPP__ 1
+
+// string-kernel
+
+#include <cicada/eval/f.hpp>
+
+namespace cicada
+{
+  namespace eval
+  {
+    class SK : protected F
+    {
+      
+    };
+
+    class SKScorerImpl;
+    
+    class SKScorer : public Scorer
+    {
+    public:
+      typedef double count_type;
+      
+    private:
+      typedef SKScorerImpl impl_type;
+      typedef std::vector<impl_type*, std::allocator<impl_type*> >  impl_set_type;
+  
+    public:
+      SKScorer() : impl() { }
+      SKScorer(const SKScorer& x);
+      ~SKScorer();
+      SKScorer& operator=(const SKScorer& x);
+
+    public:      
+      bool error_metric() const { return true; }
+      
+      scorer_ptr_type clone() const { return scorer_ptr_type(new SKScorer(*this)); }
+      
+      void clear();
+      
+      void insert(const sentence_type& sentence);
+      score_ptr_type score(const sentence_type& __sentence) const;
+      
+    private:
+      impl_set_type impl;
+    };
+    
+  };
+};
+
+
+#endif
