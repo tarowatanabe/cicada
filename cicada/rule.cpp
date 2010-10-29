7d2d0e660f28a9b94d8823b911ca342c91f605ad
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-10-29 16:24:07 +0900

    updated...

--- cicada/rule.cpp
+++ cicada/rule.cpp
@@ -105,7 +105,7 @@ namespace cicada
     
     const bool result = assign(iter, end);
     if (! result || iter != end)
-      throw std::runtime_error("rule format parsing failed..." + x);
+      throw std::runtime_error("rule format parsing failed...\"" + x + "\"");
   }
   
   std::istream& operator>>(std::istream& is, Rule& x)
