c079e6f9aa3893c9700ef8bc109955a835cd911e
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-11-19 15:45:25 +0900

    proper header inclusion

--- cicada/eval.hpp
+++ cicada/eval.hpp
@@ -7,7 +7,10 @@
 
 #include <cicada/eval/bleu.hpp>
 #include <cicada/eval/per.hpp>
+#include <cicada/eval/sb.hpp>
+#include <cicada/eval/sk.hpp>
 #include <cicada/eval/ter.hpp>
 #include <cicada/eval/wer.hpp>
+#include <cicada/eval/wlcs.hpp>
 
 #endif
