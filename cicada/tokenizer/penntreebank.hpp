c782d825e6b5cec24e2cc78e18512c9aaa0d8e52
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-11-22 12:45:52 +0900

    aded (word in progress) penntreebank style tokenizer

---
+++ cicada/tokenizer/penntreebank.hpp
@@ -0,0 +1,164 @@
+// -*- mode: c++ -*-
+
+#ifndef __CICADA__TOKENIZER__PENNTREEBANK__HPP__
+#define __CICADA__TOKENIZER__PENNTREEBANK__HPP__ 1
+
+#include <vector>
+#include <string>
+
+#include <cicada/tokenizer.hpp>
+
+#include <boost/regex.hpp>
+
+// Penntreebank style tokenizer
+/*
+#!/bin/sed -f
+
+# Sed script to produce Penn Treebank tokenization on arbitrary raw text.
+# Yeah, sure.
+
+# expected input: raw text with ONE SENTENCE TOKEN PER LINE
+
+# by Robert MacIntyre, University of Pennsylvania, late 1995.
+
+# If this wasn't such a trivial program, I'd include all that stuff about
+# no warrantee, free use, etc. from the GNU General Public License.  If you
+# want to be picky, assume that all of its terms apply.  Okay?
+
+# attempt to get correct directional quotes
+s=^"=`` =g
+s=\([ ([{<]\)"=\1 `` =g
+# close quotes handled at end
+
+s=\.\.\.= ... =g
+s=[,;:@#$%&]= & =g
+
+# Assume sentence tokenization has been done first, so split FINAL periods
+# only. 
+s=\([^.]\)\([.]\)\([])}>"']*\)[ 	]*$=\1 \2\3 =g
+# however, we may as well split ALL question marks and exclamation points,
+# since they shouldn't have the abbrev.-marker ambiguity problem
+s=[?!]= & =g
+
+# parentheses, brackets, etc.
+s=[][(){}<>]= & =g
+# Some taggers, such as Adwait Ratnaparkhi's MXPOST, use the parsed-file
+# version of these symbols.
+# UNCOMMENT THE FOLLOWING 6 LINES if you're using MXPOST.
+# s/(/-LRB-/g
+# s/)/-RRB-/g
+# s/\[/-LSB-/g
+# s/\]/-RSB-/g
+# s/{/-LCB-/g
+# s/}/-RCB-/g
+
+s=--= -- =g
+
+# NOTE THAT SPLIT WORDS ARE NOT MARKED.  Obviously this isn't great, since
+# you might someday want to know how the words originally fit together --
+# but it's too late to make a better system now, given the millions of
+# words we've already done "wrong".
+
+# First off, add a space to the beginning and end of each line, to reduce
+# necessary number of regexps.
+s=$= =
+s=^= =
+
+s="= '' =g
+# possessive or close-single-quote
+s=\([^']\)' =\1 ' =g
+# as in it's, I'm, we'd
+s='\([sSmMdD]\) = '\1 =g
+s='ll = 'll =g
+s='re = 're =g
+s='ve = 've =g
+s=n't = n't =g
+s='LL = 'LL =g
+s='RE = 'RE =g
+s='VE = 'VE =g
+s=N'T = N'T =g
+
+s= \([Cc]\)annot = \1an not =g
+s= \([Dd]\)'ye = \1' ye =g
+s= \([Gg]\)imme = \1im me =g
+s= \([Gg]\)onna = \1on na =g
+s= \([Gg]\)otta = \1ot ta =g
+s= \([Ll]\)emme = \1em me =g
+s= \([Mm]\)ore'n = \1ore 'n =g
+s= '\([Tt]\)is = '\1 is =g
+s= '\([Tt]\)was = '\1 was =g
+s= \([Ww]\)anna = \1an na =g
+# s= \([Ww]\)haddya = \1ha dd ya =g
+# s= \([Ww]\)hatcha = \1ha t cha =g
+
+# clean out extra spaces
+s=  *= =g
+s=^ *==g
+ */
+
+namespace cicada
+{
+  namespace tokenizer
+  {
+    class Penntreebank : public cicada::Tokenizer
+    {
+    private:
+      typedef boost::regex regex_type;
+      typedef const char*  replace_type;
+      typedef std::pair<regex_type, replace_type> pattern_type;
+      
+    public:
+      Penntreebank()
+	: pattern1(regex_type("^[[:space:]]*\""), " `` "),
+	  pattern2(regex_type("([\\x20\\x28\\x5B\\x7B\\x3C])\""), "$1 `` "),
+	  
+	  pattern3(regex_type("\\.\\.\\."), " ... "),
+	  pattern4(regex_type("([,;:@#$%&])"), " $1 "),
+	  
+	  pattern5(regex_type("([^.])([.])([\\x5B\\x5D\\x29\\x7D\\x3E\\x22\\x27]*)(?=[[:space:]]*$)"), "$1 $2$3"),
+	  pattern6(regex_type("([?!\\x5B\\x5D\\x28\\x29\\x7B\\x7D\\x3C\\x3E])"), " $1 "),
+	  pattern7(regex_type("--"), " -- "),
+	  pattern8(regex_type("\""), " '' ")
+      {}
+    protected:
+      virtual void tokenize(const sentence_type& source, sentence_type& tokenized) const
+      {
+	tokenized.clear();
+	
+	if (source.empty()) return;
+	
+	std::string tokenized_string = ' ';
+	sentence_type::const_iterator siter_end = source.end();
+	for (sentence_type::const_iterator siter = source.begin(); siter != siter_end; ++ siter) {
+	  tokenized_string += static_cast<const std::string&>(*siter);
+	  tokenized_string += ' ';
+	}
+	
+	std::string result;
+	boost::regex_replace(std::back_inserter(result), tokenized_string.begin(), tokenized_string.end(), pattern1.first, pattern1.second);
+	
+	tokenized_string.swap(result);
+	result.clear();
+	boost::regex_replace(std::back_inserter(result), tokenized_string.begin(), tokenized_string.end(), pattern2.first, pattern2.second);
+	
+	tokenized_string.swap(result);
+	result.clear();
+	boost::regex_replace(std::back_inserter(result), tokenized_string.begin(), tokenized_string.end(), pattern3.first, pattern3.second);
+	
+	tokenized_string.swap(result);
+	result.clear();
+	boost::regex_replace(std::back_inserter(result), tokenized_string.begin(), tokenized_string.end(), pattern4.first, pattern4.second);
+	
+	tokenized.assign(result);
+      }
+      
+    private:
+      pattern_type pattern1;
+      pattern_type pattern2;
+      pattern_type pattern3;
+      pattern_type pattern4;
+    };
+  };
+};
+
+#endif
