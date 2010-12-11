220b0afd78cba74c15b209e0283fa9374f5702b0
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-12-11 18:00:00 +0900

    use Intersect sturct and use rule::creater interface

--- cicada/intersect.hpp
+++ cicada/intersect.hpp
@@ -20,8 +20,7 @@
 
 namespace cicada
 {
-  inline
-  void intersect(const HyperGraph& source, const Lattice& lattice, HyperGraph& target)
+  struct Intersect
   {
     typedef size_t    size_type;
     typedef ptrdiff_t difference_type;
@@ -42,59 +41,67 @@ namespace cicada
     typedef hypergraph_type::feature_set_type feature_set_type;
     
     typedef grammar_type::rule_pair_type rule_pair_type;
-    
-    typedef rule_type::symbol_set_type  symbol_set_type;
-    
+        
     typedef std::vector<symbol_type, std::allocator<symbol_type> > non_terminal_set_type;
     typedef std::vector<symbol_type, std::allocator<symbol_type> > yield_type;
-
-    target.clear();
     
-    if (source.goal == hypergraph_type::invalid || lattice.empty()) return;
-    
-    non_terminal_set_type non_terminals(source.nodes.size());
-    for (size_type id = 0; id < source.nodes.size(); ++ id)
-      non_terminals[id] = std::string("[NODE_") + boost::lexical_cast<std::string>(id) + ']';
-    
-    
-    boost::shared_ptr<grammar_mutable_type> g(new grammar_mutable_type());
-    grammar_type grammar;
-    grammar.push_back(g);
-
-    yield_type yield;
-    
-    hypergraph_type::edge_set_type::const_iterator eiter_end = source.edges.end();
-    for (hypergraph_type::edge_set_type::const_iterator eiter = source.edges.begin(); eiter != eiter_end; ++ eiter) {
-      const edge_type& edge = *eiter;
+    void operator()(const hypergraph_type& source, const lattice_type& lattice, hypergraph_type& target)
+    {
+      target.clear();
       
-      yield.clear();
-      yield.insert(yield.end(), edge.rule->rhs.begin(), edge.rule->rhs.end());
+      if (source.goal == hypergraph_type::invalid || lattice.empty()) return;
       
-      // sort...
-      if (! edge.tails.empty()) {
-	int pos = 0;
-	yield_type::iterator siter_end = yield.end();
-	for (yield_type::iterator siter = yield.begin(); siter != siter_end; ++ siter)
-	  if (siter->is_non_terminal()) {
-	    int non_terminal_index = siter->non_terminal_index() - 1;
-	    if (non_terminal_index < 0)
-	      non_terminal_index = pos;
+      non_terminal_set_type non_terminals(source.nodes.size());
+      for (size_type id = 0; id < source.nodes.size(); ++ id)
+	non_terminals[id] = std::string("[NODE_") + boost::lexical_cast<std::string>(id) + ']';
+      
+      boost::shared_ptr<grammar_mutable_type> g(new grammar_mutable_type());
+      grammar_type grammar;
+      grammar.push_back(g);
+      
+      yield_type yield;
+    
+      hypergraph_type::edge_set_type::const_iterator eiter_end = source.edges.end();
+      for (hypergraph_type::edge_set_type::const_iterator eiter = source.edges.begin(); eiter != eiter_end; ++ eiter) {
+	const edge_type& edge = *eiter;
+      
+	yield.clear();
+	yield.insert(yield.end(), edge.rule->rhs.begin(), edge.rule->rhs.end());
+      
+	// sort...
+	if (! edge.tails.empty()) {
+	  int pos = 0;
+	  yield_type::iterator siter_end = yield.end();
+	  for (yield_type::iterator siter = yield.begin(); siter != siter_end; ++ siter)
+	    if (siter->is_non_terminal()) {
+	      int non_terminal_index = siter->non_terminal_index() - 1;
+	      if (non_terminal_index < 0)
+		non_terminal_index = pos;
 	    
-	    *siter = non_terminals[edge.tails[non_terminal_index]].non_terminal(pos);
-	    ++ pos;
-	  }
+	      *siter = non_terminals[edge.tails[non_terminal_index]].non_terminal(pos);
+	      ++ pos;
+	    }
+	}
+      
+	rule_ptr_type rule(rule_type::create(rule_type(non_terminals[edge.head], yield.begin(), yield.end())));
+      
+	g->insert(rule_pair_type(rule, rule, edge.features, edge.attributes));
       }
-      
-      rule_ptr_type rule(new rule_type(non_terminals[edge.head], symbol_set_type(yield.begin(), yield.end())));
-      
-      g->insert(rule_pair_type(rule, rule, edge.features));
+    
+      const symbol_type goal = non_terminals[source.goal];
+    
+      ComposeCKY composer(goal, grammar);
+    
+      composer(lattice, target);
     }
+  };
+
+  inline
+  void intersect(const HyperGraph& source, const Lattice& lattice, HyperGraph& target)
+  {
+    Intersect __intersect;
     
-    const symbol_type goal = non_terminals[source.goal];
-    
-    ComposeCKY composer(goal, grammar);
-    
-    composer(lattice, target);
+    __intersect(source, lattice, target);
   }
 };
 
