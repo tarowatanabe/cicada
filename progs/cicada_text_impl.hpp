bffc2d17fc77f8ab3ed424e6b9fbe252799bdb53
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-12-11 21:03:36 +0900

    implemented refset/tstset reader...

---
+++ progs/cicada_text_impl.hpp
@@ -0,0 +1,36 @@
+//
+//  Copyright(C) 2010 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+#ifndef __CICADA__TEXT__IMPL__HPP__
+#define __CICADA__TEXT__IMPL__HPP__ 1
+
+#define BOOST_SPIRIT_THREADSAFE
+#define PHOENIX_THREADSAFE
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/support_istream_iterator.hpp>
+
+#include <cicada/sentence.hpp>
+
+typedef std::pair<int, cicada::Sentence> id_sentence_type;
+
+template <typename Iterator>
+struct cicada_sentence_parser : boost::spirit::qi::grammar<Iterator, id_sentence_type(), boost::spirit::standard::blank_type>
+{
+  cicada_sentence_parser() : cicada_sentence_parser::base_type(id_sentence)
+  {
+    namespace qi = boost::spirit::qi;
+    namespace standard = boost::spirit::standard;
+    
+    word        %= qi::lexeme[+(standard::char_ - standard::space) - "|||"];
+    id_sentence %= qi::int_ >> "|||" >> *word >> (qi::eol | qi::eoi);
+  };
+  
+  typedef boost::spirit::standard::blank_type blank_type;
+  
+  boost::spirit::qi::rule<Iterator, std::string(), blank_type>      word;
+  boost::spirit::qi::rule<Iterator, id_sentence_type(), blank_type> id_sentence;
+};
+
+#endif
