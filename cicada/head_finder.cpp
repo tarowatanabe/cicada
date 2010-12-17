9ce7e93755be84cd5d1b6fcb620dc0a1ce3b5559
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-12-17 11:11:32 +0900

    updated...

--- cicada/head_finder.cpp
+++ cicada/head_finder.cpp
@@ -8,11 +8,7 @@ namespace cicada
   const char* HeadFinder::lists()
   {
     static const char* desc = "\
-lower: matching by lower-case\n\
-stemmer: matching by stemming algorithm\n\
-\talgorithm=[stemmer spec]\n\
-wordnet: matching by wordnet synsets\n\
-\tpath=[path to wordnet database]\n   \
+collins: collins head finder\n\
 ";
     return desc;
   }
