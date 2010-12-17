639a7e420db220535e3ee472a541a702bd89adb3
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-12-17 16:11:13 +0900

    updated...

--- cicada/head/chinese.cpp
+++ cicada/head/chinese.cpp
@@ -10,64 +10,44 @@ namespace cicada
 {
   namespace head
   {
-    static String[] leftExceptPunct = {"leftexcept", "PU"};
-    static String[] rightExceptPunct = {"rightexcept", "PU"};
-    
     static const char* __chinese_punct[] = {"PU"};
     
     static const char* __chinese_ROOT[] = {"IP"};
     static const char* __chinese_PAIR[] = {"IP"};
     static const char* __chinese_ADJP[] = {"JJ", "ADJP"};
     static const char* __chinese_ADVP[] = {"AD", "CS", "ADVP", "JJ"};
-    static const char* __chinese_CLP[] = {"M", "CLP"};
-    static const char* __chinese_CP[] = {"DEC", "WHNP", "WHPP"};
-    static const char* __chinese_DNP[] = {"DEG", "DEC"};
+    static const char* __chinese_CLP[]  = {"M", "CLP"};
+    static const char* __chinese_CP[]   = {"DEC", "WHNP", "WHPP"};
+    static const char* __chinese_DNP[]  = {"DEG", "DEC"};
+    static const char* __chinese_DP[]   = {"DT", "DP"};
+    static const char* __chinese_DVP[]  = {"DEV", "DEC"};
+    static const char* __chinese_FRAG[] = {"VV", "NN"};
+    static const char* __chinese_INTJ[] = {"INTJ", "IJ", "SP"};
+    static const char* __chinese_IP[]   = {"VP", "IP"};
+    static const char* __chinese_LCP[]  = {"LC", "LCP"};
+    static const char* __chinese_LST[]  = {"CD", "PU"};
+    static const char* __chinese_NP[]   = {"NN", "NR", "NT", "NP", "PN", "CP"};
+    static const char* __chinese_PP[]   = {"P", "PP"};
+    static const char* __chinese_PRN0[] = {"NP", "VP", "IP", "QP", "PP", "ADJP", "CLP", "LCP"};
+    static const char* __chinese_PRN1[] = {"NN", "NR", "NT", "FW"};
+    static const char* __chinese_QP[]   = {"QP", "CLP", "CD", "OD", "NP", "NT", "M"};
+    static const char* __chinese_UCP[]  = {};
+    static const char* __chinese_VP[]   = {"VP", "VCD", "VPT", "VV", "VCP", "VA", "VC", "VE", "IP", "VSB", "VCP", "VRD", "VNV"};
+    static const char* __chinese_VCD[]  = {"VCD", "VV", "VA", "VC", "VE"};
+    static const char* __chinese_VCP[]  = {"VCD", "VV", "VA", "VC", "VE"};
+    static const char* __chinese_VRD[]  = {"VCD", "VRD", "VV", "VA", "VC", "VE"};
+    static const char* __chinese_VSB[]  = {"VCD", "VSB", "VV", "VA", "VC", "VE"};
+    static const char* __chinese_VNV[]  = {"VV", "VA", "VC", "VE"};
+    static const char* __chinese_VPT[]  = {"VV", "VA", "VC", "VE"};
+    static const char* __chinese_CD[]   = {"CD"};
+    static const char* __chinese_NN[]   = {"NN"};
+    static const char* __chinese_NR[]   = {"NR"};
     
-    
-    nonTerminalInfo.put("DP", new String[][]{{left, "DT", "DP"}}); // there's one instance of DP adjunction
-    nonTerminalInfo.put("DVP", new String[][]{{right, "DEV", "DEC"}}); // DVP always has DEV under it
-    nonTerminalInfo.put("FRAG", new String[][]{{right, "VV", "NN"}, rightExceptPunct}); //FRAG seems only to be used for bits at the beginnings of articles: "Xinwenshe<DATE>" and "(wan)"
-    nonTerminalInfo.put("INTJ", new String[][]{{right, "INTJ", "IJ", "SP"}});
-    nonTerminalInfo.put("IP", new String[][]{{left, "VP", "IP"}, rightExceptPunct});  // CDM July 2010 following email from Pi-Chuan changed preference to VP over IP: IP can be -SBJ, -OBJ, or -ADV, and shouldn't be head
-    nonTerminalInfo.put("LCP", new String[][]{{right, "LC", "LCP"}}); // there's a bit of LCP adjunction
-    nonTerminalInfo.put("LST", new String[][]{{right, "CD", "PU"}}); // covers all examples
-    nonTerminalInfo.put("NP", new String[][]{{right, "NN", "NR", "NT", "NP", "PN", "CP"}}); // Basic heads are NN/NR/NT/NP; PN is pronoun.  Some NPs are nominalized relative clauses without overt nominal material; these are NP->CP unary rewrites.  Finally, note that this doesn't give any special treatment of coordination.
-    nonTerminalInfo.put("PP", new String[][]{{left, "P", "PP"}}); // in the manual there's an example of VV heading PP but I couldn't find such an example with tgrep2
-    // cdm 2006: PRN changed to not choose punctuation.  Helped parsing (if not significantly)
-    // nonTerminalInfo.put("PRN", new String[][]{{left, "PU"}}); //presumably left/right doesn't matter
-    nonTerminalInfo.put("PRN", new String[][]{{left, "NP", "VP", "IP", "QP", "PP", "ADJP", "CLP", "LCP"}, {rightdis, "NN", "NR", "NT", "FW"}});
-    // cdm 2006: QP: add OD -- occurs some; occasionally NP, NT, M; parsing performance no-op
-    nonTerminalInfo.put("QP", new String[][]{{right, "QP", "CLP", "CD", "OD", "NP", "NT", "M"}}); // there's some QP adjunction
-    // add OD?
-    nonTerminalInfo.put("UCP", new String[][]{{left, }}); //an alternative would be "PU","CC"
-    nonTerminalInfo.put("VP", new String[][]{{left, "VP", "VCD", "VPT", "VV", "VCP", "VA", "VC", "VE", "IP", "VSB", "VCP", "VRD", "VNV"}, leftExceptPunct}); //note that ba and long bei introduce IP-OBJ small clauses; short bei introduces VP
-    // add BA, LB, as needed
-
-    // verb compounds
-    nonTerminalInfo.put("VCD", new String[][]{{left, "VCD", "VV", "VA", "VC", "VE"}}); // could easily be right instead
-    nonTerminalInfo.put("VCP", new String[][]{{left, "VCD", "VV", "VA", "VC", "VE"}}); // not much info from documentation
-    nonTerminalInfo.put("VRD", new String[][]{{left, "VCD", "VRD", "VV", "VA", "VC", "VE"}}); // definitely left
-    nonTerminalInfo.put("VSB", new String[][]{{right, "VCD", "VSB", "VV", "VA", "VC", "VE"}}); // definitely right, though some examples look questionably classified (na2lai2 zhi1fu4)
-    nonTerminalInfo.put("VNV", new String[][]{{left, "VV", "VA", "VC", "VE"}}); // left/right doesn't matter
-    nonTerminalInfo.put("VPT", new String[][]{{left, "VV", "VA", "VC", "VE"}}); // activity verb is to the left
-
-    // some POS tags apparently sit where phrases are supposed to be
-    nonTerminalInfo.put("CD", new String[][]{{right, "CD"}});
-    nonTerminalInfo.put("NN", new String[][]{{right, "NN"}});
-    nonTerminalInfo.put("NR", new String[][]{{right, "NR"}});
-
-    // I'm adding these POS tags to do primitive morphology for character-level
-    // parsing.  It shouldn't affect anything else because heads of preterminals are not
-    // generally queried - GMA
-    nonTerminalInfo.put("VV", new String[][]{{left}});
-    nonTerminalInfo.put("VA", new String[][]{{left}});
-    nonTerminalInfo.put("VC", new String[][]{{left}});
-    nonTerminalInfo.put("VE", new String[][]{{left}});
-
-    // new for ctb6.
-    nonTerminalInfo.put("FLR", new String[][]{rightExceptPunct});
-
-    
+    static const char* __chinese_VV[]   = {};
+    static const char* __chinese_VA[]   = {};
+    static const char* __chinese_VC[]   = {};
+    static const char* __chinese_VE[]   = {};
+    static const char* __chinese_FLR[]  = {};
     
     Chinese::Chinese() : HeadFinder("chinese")
     {
