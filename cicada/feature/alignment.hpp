51e45a325e18285a70e626af902be83b29bcf14a
Taro Watanabe <taro.watanabe@nict.go.jp>
2010-12-24 13:19:21 +0900

    implemented alignment feature

--- cicada/feature/alignment.hpp
+++ cicada/feature/alignment.hpp
@@ -6,37 +6,24 @@
 #ifndef __CICADA__FEATURE__ALIGNMENT__HPP__
 #define __CICADA__FEATURE__ALIGNMENT__HPP__ 1
 
-#include <cicada/cluster.hpp>
+#include <cicada/cluster_stemmer.hpp>
 #include <cicada/feature_function.hpp>
 
 namespace cicada
 {
   namespace feature
   {
-    class RelativePosition : public FeatureFunction
+    
+    // alignment related feature with states...
+    template <typename F>
+    class AlignmentBaseState : public FeatureFunction
     {
     public:
-      typedef Cluster cluster_type;
-      
-    private:
-      typedef FeatureFunction base_type;
-
-    public:
-      RelativePosition(const std::string& parameter);
-      RelativePosition(const RelativePosition& x)
-	: base_type(static_cast<const base_type&>(x)), cluster(0)
+      AlignmentBaseState(const std::string& parameter)
+	: f(parameter, FeatureFunction::__state_size, FeatureFunction::__feature_name)
       {
-	if (x.cluster)
-	  cluster = &cluster_type::create(x.cluster->algorithm());
-      }
-      RelativePosition& operator=(const RelativePosition& x)
-      {
-	static_cast<base_type&>(*this) = static_cast<const base_type&>(x);
-	
-	cluster = 0;
-	if (x.cluster)
-	  cluster = &cluster_type::create(x.cluster->algorithm());
-	return *this;
+	if (__state_size == 0)
+	  throw std::runtime_error("Are we stateless feature...?")
       }
       
       virtual void apply(state_ptr_type& state,
@@ -44,47 +31,194 @@ namespace cicada
 			 const edge_type& edge,
 			 feature_set_type& features,
 			 feature_set_type& estimates,
-			 const bool final) const;
+			 const bool final) const { f(*this, state, states, edge, features, estiamtes, final); }
       virtual void apply_coarse(state_ptr_type& state,
 				const state_ptr_set_type& states,
 				const edge_type& edge,
 				feature_set_type& features,
 				feature_set_type& estimates,
-				const bool final) const;
+				const bool final) const {}
       virtual void apply_predict(state_ptr_type& state,
 				 const state_ptr_set_type& states,
 				 const edge_type& edge,
 				 feature_set_type& features,
 				 feature_set_type& estimates,
-				 const bool final) const;
+				 const bool final) const {}
       virtual void apply_scan(state_ptr_type& state,
 			      const state_ptr_set_type& states,
 			      const edge_type& edge,
 			      const int dot,
 			      feature_set_type& features,
 			      feature_set_type& estimates,
-			      const bool final) const;
+			      const bool final) const {}
       virtual void apply_complete(state_ptr_type& state,
 				  const state_ptr_set_type& states,
 				  const edge_type& edge,
 				  feature_set_type& features,
 				  feature_set_type& estimates,
-				  const bool final) const;
+				  const bool final) const { f(*this, state, states, edge, features, estiamtes, final); }
+
+      virtual void assign(const size_type& id,
+			  const hypergraph_type& hypergraph,
+			  const lattice_type& lattice,
+			  const span_set_type& spans,
+			  const sentence_set_type& targets,
+			  const ngram_count_set_type& ngram_counts) { f(*this, id, hypergraph, lattice, spans, targets, ngram_counts); }
       
-      virtual feature_function_ptr_type clone() const { return feature_function_ptr_type(new RelativePosition(*this)); }
+      virtual feature_function_ptr_type clone() const { return feature_function_ptr_type(new AlignmentBaseState<F>(*this)); }
+      
+    private:
+      F f;
+    };
+    
+    // alignment related feature without states...
+    
+    template <typename F>
+    class AlignmentBase : public FeatureFunction
+    {
+    public:
+      AlignmentBase(const std::string& parameter)
+	: f(parameter, FeatureFunction::__state_size, FeatureFunction::__feature_name)
+      {
+	if (__state_size != 0)
+	  throw std::runtime_error("we are not stateless feature...?");
+      }
+      
+      virtual void apply(state_ptr_type& state,
+			 const state_ptr_set_type& states,
+			 const edge_type& edge,
+			 feature_set_type& features,
+			 feature_set_type& estimates,
+			 const bool final) const { f(*this, state, states, edge, features, estiamtes, final); }
+      virtual void apply_coarse(state_ptr_type& state,
+				const state_ptr_set_type& states,
+				const edge_type& edge,
+				feature_set_type& features,
+				feature_set_type& estimates,
+				const bool final) const { f(*this, state, states, edge, features, estiamtes, final); }
+      virtual void apply_predict(state_ptr_type& state,
+				 const state_ptr_set_type& states,
+				 const edge_type& edge,
+				 feature_set_type& features,
+				 feature_set_type& estimates,
+				 const bool final) const { f(*this, state, states, edge, features, estiamtes, final); }
+      virtual void apply_scan(state_ptr_type& state,
+			      const state_ptr_set_type& states,
+			      const edge_type& edge,
+			      const int dot,
+			      feature_set_type& features,
+			      feature_set_type& estimates,
+			      const bool final) const {}
+      virtual void apply_complete(state_ptr_type& state,
+				  const state_ptr_set_type& states,
+				  const edge_type& edge,
+				  feature_set_type& features,
+				  feature_set_type& estimates,
+				  const bool final) const {  }
       
       virtual void assign(const size_type& id,
 			  const hypergraph_type& hypergraph,
 			  const lattice_type& lattice,
 			  const span_set_type& spans,
 			  const sentence_set_type& targets,
-			  const ngram_count_set_type& ngram_counts);
+			  const ngram_count_set_type& ngram_counts) { f(*this, id, hypergraph, lattice, spans, targets, ngram_counts); }
+      
+      virtual feature_function_ptr_type clone() const { return feature_function_ptr_type(new AlignmentBase<F>(*this)); }
       
     private:
-      const cluster_type* cluster;
-      const sentence_type* sentence;
-      int source_length;
-      int target_length;
+      F f;
+    };
+
+    // we will put into align namespace...
+    namespace align
+    {
+      struct Base
+      {
+	typedef size_t    size_type;
+	typedef ptrdiff_t difference_type;
+	
+	typedef cicada::Symbol     symbol_type;
+	typedef cicada::Vocab      vocab_type;
+	typedef cicada::Feature    feature_type;
+	typedef cicada::HyperGraph hypergraph_type;
+	typedef cicada::SpanVector span_set_type;
+	typedef cicada::Lattice    lattice_type;
+	typedef cicada::Rule       rule_type;
+	
+	typedef cicada::SentenceVector sentence_set_type;
+	typedef cicada::NGramCountSet  ngram_count_set_type;
+	
+	typedef hypergraph_type::feature_set_type   feature_set_type;
+	typedef hypergraph_type::attribute_set_type attribute_set_type;
+
+	typedef feature_set_type::feature_type     feature_type;
+	typedef attribute_set_type::attribute_type attribute_type;
+	
+	typedef FeatureFunction feature_function_type;
+	
+	typedef feature_function_type::state_ptr_type     state_ptr_type;
+	typedef feature_function_type::state_ptr_set_type state_ptr_set_type;
+	
+	typedef feature_function_type::edge_type edge_type;
+      };
+
+      class RelativePosition : public Base
+      {
+      public:
+	typedef ClusterStemmer normalizer_type;
+	typedef std::vector<normlaizer_type, std::allocator<normalizer_type> > normalizer_set_type;
+	
+	RelativePositoin(const std::string& parameter, size_type& __state_size, feature_type& __feature_name);
+	
+	void operator()(const feature_function_type& feature_function,
+			state_ptr_type& state,
+			const state_ptr_set_type& states,
+			const edge_type& edge,
+			feature_set_type& features,
+			feature_set_type& estimates,
+			const bool final) const;
+	
+	void operator()(const feature_function_type& feature_function,
+			const size_type& id,
+			const hypergraph_type& hypergraph,
+			const lattice_type& lattice,
+			const span_set_type& spans,
+			const sentence_set_type& targets,
+			const ngram_count_set_type& ngram_counts);
+	
+      private:
+	normalizer_set_type normalizers;
+	const sentence_type* sentence;
+	int source_length;
+	int target_length;
+      };
+      
+      struct NullJump : public Base
+      {
+	NullJump(const std::string& parameter, size_type& __state_size, feature_type& __feature_name);
+	
+	void operator()(const feature_function_type& feature_function,
+			state_ptr_type& state,
+			const state_ptr_set_type& states,
+			const edge_type& edge,
+			feature_set_type& features,
+			feature_set_type& estimates,
+			const bool final) const;
+	
+	void operator()(const feature_function_type& feature_function,
+			const size_type& id,
+			const hypergraph_type& hypergraph,
+			const lattice_type& lattice,
+			const span_set_type& spans,
+			const sentence_set_type& targets,
+			const ngram_count_set_type& ngram_counts);
+      };
+      
+      
+      // actual classes...
+      typedef AlignmentBase<align::RelativePosition> RelativePosition;
+      typedef AlignmentBaseState<align::NullJump>    NullJump;
+      
     };
   };
 };
