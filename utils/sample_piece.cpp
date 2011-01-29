db553cd60766cc38b4e037a1e77149fed21c76ed
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-01-29 16:14:36 +0900

    updated

--- utils/sample_piece.cpp
+++ utils/sample_piece.cpp
@@ -12,11 +12,11 @@ int main(int argc, char** argv)
   utils::piece pie1(str1);
   utils::piece pie2(str2);
   
-  std::cout << "piece1: " << pie1 << std::endl
-	    << "piece2: " << pie2 << std::endl;
+  std::cout << "piece1: " << pie1 << " base: " << (void*) pie1.c_str() << std::endl
+	    << "piece2: " << pie2 << " base: " << (void*) pie2.c_str() << std::endl;
   
-  std::cout << "piece1 substr: " << pie1.substr(5) << std::endl
-	    << "piece2 substr: " << pie2.substr(4) << std::endl
+  std::cout << "piece1 substr: " << pie1.substr(5) << " base: " << (void*) pie1.substr(5).c_str() << std::endl
+	    << "piece2 substr: " << pie2.substr(4) << " base: " << (void*) pie2.substr(4).c_str() << std::endl
 	    << "equal? " << (pie1.substr(5) == pie2.substr(4)) << std::endl;
   
   typedef boost::tokenizer<utils::space_separator, utils::piece::const_iterator, utils::piece> tokenizer_type;
@@ -25,8 +25,8 @@ int main(int argc, char** argv)
   tokenizer_type tokens2(pie2);
   
   for (tokenizer_type::iterator iter = tokens1.begin(); iter != tokens1.end(); ++ iter)
-    std::cout << "token1: " << *iter << std::endl;
+    std::cout << "token1: " << *iter << " base: " << (void*) (*iter).c_str() << " cast: " << static_cast<std::string>(*iter) << std::endl;
   for (tokenizer_type::iterator iter = tokens2.begin(); iter != tokens2.end(); ++ iter)
-    std::cout << "token2: " << *iter << std::endl;
+    std::cout << "token2: " << *iter << " base: " << (void*) (*iter).c_str() << " cast: " << static_cast<std::string>(*iter) << std::endl;
   
 }
