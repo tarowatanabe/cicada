a65ef13994d3e75ff4f21e65a1308ecadf75bf18
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-01 13:17:18 +0900

    added bitext filter

---
+++ progs/cicada_filter_bitext.cpp
@@ -0,0 +1,169 @@
+//
+//  Copyright(C) 2011 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+// bitext filter
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/karma.hpp>
+
+#include <boost/fusion/tuple.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/thread.hpp>
+
+#include "utils/bithack.hpp"
+#include "utils/program_options.hpp"
+#include "utils/compress_stream.hpp"
+#include "utils/subprocess.hpp"
+#include "utils/lockfree_list_queue.hpp"
+
+typedef boost::filesystem::path path_type;
+
+typedef std::vector<std::string, std::allocator<std::string> > sentence_type;
+
+template <typename Iterator>
+struct sentence_parser : boost::spirit::qi::grammar<Iterator, sentence_type(), boost::spirit::standard::blank_type>
+{
+  sentence_parser() : sentence_parser::base_type(tokens)
+  {
+    namespace qi = boost::spirit::qi;
+    namespace standard = boost::spirit::standard;
+    
+    tokens  %= *qi::lexeme[+(standard::char_ - standard::space) - "|||"] >> (qi::eoi | qi::eol);
+  }
+  
+  typedef boost::spirit::standard::blank_type blank_type;
+  
+  boost::spirit::qi::rule<Iterator, sentence_type(), blank_type> tokens;
+};
+
+template <typename Iterator>
+struct sentence_generator : boost::spirit::karma::grammar<Iterator, sentence_type()>
+{
+  sentence_generator() : sentence_generator::base_type(tokens)
+  {
+    namespace karma = boost::spirit::karma;
+    namespace standard = boost::spirit::standard;
+    
+    tokens  %= -((+standard::char_) % ' ') << '\n';
+  }
+  
+  typedef boost::spirit::standard::blank_type blank_type;
+  
+  boost::spirit::karma::rule<Iterator, sentence_type()> tokens;
+};
+
+
+path_type source_file = "-";
+path_type target_file = "-";
+
+path_type output_source_file = "-";
+path_type output_target_file = "-";
+
+int max_length = 100;
+double max_fertility = 10;
+
+void options(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+  try {
+    options(argc, argv);
+    
+    utils::compress_istream is_src(source_file, 1024 * 1024);
+    utils::compress_istream is_trg(target_file, 1024 * 1024);
+
+    is_src.unsetf(std::ios::skipws);
+    is_trg.unsetf(std::ios::skipws);
+    
+    utils::compress_ostream os_src(output_source_file, 1024 * 1024);
+    utils::compress_ostream os_trg(output_target_file, 1024 * 1024);
+    
+    typedef boost::spirit::istream_iterator iiter_type;
+    typedef std::ostream_iterator<char>     oiter_type;
+    
+    sentence_parser<iiter_type>    parser;
+    sentence_generator<oiter_type> generator;
+    
+    iiter_type siter(is_src);
+    iiter_type titer(is_trg);
+    iiter_type siter_end;
+    iiter_type titer_end;
+
+    sentence_type source;
+    sentence_type target;
+    
+    for (size_t line_no = 0; siter != siter_end && titer != titer_end; ++ line_no) {
+      source.clear();
+      target.clear();
+      
+      if (! boost::spirit::qi::phrase_parse(siter, siter_end, parser, boost::spirit::standard::blank, source))
+	throw std::runtime_error("source sentence parsing failed at #: " + boost::lexical_cast<std::string>(line_no));
+      if (! boost::spirit::qi::phrase_parse(titer, titer_end, parser, boost::spirit::standard::blank, target))
+	throw std::runtime_error("target sentence parsing failed at #: " + boost::lexical_cast<std::string>(line_no));
+      
+      const size_t source_size = source.size();
+      const size_t target_size = target.size();
+      
+      if (source_size == 0 || target_size == 0) continue;
+      if (max_length > 0)
+	if (source_size > max_length || target_size > max_length) continue;
+      if (max_fertility > 0)
+	if (double(utils::bithack::max(source_size, target_size)) / double(utils::bithack::min(source_size, target_size)) >= max_fertility) continue;
+      
+      if (! boost::spirit::karma::generate(oiter_type(os_src), generator, source))
+	throw std::runtime_error("source sentence generation failed at #: " + boost::lexical_cast<std::string>(line_no));
+      if (! boost::spirit::karma::generate(oiter_type(os_trg), generator, target))
+	throw std::runtime_error("target sentence generation failed at #: " + boost::lexical_cast<std::string>(line_no));
+    }
+    if (siter != siter_end || titer != titer_end)
+      throw std::runtime_error("# of lines do not match");
+    
+  }
+  catch(std::exception& err) {
+    std::cerr << "error: " << err.what() << std::endl;
+    return -1;
+  }
+  return 0;
+}
+
+
+void options(int argc, char** argv)
+{
+  namespace po = boost::program_options;
+  
+  po::options_description desc("options");
+  desc.add_options()
+    ("source", po::value<path_type>(&source_file)->default_value(source_file), "source file")
+    ("target", po::value<path_type>(&target_file)->default_value(target_file), "target file")
+
+    ("output-source", po::value<path_type>(&output_source_file)->default_value(output_source_file), "output source file")
+    ("output-target", po::value<path_type>(&output_target_file)->default_value(output_target_file), "output target file")
+    
+    ("max-length",    po::value<int>(&max_length)->default_value(max_length),          "maximum length")
+    ("max-fertility", po::value<double>(&max_fertility)->default_value(max_fertility), "maximum fertility")
+        
+    ("help", "help message");
+  
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc, po::command_line_style::unix_style & (~po::command_line_style::allow_guessing)), vm);
+  po::notify(vm);
+  
+  if (vm.count("help")) {
+    std::cout << argv[0] << " [options]" << '\n' << desc << '\n';
+    exit(0);
+  }
+}
+
