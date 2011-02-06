ab52478e58128d4ec4cbe8ce9a2781ee316f8d96
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-06 14:27:37 +0900

    updated...

--- cicada/binarize_cyk.hpp
+++ cicada/binarize_cyk.hpp
@@ -6,29 +6,42 @@
 #ifndef __CICADA__BINARIZE_CYK__HPP__
 #define __CICADA__BINARIZE_CYK__HPP__ 1
 
+#include <deque>
+
 #include <cicada/binarize_base.hpp>
 
 namespace cicada
 {
   struct BinarizeCYK : public BinarizeBase
   {
-    BinarizeCYK(const int __order=-1)
+    BinarizeCYK(const int __order=0)
       : order(__order) {}
-
+    
+    typedef std::pair<int, int> span_type;
+    typedef std::vector<span_type, std::allocator<span_type> > span_set_type;
+    
+    typedef std::vector<symbol_type, std::allocator<symbol_type> > label_set_type;
+    typedef std::deque<label_set_type, std::allocator<label_set_type> > label_nodes_type;
+    
+    typedef utils::chart<hypergraph_type::id_type, std::allocator<hypergraph_type::id_type> > node_chart_type;
+    
     void operator()(const hypergraph_type& source, hypergraph_type& target)
     {
-      // first, copy...
-      target = source;
+      // first, topological-order traversal to compute span for each node and 
+      // ancestors.
+      // I don't know how to handle unary rules... we will take label from the bottom. (top or concatenate them together?)
       
-      
+      // order <= 0 implies all the ancestors...
       
     }
     
+    span_set_type spans;
+    
     const int order;
   };
-
+  
   inline
-  void binarize_cyk(const HyperGraph& source, HyperGraph& target, const int order=-1)
+  void binarize_cyk(const HyperGraph& source, HyperGraph& target, const int order=0)
   {
     BinarizeCYK binarizer(order);
     
@@ -36,7 +49,7 @@ namespace cicada
   }
 
   inline
-  void binarize_cyk(HyperGraph& source, const int order=-1)
+  void binarize_cyk(HyperGraph& source, const int order=0)
   {
     HyperGraph target;
 
