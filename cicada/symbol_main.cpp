ca735580c57719f065764251c1c21fad520a9109
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-11 16:38:47 +0900

    updatd...

--- cicada/symbol_main.cpp
+++ cicada/symbol_main.cpp
@@ -6,30 +6,27 @@
 
 #include "symbol.hpp"
 
+void process(const cicada::Symbol& x, int index)
+{
+  std::cout << x << ' ' << index << std::endl
+	    << "is a non-terminal? " << x.is_non_terminal() << std::endl
+	    << "non-terminal? " << x.non_terminal() << std::endl
+	    << "non-terminal index? " << x.non_terminal_index() << std::endl
+	    << "non-terminal index? " << x.non_terminal(index) << std::endl
+	    << "non-terminal strip? " << x.non_terminal_strip() << std::endl;
+}
+
 int main(int argc, char** argv)
 {
   typedef cicada::Symbol symbol_type;
-  
-  std::cout << symbol_type("good") << " is a non-terminal? " << symbol_type("good").is_non_terminal() << std::endl;
 
-  std::cout << symbol_type("[,]") << " is a non-terminal? " << symbol_type("[,]").is_non_terminal() << std::endl;
-  
-  std::cout << symbol_type("[x]") << " is a non-terminal? " << symbol_type("[x]").is_non_terminal() << std::endl;
-
-  std::cout << symbol_type("[x]") << " non-terminal-index? " << symbol_type("[x]").non_terminal_index() << std::endl;
-
-  std::cout << symbol_type("[x,3]") << " non-terminal-index? " << symbol_type("[x,3]").non_terminal_index() << std::endl;
-  std::cout << symbol_type("[x,3]") << " non-terminal-index? " << symbol_type("[x,3]").non_terminal_index() << std::endl;
-  std::cout << symbol_type("[x,5]") << " non-terminal-index? " << symbol_type("[x,5]").non_terminal_index() << std::endl;
-  std::cout << symbol_type("[x,3]") << " non-terminal-index? " << symbol_type("[x,3]").non_terminal(4) << std::endl;
-  std::cout << symbol_type("[x,3]") << " non-terminal-index? " << symbol_type("[x,3]").non_terminal(5) << std::endl;
-  std::cout << symbol_type("[x,3]") << " non-terminal-index? " << symbol_type("[x,3]").non_terminal(0) << std::endl;
-  std::cout << symbol_type("[x,3]") << " non-terminal-strip? " << symbol_type("[x,3]").non_terminal(5).non_terminal_strip() << std::endl;
-  
-  std::cout << symbol_type("[x,3]") << " non-terminal? " << symbol_type("[x,3]").non_terminal() << std::endl;
-  std::cout << symbol_type("[x]") << " non-terminal? " << symbol_type("[x]").non_terminal() << std::endl;
-
-  std::cout << symbol_type("[x,3g]") << " non-terminal-index? " << symbol_type("[x,3g]").non_terminal_index() << std::endl;
-  std::cout << symbol_type("[x,3g]") << " non-terminal-index? " << symbol_type("[x,3g]").non_terminal() << std::endl;
+  process("good", 1);
+  process("[,]", 2);
+  process("[x]", 1);
+  process("[x]", 2);
+  process("[x,3]", 2);
+  process("[x,3]", 3);
+  process("[x,4]", 3);
+  process("[x,5g]", 4);
 
 }
