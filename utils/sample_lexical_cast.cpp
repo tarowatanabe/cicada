808fc3aa2c6974f7100944c13265960c2428fae5
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-14 11:51:47 +0900

    updated sample to measure "speed"

--- utils/sample_lexical_cast.cpp
+++ utils/sample_lexical_cast.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 #include "utils/lexical_cast.hpp"
+#include "utils/resource.hpp"
+
+#include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
@@ -34,4 +38,83 @@ int main(int argc, char** argv)
   std::cout << utils::lexical_cast<std::string>(1e-60) << std::endl;
   std::cout << utils::lexical_cast<std::string>(std::numeric_limits<double>::infinity()) << std::endl;
   std::cout << utils::lexical_cast<std::string>(utils::lexical_cast<double>("nan")) << std::endl;
+
+  ::srandom(time(0));
+  
+  {
+    utils::resource boost_start;
+    
+    for (int i = 0; i != 1024 * 64; ++ i)
+      boost::lexical_cast<int>(boost::lexical_cast<std::string>(random()));
+
+    utils::resource boost_end;
+
+    utils::resource utils_start;
+
+    for (int i = 0; i != 1024 * 64; ++ i)
+      utils::lexical_cast<int>(utils::lexical_cast<std::string>(random()));
+
+    utils::resource utils_end;
+
+    utils::resource std_start;
+    char buffer[256];
+    int integer;
+    
+    for (int i = 0; i != 1024 * 64; ++ i) {
+      sprintf(buffer, "%d", random());
+      sscanf(buffer, "%d", &integer);
+    }
+    
+    utils::resource std_end;
+
+
+    std::cout << "boost cpu time: " << (boost_end.cpu_time() - boost_start.cpu_time())
+	      << " user time: " << (boost_end.user_time() - boost_start.user_time())
+	      << std::endl;
+    std::cout << "utils cpu time: " << (utils_end.cpu_time() - utils_start.cpu_time())
+	      << " user time: " << (utils_end.user_time() - utils_start.user_time())
+	      << std::endl;
+    std::cout << "std cpu time: " << (std_end.cpu_time() - std_start.cpu_time())
+	      << " user time: " << (std_end.user_time() - std_start.user_time())
+	      << std::endl;
+  }
+
+  {
+    utils::resource boost_start;
+    
+    for (int i = 0; i != 1024 * 64; ++ i)
+      boost::lexical_cast<double>(boost::lexical_cast<std::string>(random() / random()));
+    
+    utils::resource boost_end;
+    
+    utils::resource utils_start;
+    
+    for (int i = 0; i != 1024 * 64; ++ i)
+      utils::lexical_cast<double>(utils::lexical_cast<std::string>(random() / random()));
+    
+    utils::resource utils_end;
+    
+    utils::resource std_start;
+    char buffer[256];
+    double integer;
+    
+    for (int i = 0; i != 1024 * 64; ++ i) {
+      sprintf(buffer, "%g", random() / random());
+      sscanf(buffer, "%g", &integer);
+    }
+    
+    utils::resource std_end;
+
+    
+    std::cout << "boost cpu time: " << (boost_end.cpu_time() - boost_start.cpu_time())
+	      << " user time: " << (boost_end.user_time() - boost_start.user_time())
+	      << std::endl;
+    std::cout << "utils cpu time: " << (utils_end.cpu_time() - utils_start.cpu_time())
+	      << " user time: " << (utils_end.user_time() - utils_start.user_time())
+	      << std::endl;
+    std::cout << "std cpu time: " << (std_end.cpu_time() - std_start.cpu_time())
+	      << " user time: " << (std_end.user_time() - std_start.user_time())
+	      << std::endl;
+  }
+
 }
