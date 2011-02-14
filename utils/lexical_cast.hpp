f4c3508d4095e139e917374a318136888227d5e1
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-14 13:31:49 +0900

    better lexical-cast implementation

--- utils/lexical_cast.hpp
+++ utils/lexical_cast.hpp
@@ -124,248 +124,95 @@ namespace utils
     {
       typedef const T * type;
     };
-  
-    template <typename Target, typename Source>
-    inline
-    Target __lexical_cast(const Source& arg)
+
+    template <typename Target, typename Source, bool TargetArithmetic, bool SourceArithmetic>
+    struct __lexical_cast
     {
-      return boost::lexical_cast<Target>(arg);
-    }
+      static inline
+      Target cast(const Source& arg)
+      {
+	return boost::lexical_cast<Target>(arg);
+      }
+    };
+
+    template <typename Target>
+    struct __lexical_cast<Target, utils::piece, true, false>
+    {
+      static inline
+      Target cast(const utils::piece& arg)
+      {
+	return __lexical_cast_parse<Target>(arg);
+      }
+    };
     
     template <>
-    inline
-    int64_t __lexical_cast<int64_t, utils::piece>(const utils::piece& arg)
+    struct __lexical_cast<bool, utils::piece, true, false>
     {
-      return __lexical_cast_parse<int64_t>(arg);
-    }
-  
-    template <>
-    inline
-    int32_t __lexical_cast<int32_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<int32_t>(arg);
-    }
-
-    template <>
-    inline
-    int16_t __lexical_cast<int16_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<int16_t>(arg);
-    }
-
-    template <>
-    inline
-    int8_t __lexical_cast<int8_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<int8_t>(arg);
-    }
-
-    template <>
-    inline
-    uint64_t __lexical_cast<uint64_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<uint64_t>(arg);
-    }
-  
-    template <>
-    inline
-    uint32_t __lexical_cast<uint32_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<uint32_t>(arg);
-    }
-
-    template <>
-    inline
-    uint16_t __lexical_cast<uint16_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<uint16_t>(arg);
-    }
-
-    template <>
-    inline
-    uint8_t __lexical_cast<uint8_t, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<uint8_t>(arg);
-    }
-  
-
-    template <>
-    inline
-    float __lexical_cast<float, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<float>(arg);
-    }
-
-    template <>
-    inline
-    double __lexical_cast<double, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<double>(arg);
-    }
-
-    template <>
-    inline
-    long double __lexical_cast<long double, utils::piece>(const utils::piece& arg)
-    {
-      return __lexical_cast_parse<long double>(arg);
-    }
+      static inline
+      bool cast(const utils::piece& arg)
+      {
+	namespace qi = boost::spirit::qi;
+	namespace standard = boost::spirit::standard;
+	namespace phoenix = boost::phoenix;
     
-    template <>
-    inline
-    std::string __lexical_cast<std::string, int64_t>(const int64_t& arg)
-    {
-      return __lexical_cast_generate<int64_t>(arg);
-    }
-  
-    template <>
-    inline
-    std::string __lexical_cast<std::string, int32_t>(const int32_t& arg)
-    {
-      return __lexical_cast_generate<int32_t>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, int16_t>(const int16_t& arg)
-    {
-      return __lexical_cast_generate<int16_t>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, int8_t>(const int8_t& arg)
-    {
-      return __lexical_cast_generate<int8_t>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, uint64_t>(const uint64_t& arg)
-    {
-      return __lexical_cast_generate<uint64_t>(arg);
-    }
-  
-    template <>
-    inline
-    std::string __lexical_cast<std::string, uint32_t>(const uint32_t& arg)
-    {
-      return __lexical_cast_generate<uint32_t>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, uint16_t>(const uint16_t& arg)
-    {
-      return __lexical_cast_generate<uint16_t>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, uint8_t>(const uint8_t& arg)
-    {
-      return __lexical_cast_generate<uint8_t>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, float>(const float& arg)
-    {
-      return __lexical_cast_generate<float>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, double>(const double& arg)
-    {
-      return __lexical_cast_generate<double>(arg);
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, long double>(const long double& arg)
-    {
-      return __lexical_cast_generate<long double>(arg);
-    }
-  
-    template <>
-    inline
-    bool __lexical_cast<bool, utils::piece>(const utils::piece& arg)
-    {
-      namespace qi = boost::spirit::qi;
-      namespace standard = boost::spirit::standard;
-      namespace phoenix = boost::phoenix;
+	utils::piece::const_iterator iter = arg.begin();
+	utils::piece::const_iterator iter_end = arg.end();
+	
+	bool parsed = false;
+	const bool result = qi::phrase_parse(iter, iter_end,
+					     qi::no_case["true"] [phoenix::ref(parsed) = true] 
+					     || qi::no_case["yes"] [phoenix::ref(parsed) = true] 
+					     || qi::no_case["no"] [phoenix::ref(parsed) = false] 
+					     || qi::no_case["nil"] [phoenix::ref(parsed) = false] 
+					     || qi::int_ [phoenix::ref(parsed) = (qi::_1 > 0)],
+					     standard::space);
+	
+	return result && iter == iter_end && parsed;
+      }
+    };
     
-      utils::piece::const_iterator iter = arg.begin();
-      utils::piece::const_iterator iter_end = arg.end();
-    
-      bool parsed = false;
-      const bool result = qi::phrase_parse(iter, iter_end,
-					   qi::no_case["true"] [phoenix::ref(parsed) = true] 
-					   || qi::no_case["yes"] [phoenix::ref(parsed) = true] 
-					   || qi::no_case["no"] [phoenix::ref(parsed) = false] 
-					   || qi::no_case["nil"] [phoenix::ref(parsed) = false] 
-					   || qi::int_ [phoenix::ref(parsed) = (qi::_1 > 0)],
-					   standard::space);
-    
-      return result && iter == iter_end && parsed;
-    }
-
-    template <>
-    inline
-    std::string __lexical_cast<std::string, bool>(const bool& arg)
-    {
-      return (arg ? "true" : "false");
-    }
-
-#define __LEXICAL_CAST_PARSE__(trg, src)		\
-    template <>						\
-    inline						\
-    trg __lexical_cast<trg, src>(const src& arg)	\
-    {							\
-      return __lexical_cast<trg, utils::piece>(arg);	\
-    } 
-
-  
     typedef const char* __lexical_cast_char_pointer;
 
-    __LEXICAL_CAST_PARSE__(bool, std::string)
-    __LEXICAL_CAST_PARSE__(bool, __lexical_cast_char_pointer)
+    template <typename Target>
+    struct __lexical_cast<Target, __lexical_cast_char_pointer, true, false>
+    {
+      static inline
+      Target cast(const __lexical_cast_char_pointer& arg)
+      {
+	return __lexical_cast<Target, utils::piece, true, false>::cast(arg);
+      }
+    };
 
-    __LEXICAL_CAST_PARSE__(int64_t, std::string)
-    __LEXICAL_CAST_PARSE__(int64_t, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(int32_t, std::string)
-    __LEXICAL_CAST_PARSE__(int32_t, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(int16_t, std::string)
-    __LEXICAL_CAST_PARSE__(int16_t, __lexical_cast_char_pointer)
-  
-    __LEXICAL_CAST_PARSE__(int8_t, std::string)
-    __LEXICAL_CAST_PARSE__(int8_t, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(uint64_t, std::string)
-    __LEXICAL_CAST_PARSE__(uint64_t, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(uint32_t, std::string)
-    __LEXICAL_CAST_PARSE__(uint32_t, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(uint16_t, std::string)
-    __LEXICAL_CAST_PARSE__(uint16_t, __lexical_cast_char_pointer)
-  
-    __LEXICAL_CAST_PARSE__(uint8_t, std::string)
-    __LEXICAL_CAST_PARSE__(uint8_t, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(float, std::string)
-    __LEXICAL_CAST_PARSE__(float, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(double, std::string)
-    __LEXICAL_CAST_PARSE__(double, __lexical_cast_char_pointer)
-
-    __LEXICAL_CAST_PARSE__(long double, std::string)
-    __LEXICAL_CAST_PARSE__(long double, __lexical_cast_char_pointer)
+    template <typename Target>
+    struct __lexical_cast<Target, std::string, true, false>
+    {
+      static inline
+      Target cast(const std::string& arg)
+      {
+	return __lexical_cast<Target, utils::piece, true, false>::cast(arg);
+      }
+    };
     
+    template <typename Source>
+    struct __lexical_cast<std::string, Source, false, true>
+    {
+      static inline
+      std::string cast(const Source& arg)
+      {
+	return __lexical_cast_generate<Source>(arg);
+      }
+    };
 
-#undef __LEXICAL_CAST_PARSE__
+    template <>
+    struct __lexical_cast<std::string, bool, false, true>
+    {
+      static inline
+      std::string cast(const bool& arg)
+      {
+	return (arg ? "true" : "false");
+      }
+    };
+    
   };
   
   template <typename Target, typename Source>
@@ -374,7 +221,7 @@ namespace utils
   {
     typedef typename impl::__lexical_cast_array_to_pointer_decay<Source>::type src;
     
-    return impl::__lexical_cast<Target, src>(arg);
+    return impl::__lexical_cast<Target, src, boost::is_arithmetic<Target>::value, boost::is_arithmetic<Source>::value>::cast(arg);
   }
 
 };
