aa446f27045cf42c36af56290bf88b4ba01926fe
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-16 15:50:11 +0900

    revise the order of checking paths

--- scripts/cicada-extract.py
+++ scripts/cicada-extract.py
@@ -238,16 +238,15 @@ class MPI:
             self.hosts_file = os.path.realpath(hosts_file)
 
         self.bindir = self.dir
-        if self.bindir:
-            self.bindir = os.path.join(self.bindir, 'bin')
-            if not os.path.exists(self.bindir) or not os.path.isdir(self.bindir):
-                raise ValueError, self.bindir + " does not exist"
 	
         for binprog in ['mpirun']:
             if self.bindir:
-                prog = os.path.join(self.bindir, binprog)
+                prog = os.path.join(self.bindir, 'bin', binprog)
                 if not os.path.exists(prog):
-                    raise ValueError, prog + " does not exist at " + self.bindir
+                    prog = os.path.join(self.bindir, binprog)
+                    if not os.path.exists(prog):
+                        raise ValueError, prog + " does not exist at " + self.bindir
+                    
                 setattr(self, binprog, prog)
             else:
                 setattr(self, binprog, binprog)
@@ -283,9 +282,7 @@ class Toolkit:
 	    bindir = os.path.join(self.dir, dir)
 	    if os.path.exists(bindir) and os.path.isdir(bindir):
 		self.bindirs.append(bindir)
-
-	if not self.bindirs:
-	    raise ValueError, str(self.bindirs) + "  does not exist"
+        self.bindirs.append(self.dir)
 	
         for binprog in ('cicada_alignment',
                         ## step 4
