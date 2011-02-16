93a819827a82179661296e0c2f2d9ed4bc12d487
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-16 11:17:24 +0900

    make sure to erase all the files...

--- progs/cicada_mpi.cpp
+++ progs/cicada_mpi.cpp
@@ -170,7 +170,8 @@ int main(int argc, char ** argv)
       
       boost::filesystem::directory_iterator iter_end;
       for (boost::filesystem::directory_iterator iter(directory); iter != iter_end; ++ iter)
-	utils::filesystem::remove_all(*iter);
+	while (boost::filesystem::exists(*iter))
+	  utils::filesystem::remove_all(*iter);
       
       ::sync();
     }
