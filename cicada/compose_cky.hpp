3f1ff080f55b28229bd28e88d315cd2748b55297
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-02-27 16:16:44 +0900

    revert back to the original implementation...

--- cicada/compose_cky.hpp
+++ cicada/compose_cky.hpp
@@ -6,9 +6,9 @@
 #ifndef __CICADA__COMPOSE_CKY__HPP__
 #define __CICADA__COMPOSE_CKY__HPP__ 1
 
-#include <deque>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 #include <cicada/symbol.hpp>
 #include <cicada/vocab.hpp>
@@ -20,6 +20,8 @@
 #include <utils/chunk_vector.hpp>
 #include <utils/chart.hpp>
 #include <utils/hashmurmur.hpp>
+#include <utils/sgi_hash_set.hpp>
+#include <utils/sgi_hash_map.hpp>
 
 #include <google/dense_hash_map>
 #include <google/dense_hash_set>
@@ -29,6 +31,9 @@ namespace cicada
   
   struct ComposeCKY
   {
+    typedef size_t    size_type;
+    typedef ptrdiff_t difference_type;
+
     typedef Symbol symbol_type;
     typedef Vocab  vocab_type;
 
@@ -53,7 +58,7 @@ namespace cicada
     {
       goal_rule = rule_type::create(rule_type(vocab_type::GOAL, rule_type::symbol_set_type(1, goal.non_terminal())));
       
-      node_map.set_empty_key(symbol_index_type());
+      node_map.set_empty_key(symbol_level_type());
       closure.set_empty_key(symbol_type());
       closure_head.set_empty_key(symbol_type());
       closure_tail.set_empty_key(symbol_type());
@@ -110,14 +115,101 @@ namespace cicada
     typedef std::vector<passive_type, std::allocator<passive_type> > passive_set_type;
     typedef utils::chart<passive_set_type, std::allocator<passive_set_type> > passive_chart_type;
 
-    typedef std::pair<symbol_type, int> symbol_index_type;
-    typedef google::dense_hash_map<symbol_index_type, hypergraph_type::id_type, utils::hashmurmur<size_t>, std::equal_to<symbol_index_type> > node_map_type;
+    typedef std::pair<symbol_type, int> symbol_level_type;
+    
+    struct symbol_level_hash : public utils::hashmurmur<size_t>
+    {
+      typedef utils::hashmurmur<size_t> hasher_type;
+      
+      size_t operator()(const symbol_level_type& x) const
+      {
+	return hasher_type::operator()(x.first, x.second);
+      }
+    };
+
+    typedef google::dense_hash_map<symbol_level_type, hypergraph_type::id_type, symbol_level_hash, std::equal_to<symbol_level_type> > node_map_type;
     
     typedef google::dense_hash_map<symbol_type, int, boost::hash<symbol_type>, std::equal_to<symbol_type> > closure_level_type;
     typedef google::dense_hash_set<symbol_type, boost::hash<symbol_type>, std::equal_to<symbol_type> > closure_type;
 
+    struct transducer_rule_type
+    {
+      size_type table;
+      transducer_type::id_type node;
+      size_type pos;
+
+      transducer_rule_type(const size_type& __table, const transducer_type::id_type& __node, const size_type& __pos)
+	: table(__table), node(__node), pos(__pos) {}
+      transducer_rule_type()
+	: table(), node(), pos() {}
+      
+      friend
+      size_t hash_value(transducer_rule_type const& x)
+      {
+	return utils::hashmurmur<size_t>()(x);
+      }
+
+      friend
+      bool operator==(const transducer_rule_type& x, const transducer_rule_type& y)
+      {
+	return x.table == y.table && x.node == y.node && x.pos == y.pos;
+      }
+
+      friend
+      bool operator<(const transducer_rule_type& x, const transducer_rule_type& y)
+      {
+	return (x.table < y.table || (x.table == y.table && (x.node < y.node || (x.node == y.node && x.pos < y.pos))));
+      }
+    };
+    
+#ifdef HAVE_TR1_UNORDERED_SET
+    typedef std::tr1::unordered_set<transducer_rule_type, boost::hash<transducer_rule_type>, std::equal_to<transducer_rule_type>,
+				    std::allocator<transducer_rule_type> > transducer_rule_set_type;
+    typedef std::tr1::unordered_set<symbol_level_type, symbol_level_hash, std::equal_to<symbol_level_type>,
+				    std::allocator<symbol_level_type> > symbol_level_set_type;
+#else
+    typedef sgi::hash_set<transducer_rule_type, boost::hash<transducer_rule_type>, std::equal_to<transducer_rule_type>,
+			  std::allocator<transducer_rule_type> > transducer_rule_set_type;
+    typedef sgi::hash_set<symbol_level_type, symbol_level_hash, std::equal_to<symbol_level_type>,
+			  std::allocator<symbol_level_type> > symbol_level_set_type;
+#endif
+
+#ifdef HAVE_TR1_UNORDERED_MAP
+    typedef std::tr1::unordered_map<symbol_level_type, transducer_rule_set_type, symbol_level_hash, std::equal_to<symbol_level_type>,
+				    std::allocator<std::pair<const symbol_level_type, transducer_rule_set_type> > > unary_set_type;
+    typedef std::tr1::unordered_map<symbol_level_type, unary_set_type, symbol_level_hash, std::equal_to<symbol_level_type>,
+				    std::allocator<std::pair<const symbol_level_type, unary_set_type> > > unary_map_type; 
+#else
+    typedef sgi::hash_map<symbol_level_type, transducer_rule_set_type, symbol_level_hash, std::equal_to<symbol_level_type>,
+			  std::allocator<std::pair<const symbol_level_type, transducer_rule_set_type> > > unary_set_type;
+    typedef sgi::hash_map<symbol_level_type, unary_set_type, symbol_level_hash, std::equal_to<symbol_level_type>,
+			  std::allocator<std::pair<const symbol_level_type, unary_set_type> > > unary_map_type; 
+#endif
+
+    typedef std::pair<symbol_level_type, symbol_level_type> symbol_level_pair_type;
+    
+    struct symbol_level_pair_hash : public utils::hashmurmur<size_t>
+    {
+      typedef utils::hashmurmur<size_t> hasher_type;
+      
+      size_t operator()(const symbol_level_pair_type& x) const
+      {
+	return hasher_type::operator()(x);
+      }
+    };
+
+#ifdef HAVE_TR1_UNORDERED_SET
+    typedef std::tr1::unordered_set<symbol_level_pair_type, symbol_level_pair_hash, std::equal_to<symbol_level_pair_type>,
+				    std::allocator<symbol_level_pair_type> > symbol_level_pair_set_type;
+#else
+    typedef sgi::hash_set<symbol_level_pair_type, symbol_level_pair_hash, std::equal_to<symbol_level_pair_type>,
+			  std::allocator<symbol_level_pair_type> > symbol_level_pair_set_type;
+#endif
+  
     typedef std::vector<symbol_type, std::allocator<symbol_type> > non_terminal_set_type;
 
+    
+    
     struct less_non_terminal
     {
       less_non_terminal(const non_terminal_set_type& __non_terminals) : non_terminals(__non_terminals) {}
@@ -155,7 +247,6 @@ namespace cicada
 	  if (grammar[table].valid_span(pos, pos, 0))
 	    actives[table](pos, pos).push_back(active_type(root));
       }
-
       
       for (size_t length = 1; length <= lattice.size(); ++ length)
 	for (size_t first = 0; first + length <= lattice.size(); ++ first) {
@@ -238,7 +329,79 @@ namespace cicada
 	  // handle unary rules...
 	  // order is very important...
 	  // actually, we need to loop-forever...
+	  
+#if 0
+	  if (! passives(first, last).empty()) {
+	    typedef std::pair<symbol_level_type, hypergraph_type::id_type> symbol_node_type;
+	    typedef std::vector<symbol_node_type, std::allocator<symbol_node_type> > symbol_node_set_type;
 
+	    passive_set_type& passive_arcs = passives(first, last);
+
+	    symbol_node_set_type symbol_nodes;
+	    symbol_node_set_type symbol_nodes_next;
+	    
+	    passive_set_type::const_iterator piter_end = passive_arcs.end();
+	    for (passive_set_type::const_iterator piter = passive_arcs.begin(); piter != piter_end; ++ piter) {
+	      unary_closure(non_terminals[*piter]);
+	      symbol_nodes.push_back(std::make_pair(symbol_level_type(non_terminals[*piter], 0), *piter));
+
+	      //std::cerr << "passive arc: " << *piter << std::endl;
+	    }
+	    
+	    // we will collect edges from unary_map
+	    symbol_level_pair_set_type edges;
+	    
+	    while (! symbol_nodes.empty()) {
+	      symbol_nodes_next.clear();
+	      
+	      symbol_node_set_type::const_iterator piter_end = symbol_nodes.end();
+	      for (symbol_node_set_type::const_iterator piter = symbol_nodes.begin(); piter != piter_end; ++ piter) {
+		unary_map_type::const_iterator uiter = unary_map.find(piter->first);
+		if (uiter == unary_map.end()) {
+		  //std::cerr << "no entry? " << piter->first.first << ':' << piter->first.second << std::endl;
+		  continue;
+		}
+		
+		unary_set_type::const_iterator niter_end = uiter->second.end();
+		for (unary_set_type::const_iterator niter = uiter->second.begin(); niter != niter_end; ++ niter)
+		  if (edges.find(std::make_pair(piter->first, niter->first)) == edges.end()) {
+		    edges.insert(std::make_pair(piter->first, niter->first));
+		    
+#if 0
+		    std::cerr << "construct: " << piter->first.first << ':' << piter->first.second
+			      << " next: " << niter->first.first << ':' << niter->first.second
+			      << std::endl;
+#endif
+		    
+		    transducer_rule_set_type::const_iterator riter_end = niter->second.end();
+		    for (transducer_rule_set_type::const_iterator riter = niter->second.begin(); riter != riter_end; ++ riter) {
+		      const transducer_type& transducer = grammar[riter->table];
+		      const transducer_type::id_type& node = riter->node;
+		      
+		      const transducer_type::rule_pair_set_type& rules = transducer.rules(node);
+		      const transducer_type::rule_pair_type& rule_pair = rules[riter->pos];
+		      
+		      const rule_ptr_type rule = (yield_source ? rule_pair.source : rule_pair.target);
+		      
+		      const size_type passive_size_prev = passive_arcs.size();
+		      
+		      apply_rule(rule, rule_pair.features, rule_pair.attributes,
+				 &(piter->second), &(piter->second) + 1, node_map, passive_arcs, graph,
+				 first, last, niter->first.second);
+		      
+		      if (passive_size_prev != passive_arcs.size())
+			symbol_nodes_next.push_back(std::make_pair(niter->first, passive_arcs.back()));
+		    }
+		  }
+	      }
+	      
+	      symbol_nodes.clear();
+	      symbol_nodes.swap(symbol_nodes_next);
+	    }
+	  }
+#endif
+	  
+#if 1
 	  if (! passives(first, last).empty()) {
 	    //std::cerr << "closure from passives: " << passives(first, last).size() << std::endl;
 
@@ -315,9 +478,10 @@ namespace cicada
 	      else
 		++ closure_loop;
 	      
-	      if (closure_loop == 4) break;
+	      if (closure_loop == 1) break;
 	    }
 	  }
+#endif
 	  
 	  // sort passives at passives(first, last) wrt non-terminal label in non_terminals
 	  std::sort(passives(first, last).begin(), passives(first, last).end(), less_non_terminal(non_terminals));
@@ -458,7 +622,100 @@ namespace cicada
       return found;
     }
 
+    void unary_closure(const symbol_type& non_terminal)
+    {
+      if (unary_map.find(symbol_level_type(non_terminal, 0)) != unary_map.end()) return;
 
+      //std::cerr << "unary closure: " << non_terminal << std::endl;
+      
+      symbol_level_set_type nodes;
+      symbol_level_set_type nodes_all;
+      symbol_level_set_type nodes_next;
+
+      unary_map[symbol_level_type(non_terminal, 0)];
+      
+      closure.clear();
+      closure[non_terminal] = 0;
+      
+      nodes.insert(symbol_level_type(non_terminal, 0));
+      nodes_all.insert(symbol_level_type(non_terminal, 0));
+      
+      int  closure_loop = 0;
+      for (;;) {
+	const size_t closure_size = closure.size();
+	
+	closure_head.clear();
+	closure_tail.clear();
+
+	nodes_next.clear();
+	
+	for (size_t table = 0; table != grammar.size(); ++ table) {
+	  const transducer_type& transducer = grammar[table];
+	  
+	  symbol_level_set_type::const_iterator piter_end = nodes.end();
+	  for (symbol_level_set_type::const_iterator piter = nodes.begin(); piter != piter_end; ++ piter) {
+	    const symbol_type& non_terminal = piter->first;
+	    
+	    const transducer_type::id_type node = transducer.next(transducer.root(), non_terminal);
+	    if (node == transducer.root()) continue;
+	    
+	    const transducer_type::rule_pair_set_type& rules = transducer.rules(node);
+	    
+	    if (rules.empty()) continue;
+
+	    //std::cerr << "non-terminal: " << non_terminal << std::endl;
+	    
+	    closure_tail.insert(non_terminal);
+	    
+	    size_type pos = 0;
+	    transducer_type::rule_pair_set_type::const_iterator riter_end = rules.end();
+	    for (transducer_type::rule_pair_set_type::const_iterator riter = rules.begin(); riter != riter_end; ++ riter, ++ pos) {
+	      const rule_ptr_type rule = (yield_source ? riter->source : riter->target);
+	      const symbol_type& lhs = rule->lhs;
+	      
+	      closure_level_type::const_iterator citer = closure.find(lhs);
+	      const int level = (citer != closure.end() ? citer->second : 0);
+	      
+	      // we will assign (lhs, level + 1)
+	      
+	      closure_head.insert(lhs);
+	      
+	      unary_map[*piter][symbol_level_type(lhs, level + 1)].insert(transducer_rule_type(table, node, pos));
+	      
+	      if (nodes_all.find(symbol_level_type(lhs, level + 1)) == nodes_all.end()) {
+		nodes_all.insert(symbol_level_type(lhs, level + 1));
+		nodes_next.insert(symbol_level_type(lhs, level + 1));
+
+		//std::cerr << "prev: " << non_terminal << ':' << piter->second << " next: " << lhs << ':' << (level + 1) << std::endl;
+	      }
+	    }
+	  }
+	}
+	
+	if (nodes_next.empty()) break;
+	
+	nodes.clear();
+	nodes.swap(nodes_next);
+	
+	// we use level-one, that is the label assigned for new-lhs!
+	closure_type::const_iterator hiter_end = closure_head.end();
+	for (closure_type::const_iterator hiter = closure_head.begin(); hiter != hiter_end; ++ hiter)
+	  closure.insert(std::make_pair(*hiter, 1));
+	
+	// increment non-terminal level when used as tails...
+	closure_type::const_iterator titer_end = closure_tail.end();
+	for (closure_type::const_iterator titer = closure_tail.begin(); titer != titer_end; ++ titer)
+	  ++ closure[*titer];
+	
+	if (closure_size != closure.size())
+	  closure_loop = 0;
+	else
+	  ++ closure_loop;
+	
+	if (closure_loop == 4) break;
+      }
+      
+    }
     
   private:
     const symbol_type goal;
@@ -478,6 +735,8 @@ namespace cicada
     closure_type          closure_head;
     closure_type          closure_tail;
     non_terminal_set_type non_terminals;
+
+    unary_map_type unary_map;
   };
   
   inline
