261cc83ddde73712df6cf1bf041ecd13afa0a382
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-03-07 09:26:36 +0900

    use of bithack branch

--- cicada/semiring/envelope.cpp
+++ cicada/semiring/envelope.cpp
@@ -4,6 +4,8 @@
 
 #include "cicada/semiring/envelope.hpp"
 
+#include "utils/bithack.hpp"
+
 namespace cicada
 {
   namespace semiring
@@ -186,9 +188,8 @@ namespace cicada
       rule_type::symbol_set_type::const_iterator titer_end = phrase.end();
       for (rule_type::symbol_set_type::const_iterator titer = phrase.begin(); titer != titer_end; ++ titer)
 	if (titer->is_non_terminal()) {
-	  int pos = titer->non_terminal_index() - 1;
-	  if (pos < 0)
-	    pos = pos_non_terminal;
+	  const int __non_terminal_index = titer->non_terminal_index();
+	  const int pos = utils::bithack::branch(__non_terminal_index <= 0 ? pos_non_terminal : __non_terminal_index - 1);
 	  
 	  const sentence_type& antecedent = *(yields.rbegin() + pos);
 	  
