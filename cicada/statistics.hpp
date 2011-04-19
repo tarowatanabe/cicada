a2900bb859d165c917ae9f129791e140ab6710f4
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-04-19 13:24:28 +0900

    updated doc and started implementing statistics

---
+++ cicada/statistics.hpp
@@ -0,0 +1,18 @@
+// -*- mode: c++ -*-
+//
+//  Copyright(C) 2011 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+#ifndef __CICADA__STATISTICS__HPP__
+#define __CICADA__STATISTICS__HPP__ 1
+
+namespace cicada
+{
+  struct Statistics
+  {
+    
+    
+  };
+};
+
+#endif
