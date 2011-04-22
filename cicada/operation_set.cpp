a6b2fc7616a7f21fea96a41f27eb80b557ecdc34
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-04-22 12:55:52 +0900

    register parse-tree as an operation

--- cicada/operation_set.cpp
+++ cicada/operation_set.cpp
@@ -154,6 +154,14 @@ parse-coarse: parsing via coarse-to-fine\n\
 \tcoarse1=[coarse grammar spec] the second coarse grammar\n\
 \tthreshold1=[double] threshold for the second coarse grammar\n\
 \t...\n\
+parse-tree: parsing for tree-matching\n\
+\tyield=[source|target] use source or target yield for rule\n\
+\tgoal=[goal symbol]\n\
+\tgrammar=[grammar spec] grammar\n\
+\ttree-grammar=[grammar spec] tree grammar\n\
+\tsize=<beam size>\n\
+\tweights=weight file for feature\n\
+\tweights-one=[true|false] one initialized weight\n\
 permute: permute tree (monolingual tree only)\n\
 \tsize=permute size\n\
 \texclude=[a non-terminal] to prohibit permutation. You can supply multiple\n\
@@ -244,6 +252,8 @@ span-forest: annotate terminal span\n\
 	operations.push_back(operation_ptr_type(new operation::ParseCKY(*piter, grammar, goal, debug)));
       else if (param_name == "parse-coarse")
 	operations.push_back(operation_ptr_type(new operation::ParseCoarse(*piter, grammar, goal, debug)));
+      else if (param_name == "parse-tree")
+	operations.push_back(operation_ptr_type(new operation::ParseTree(*piter, tree_grammar, grammar, goal, debug)));
       else if (param_name == "generate-earley")
 	operations.push_back(operation_ptr_type(new operation::GenerateEarley(*piter, grammar, goal, debug)));
       else if (param_name == "apply")
