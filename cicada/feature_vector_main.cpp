5296672a5cd646d873a5fb1c0fa821d51eb6f0b6
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-04-24 16:07:00 +0900

    added more test case

--- cicada/feature_vector_main.cpp
+++ cicada/feature_vector_main.cpp
@@ -54,5 +54,26 @@ int main(int argc, char** argv)
   features1.erase_prefix(std::string("bad"));
   std::cout << "erased prefix for bad" << std::endl;
   std::cout << features1;
+
+  std::cout << "inserted many" << std::endl;
+  for (int i = 0; i != 32; ++ i) {
+    std::string feat = "good:" + utils::lexical_cast<std::string>(i);
+    
+    features1[feat] = i;
+  }
   
+  std::cout << "feature1" << std::endl;
+  std::cout << features1;
+  
+  std::cout << "feature1 + feature2" << std::endl;
+  std::cout << features1 + features2;
+  
+  std::cout << "feature1 * feature2" << std::endl;
+  std::cout << features1 * features2;
+  
+  std::cout << "feature1 - feature3" << std::endl;
+  std::cout << features1 - features3;
+  
+  std::cout << "feature3 - feature1" << std::endl;
+  std::cout << features3 - features1;
 }
