61e3fc5bf19db4bb22416ab31fb09bc24dac286f
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-05-17 12:39:54 +0900

    tweaked max-malloc checking frequency

--- progs/cicada_extract_phrase_impl.hpp
+++ progs/cicada_extract_phrase_impl.hpp
@@ -523,7 +523,7 @@ struct Task
     bitext_type bitext;
     phrase_pair_set_type phrase_pairs;
     
-    const int iteration_mask = (1 << 6) - 1;
+    const int iteration_mask = (1 << 3) - 1;
     const size_t malloc_threshold = size_t(max_malloc * 1024 * 1024 * 1024);
     
     for (int iter = 0;/**/; ++ iter) {
