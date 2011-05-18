d21935ba7748bc5a765e4fcfd540ba726e5d112a
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-05-18 17:04:08 +0900

    updated...

--- progs/cicada_extract_tree_impl.hpp
+++ progs/cicada_extract_tree_impl.hpp
@@ -625,8 +625,8 @@ struct ExtractTree
 		  
 		  candidate_type& item = candidates.back();
 		  
-		  item.edge_composed.edges.swap(edges_new);
-		  item.edge_composed.tails.swap(tails_new);
+		  item.edge_composed.edges = edges_new;
+		  item.edge_composed.tails = tails_new;
 		  item.edge_composed.height = rule_stat.first;
 		  item.edge_composed.internal = rule_stat.second;
 		
