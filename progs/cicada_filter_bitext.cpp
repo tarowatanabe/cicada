c9beae74f33e48d77f953d2bfe560d6e0e6ec40f
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-05-24 16:15:17 +0900

    added --list

--- progs/cicada_filter_bitext.cpp
+++ progs/cicada_filter_bitext.cpp
@@ -38,6 +38,7 @@ typedef cicada::Vocab     vocab_type;
 typedef cicada::Alignment alignment_type;
 
 typedef boost::filesystem::path path_type;
+typedef std::vector<path_type, std::allocator<path_type> > path_set_type;
 
 typedef std::vector<std::string, std::allocator<std::string> > sentence_type;
 
@@ -74,9 +75,13 @@ struct sentence_generator : boost::spirit::karma::grammar<Iterator, sentence_typ
 };
 
 
-path_type source_file = "-";
-path_type target_file = "-";
-path_type alignment_file;
+path_set_type source_files;
+path_set_type target_files;
+path_set_type alignment_files;
+
+path_type list_source_file;
+path_type list_target_file;
+path_type list_alignment_file;
 
 path_type output_source_file = "-";
 path_type output_target_file = "-";
@@ -87,108 +92,138 @@ double max_fertility = 10;
 
 bool add_bos_eos = false;
 
+void read_list(const path_type& path, path_set_type& files);
 void options(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
   try {
     options(argc, argv);
+    
+    read_list(list_source_file, source_files);
+    read_list(list_target_file, target_files);
+    read_list(list_alignment_file, alignment_files);
+    
+    const bool alignment_mode = ! alignment_files.empty();
 
-    if (! alignment_file.empty())
-      if (alignment_file != "-" && ! boost::filesystem::exists(alignment_file))
-	throw std::runtime_error("no alignment file? " + alignment_file.string());
-
-    const bool alignment_mode = ! alignment_file.empty();
+    if (source_files.size() != target_files.size())
+      throw std::runtime_error("# of files do not match");
+    if (alignment_mode) {
+      if (source_files.size() != alignment_files.size())
+	throw std::runtime_error("# of alignemnt files do not match");
+      if (target_files.size() != alignment_files.size())
+	throw std::runtime_error("# of alignemnt files do not match");
+    }
     
     const std::string& bos = static_cast<const std::string&>(vocab_type::BOS);
     const std::string& eos = static_cast<const std::string&>(vocab_type::EOS);
-    
-    utils::compress_istream is_src(source_file, 1024 * 1024);
-    utils::compress_istream is_trg(target_file, 1024 * 1024);
-    std::auto_ptr<utils::compress_istream> is_align(alignment_mode ? new utils::compress_istream(alignment_file, 1024 * 1024) : 0);
 
-    is_src.unsetf(std::ios::skipws);
-    is_trg.unsetf(std::ios::skipws);
-    
     utils::compress_ostream os_src(output_source_file, 1024 * 1024);
     utils::compress_ostream os_trg(output_target_file, 1024 * 1024);
     std::auto_ptr<utils::compress_ostream> os_align(alignment_mode ? new utils::compress_ostream(output_alignment_file, 1024 * 1024) : 0);
-    
+
     typedef boost::spirit::istream_iterator iiter_type;
     typedef std::ostream_iterator<char>     oiter_type;
     
     sentence_parser<iiter_type>    parser;
     sentence_generator<oiter_type> generator;
     
-    iiter_type siter(is_src);
-    iiter_type titer(is_trg);
-    iiter_type siter_end;
-    iiter_type titer_end;
-
-    sentence_type source;
-    sentence_type target;
-    alignment_type alignment;
-    alignment_type alignment_new;
     
-    for (size_t line_no = 0; siter != siter_end && titer != titer_end; ++ line_no) {
-      source.clear();
-      target.clear();
-      alignment.clear();
+    
+    
+    for (size_t i = 0; i != source_files.size(); ++ i) {
+      utils::compress_istream is_src(source_files[i], 1024 * 1024);
+      utils::compress_istream is_trg(target_files[i], 1024 * 1024);
+      std::auto_ptr<utils::compress_istream> is_align(alignment_mode ? new utils::compress_istream(alignment_files[i], 1024 * 1024) : 0);
       
-      if (! boost::spirit::qi::phrase_parse(siter, siter_end, parser, boost::spirit::standard::blank, source))
-	throw std::runtime_error("source sentence parsing failed at # " + utils::lexical_cast<std::string>(line_no));
-      if (! boost::spirit::qi::phrase_parse(titer, titer_end, parser, boost::spirit::standard::blank, target))
-	throw std::runtime_error("target sentence parsing failed at # " + utils::lexical_cast<std::string>(line_no));
+      is_src.unsetf(std::ios::skipws);
+      is_trg.unsetf(std::ios::skipws);
       
-      if (alignment_mode) {
-	*is_align >> alignment;
-	if (! *is_align)
-	  throw std::runtime_error("no alignment?");
+      iiter_type siter(is_src);
+      iiter_type titer(is_trg);
+      iiter_type siter_end;
+      iiter_type titer_end;
+      
+      sentence_type source;
+      sentence_type target;
+      alignment_type alignment;
+      alignment_type alignment_new;
+    
+      for (size_t line_no = 0; siter != siter_end && titer != titer_end; ++ line_no) {
+	source.clear();
+	target.clear();
+	alignment.clear();
+      
+	if (! boost::spirit::qi::phrase_parse(siter, siter_end, parser, boost::spirit::standard::blank, source))
+	  throw std::runtime_error("source sentence parsing failed at # " + utils::lexical_cast<std::string>(line_no));
+	if (! boost::spirit::qi::phrase_parse(titer, titer_end, parser, boost::spirit::standard::blank, target))
+	  throw std::runtime_error("target sentence parsing failed at # " + utils::lexical_cast<std::string>(line_no));
+      
+	if (alignment_mode) {
+	  *is_align >> alignment;
+	  if (! *is_align)
+	    throw std::runtime_error("no alignment?");
+	}
+      
+	const int source_size = source.size();
+	const int target_size = target.size();
+      
+	if (source_size == 0 || target_size == 0) continue;
+	if (max_length > 0)
+	  if (source_size > max_length || target_size > max_length) continue;
+	if (max_fertility > 0)
+	  if (double(utils::bithack::max(source_size, target_size)) / double(utils::bithack::min(source_size, target_size)) >= max_fertility) continue;
+      
+	if (add_bos_eos) {
+	  source.insert(source.begin(), bos);
+	  source.push_back(eos);
+	
+	  target.insert(target.begin(), bos);
+	  target.push_back(eos);
+	
+	  alignment_new.clear();
+	
+	  alignment_new.push_back(std::make_pair(0, 0));
+	  alignment_type::const_iterator aiter_end = alignment.end();
+	  for (alignment_type::const_iterator aiter = alignment.begin(); aiter != aiter_end; ++ aiter)
+	    alignment_new.push_back(std::make_pair(aiter->source + 1, aiter->target + 1));
+	  alignment_new.push_back(std::make_pair(source.size() - 1, target.size() - 1));
+	  
+	  alignment_new.swap(alignment);
+	}
+	
+	if (! boost::spirit::karma::generate(oiter_type(os_src), generator, source))
+	  throw std::runtime_error("source sentence generation failed at # " + utils::lexical_cast<std::string>(line_no));
+	if (! boost::spirit::karma::generate(oiter_type(os_trg), generator, target))
+	  throw std::runtime_error("target sentence generation failed at # " + utils::lexical_cast<std::string>(line_no));
+	
+	*os_align << alignment << '\n';
       }
-      
-      const int source_size = source.size();
-      const int target_size = target.size();
-      
-      if (source_size == 0 || target_size == 0) continue;
-      if (max_length > 0)
-	if (source_size > max_length || target_size > max_length) continue;
-      if (max_fertility > 0)
-	if (double(utils::bithack::max(source_size, target_size)) / double(utils::bithack::min(source_size, target_size)) >= max_fertility) continue;
-      
-      if (add_bos_eos) {
-	source.insert(source.begin(), bos);
-	source.push_back(eos);
-	
-	target.insert(target.begin(), bos);
-	target.push_back(eos);
-	
-	alignment_new.clear();
-	
-	alignment_new.push_back(std::make_pair(0, 0));
-	alignment_type::const_iterator aiter_end = alignment.end();
-	for (alignment_type::const_iterator aiter = alignment.begin(); aiter != aiter_end; ++ aiter)
-	  alignment_new.push_back(std::make_pair(aiter->source + 1, aiter->target + 1));
-	alignment_new.push_back(std::make_pair(source.size() - 1, target.size() - 1));
-	
-	alignment_new.swap(alignment);
-      }
-      
-      if (! boost::spirit::karma::generate(oiter_type(os_src), generator, source))
-	throw std::runtime_error("source sentence generation failed at # " + utils::lexical_cast<std::string>(line_no));
-      if (! boost::spirit::karma::generate(oiter_type(os_trg), generator, target))
-	throw std::runtime_error("target sentence generation failed at # " + utils::lexical_cast<std::string>(line_no));
-
-      *os_align << alignment << '\n';
+      if (siter != siter_end || titer != titer_end || (alignment_mode && *is_align >> alignment))
+	throw std::runtime_error("# of lines do not match: " + source_files[i].string() + " " + target_files[i].string());
     }
-    if (siter != siter_end || titer != titer_end || (alignment_mode && *is_align >> alignment))
-      throw std::runtime_error("# of lines do not match");
-    
   }
   catch(std::exception& err) {
     std::cerr << "error: " << err.what() << std::endl;
     return -1;
   }
   return 0;
+}
+
+void read_list(const path_type& path, path_set_type& files)
+{
+  if (path.empty()) return;
+  if (path != "-" && ! boost::filesystem::exists(path))
+    throw std::runtime_error("no file? " + path.string());
+  
+  utils::compress_istream is(path);
+  std::string file;
+  while (std::getline(is, file)) {
+    if (file.empty()) continue;
+    if (! boost::filesystem::exists(file))
+      throw std::runtime_error("no file? " + file);
+    files.push_back(file);
+  }
 }
 
 
@@ -198,9 +233,13 @@ void options(int argc, char** argv)
   
   po::options_description desc("options");
   desc.add_options()
-    ("source",    po::value<path_type>(&source_file)->default_value(source_file),       "source file")
-    ("target",    po::value<path_type>(&target_file)->default_value(target_file),       "target file")
-    ("alignment", po::value<path_type>(&alignment_file)->default_value(alignment_file), "alignment file")
+    ("source",    po::value<path_set_type>(&source_files)->multitoken(),    "source file(s)")
+    ("target",    po::value<path_set_type>(&target_files)->multitoken(),    "target file(s)")
+    ("alignment", po::value<path_set_type>(&alignment_files)->multitoken(), "alignment file(s)")
+
+    ("list-source",    po::value<path_type>(&list_source_file),    "source list file")
+    ("list-target",    po::value<path_type>(&list_target_file),    "target list file")
+    ("list-alignment", po::value<path_type>(&list_alignment_file), "alignment list file")
 
     ("output-source",    po::value<path_type>(&output_source_file)->default_value(output_source_file),       "output source file")
     ("output-target",    po::value<path_type>(&output_target_file)->default_value(output_target_file),       "output target file")
