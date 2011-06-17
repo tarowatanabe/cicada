383bbedc68497f0375a3268d2137c3771fa1ccbf
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-06-17 13:32:10 +0900

    added...

--- progs/cicada_oracle_kbest_mpi.cpp
+++ progs/cicada_oracle_kbest_mpi.cpp
@@ -1,6 +1,8 @@
 //
-//  Copyright(C) 2010-2011 Taro Watanabe <taro.watanabe@nict.go.jp>
+//  Copyright(C) 2011 Taro Watanabe <taro.watanabe@nict.go.jp>
 //
+
+// kbest variant of forest-oracle computer
 
 //
 // refset format:
