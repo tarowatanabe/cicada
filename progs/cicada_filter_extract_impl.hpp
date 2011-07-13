20052b512c77f5fa04f283e048f785a81c7bfcbf
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-07-13 11:52:22 +0900

    implemented three lexicon model related scoring

--- progs/cicada_filter_extract_impl.hpp
+++ progs/cicada_filter_extract_impl.hpp
@@ -11,13 +11,24 @@
 
 #include <boost/fusion/tuple.hpp>
 #include <boost/fusion/adapted.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/math/special_functions/expm1.hpp>
 
 #include <string>
 #include <vector>
 #include <iostream>
 
 #include <utils/hashmurmur.hpp>
+#include <utils/alloc_vector.hpp>
+#include <utils/compress_stream.hpp>
+#include <utils/mathop.hpp>
 
+#include <cicada/symbol.hpp>
+#include <cicada/vocab.hpp>
+#include <cicada/sentence.hpp>
+#include <cicada/tree_rule.hpp>
+
+#include <google/dense_hash_map>
 
 struct RootCount
 {
@@ -419,6 +430,262 @@ struct ExtractRootGHKM
       throw std::runtime_error("no label?");
     
     return label;
+  }
+};
+
+struct LexiconModel
+{
+  typedef cicada::Symbol word_type;
+  typedef cicada::Vocab vocab_type;
+
+  typedef boost::filesystem::path path_type;
+
+  struct table_type
+  {
+    typedef google::dense_hash_map<word_type, double, boost::hash<word_type> , std::equal_to<word_type> > __table_type;
+
+    table_type() : table() { table.set_empty_key(word_type()); }
+
+    __table_type table;
+  };
+
+  typedef utils::alloc_vector<table_type, std::allocator<table_type> > table_set_type;
+
+  LexiconModel() : smooth(1e-7), tables() {}
+  LexiconModel(const path_type& path) : smooth(), tables() { open(path); }
+
+  void open(const path_type& path)
+  {
+    namespace qi = boost::spirit::qi;
+    namespace standard = boost::spirit::standard;
+    
+    typedef boost::fusion::tuple<std::string, std::string, double> parsed_type;
+
+    qi::rule<std::string::const_iterator, std::string(), standard::space_type> word;
+    qi::rule<std::string::const_iterator, parsed_type(), standard::space_type> lexicon;
+
+    word %= qi::lexeme[+(standard::char_ - standard::space)];
+    lexicon %= word >> word >> qi::double_;
+
+    smooth = std::numeric_limits<double>::infinity();
+    tables.clear();
+
+    utils::compress_istream is(path, 1024 * 1024);
+    std::string line;
+    parsed_type parsed;
+
+    while (std::getline(is, line)) {
+      std::string::const_iterator iter = line.begin();
+      std::string::const_iterator end = line.end();
+
+      boost::fusion::get<0>(parsed).clear();
+      boost::fusion::get<1>(parsed).clear();
+
+      const bool result = qi::phrase_parse(iter, end, lexicon, standard::space, parsed);
+      if (! result || iter != end) continue;
+
+
+      tables[word_type(boost::fusion::get<1>(parsed)).id()].table[boost::fusion::get<0>(parsed)] = boost::fusion::get<2>(parsed);
+
+      smooth = std::min(smooth, boost::fusion::get<2>(parsed));
+    }
+
+  }
+
+  double operator()(const word_type& source, const word_type& target) const
+  {
+    if (tables.exists(source.id())) {
+      const table_type& table = tables[source.id()];
+
+      table_type::__table_type::const_iterator iter = table.table.find(target);
+      return (iter != table.table.end() ? iter->second : smooth);
+    } else 
+      return smooth;
+  }
+
+  double smooth;
+  table_set_type tables;
+};
+
+struct LexiconBase
+{
+  typedef cicada::Sentence sentence_type;
+
+  typedef LexiconModel lexicon_model_type;
+
+  LexiconBase(const lexicon_model_type& __lexicon_source_target,
+	      const lexicon_model_type& __lexicon_target_source)
+    : lexicon_source_target(__lexicon_source_target),
+      lexicon_target_source(__lexicon_target_source) {}
+
+  const lexicon_model_type& lexicon_source_target;
+  const lexicon_model_type& lexicon_target_source;
+  
+  std::pair<double, double> noisy_xor() const
+  {
+    const size_t source_size = source.size();
+    const size_t target_size = target.size();
+    
+    double score_source_target = 0.0;
+    
+    for (size_t trg = 0; trg != target_size; ++ trg)
+      if (target[trg].is_terminal()) {
+	double score = 0.0;
+	for (size_t src = 0; src != source_size; ++ src)
+	  if (source[src].is_terminal())
+	    score += utils::mathop::log(1.0 - lexicon_source_target(source[src], target[trg]));
+	
+	//
+	// 1.0 - exp(score) == - expm1(score)
+	//
+	score_source_target += utils::mathop::log(std::max(- boost::math::expm1(score), lexicon_source_target.smooth));
+      }
+    
+
+    double score_target_source = 0.0;
+    for (size_t src = 0; src != source_size; ++ src)
+      if (source[src].is_terminal()) {
+	double score = 0.0;
+	for (size_t trg = 0; trg != target_size; ++ trg)
+	  if (target[trg].is_terminal())
+	    score += utils::mathop::log(1.0 - lexicon_target_source(target[trg], source[src]));
+	
+	score_target_source += utils::mathop::log(std::max(- boost::math::expm1(score), lexicon_target_source.smooth));
+      }
+    
+    return std::make_pair(score_source_target, score_target_source);
+  }
+  
+  std::pair<double, double> model1() const
+  {
+    const size_t source_size = source.size();
+    const size_t target_size = target.size();
+    
+    double score_source_target = 0.0;
+    
+    for (size_t trg = 0; trg != target_size; ++ trg)
+      if (target[trg].is_terminal()) {
+	
+	double score = lexicon_source_target(lexicon_model_type::vocab_type::EPSILON, target[trg]);
+	for (size_t src = 0; src != source_size; ++ src)
+	  if (source[src].is_terminal())
+	    score += lexicon_source_target(source[src], target[trg]);
+	
+	score_source_target += utils::mathop::log(score);
+      }
+    
+    double score_target_source = 0.0;
+    
+    for (size_t src = 0; src != source_size; ++ src)
+      if (source[src].is_terminal()) {
+	
+	double score = lexicon_target_source(lexicon_model_type::vocab_type::EPSILON, source[src]);
+	for (size_t trg = 0; trg != target_size; ++ trg)
+	  if (target[trg].is_terminal())
+	    score += lexicon_target_source(target[trg], source[src]);
+	
+	score_target_source += utils::mathop::log(score);
+      }
+    
+    return std::make_pair(score_source_target, score_target_source);
+  }
+  
+  std::pair<double, double> insertion_deletion(const double threshold_insertion=0.01,
+					       const double threshold_deletion=0.01) const
+  {
+    const size_t source_size = source.size();
+    const size_t target_size = target.size();
+    
+    double score_source_target = 0.0;
+    
+    for (size_t trg = 0; trg != target_size; ++ trg)
+      if (target[trg].is_terminal()) {
+	
+	double score = 0.0;
+	for (size_t src = 0; src != source_size; ++ src)
+	  if (source[src].is_terminal())
+	    score = std::max(score, lexicon_source_target(source[src], target[trg]));
+	
+	score_source_target -= (score < threshold_insertion);
+      }
+    
+    double score_target_source = 0.0;
+    
+    for (size_t src = 0; src != source_size; ++ src)
+      if (source[src].is_terminal()) {
+	
+	double score = 0.0;
+	for (size_t trg = 0; trg != target_size; ++ trg)
+	  if (target[trg].is_terminal())
+	    score = std::max(score, lexicon_target_source(target[trg], source[src]));
+	
+	score_target_source -= (score < threshold_deletion);
+      }
+    
+    return std::make_pair(score_source_target, score_target_source);
+  }
+  
+  
+  sentence_type source;
+  sentence_type target;
+};
+
+struct LexiconPhrase : public LexiconBase
+{
+
+  LexiconPhrase(const lexicon_model_type& __lexicon_source_target,
+		const lexicon_model_type& __lexicon_target_source)
+    : LexiconBase(__lexicon_source_target, __lexicon_target_source) {}
+
+  void assign_source(const std::string& phrase)
+  {
+    source.assign(phrase);
+  }
+
+  void assign_target(const std::string& phrase)
+  {
+    target.assign(phrase);
+  }
+
+};
+
+struct LexiconSCFG : public LexiconBase
+{
+
+  LexiconSCFG(const lexicon_model_type& __lexicon_source_target,
+	      const lexicon_model_type& __lexicon_target_source)
+    : LexiconBase(__lexicon_source_target, __lexicon_target_source) {}
+
+  void assign_source(const std::string& phrase)
+  {
+    source.assign(phrase);
+    source.erase(source.begin());
+  }
+
+  void assign_target(const std::string& phrase)
+  {
+    target.assign(phrase);
+    target.erase(target.begin());
+  }
+};
+
+struct LexiconGHKM : public LexiconBase
+{
+
+  LexiconGHKM(const lexicon_model_type& __lexicon_source_target,
+	      const lexicon_model_type& __lexicon_target_source)
+    : LexiconBase(__lexicon_source_target, __lexicon_target_source) {}
+
+  void assign_source(const utils::piece& phrase)
+  {
+    source.clear();
+    cicada::TreeRule(phrase).frontier(std::back_inserter(source));
+  }
+
+  void assign_target(const utils::piece& phrase)
+  {
+    target.clear();
+    cicada::TreeRule(phrase).frontier(std::back_inserter(target));
   }
 };
 
