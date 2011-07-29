e0961f1fdaec19412212dc9ee6a796a3e73d5ace
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-07-29 16:04:55 +0900

    started new scripts: config generator and mert iterator

---
+++ scripts/cicada-config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+#  Copyright(C) 2011 Taro Watanabe <taro.watanabe@nict.go.jp>
+#
+
+###
+### a cicada.config generator
+###
---
+++ scripts/cicada-mert.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#
+#  Copyright(C) 2011 Taro Watanabe <taro.watanabe@nict.go.jp>
+#
+###
+### a wrapper script for MERT
+###
+
+#
+# we will rely on the cicada_filter_config to preprocess cicada-config
+#
