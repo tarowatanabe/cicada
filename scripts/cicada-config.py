e7f84ab2c42f13d2d4cd43ce671f99971a133ce7
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-07-31 19:11:26 +0900

    implemented grammar/tree-grammar class

--- scripts/cicada-config.py
+++ scripts/cicada-config.py
@@ -12,14 +12,15 @@
 ### (SCFG and Tree-to-string)
 ### 
 
+import UserList
 from optparse import OptionParser, make_option
 
 opt_parser = OptionParser(
     option_list=[
             
     ### grammars
-    make_option("--grammar",      default="", action="store", type="string", help="indexed grammar")
-    make_option("--tree-grammar", default="", action="store", type="string", help="indexed tree-grammar")
+    make_option("--grammar",      default="", action="store", type="string", help="indexed grammar"),
+    make_option("--tree-grammar", default="", action="store", type="string", help="indexed tree-grammar"),
     
     ### only affect grammars
     make_option("--max-span", default=15, action="store", type="int",
@@ -38,18 +39,67 @@ opt_parser = OptionParser(
     make_option("--tree-cky", default=None, action="store_true", help="string-to-{string,tree}"),
     
     ### feature application strategy
-    make_option("--apply-cube", default=None, action="store_true", help="feature application strategy (cube_pruning)")
-    make_option("--apply-grow", default=None, action="store_true", help="feature application strategy (cube_growing)")
+    make_option("--apply-cube", default=None, action="store_true", help="feature application strategy (cube_pruning)"),
+    make_option("--apply-grow", default=None, action="store_true", help="feature application strategy (cube_growing)"),
     
     ### some recommended configuration
-    make_option("--push-bos-eos", default=None, action="store_true", help="sentence bondary marker pushing")
+    make_option("--push-bos-eos", default=None, action="store_true", help="sentence bondary marker pushing"),
     
     ### outputs
-    make_option("--output-file",      default=None, action="store_true", help="output in a file")
-    make_option("--output-directory", default=None, action="store_true", help="output in a directory")
+    make_option("--output-file",      default=None, action="store_true", help="output in a file"),
+    make_option("--output-directory", default=None, action="store_true", help="output in a directory"),
     
     ## debug messages
     make_option("--debug", default=0, action="store", type="int"),
     ])
 
 
+class Grammar(UserList.UserList):
+    
+    def __init__(self, grammar_dir="", max_span=15):
+        
+        UserList.UserList.__init__(self)
+        
+        path_files = os.path.join(grammar_dir, 'files');
+        
+        if not os.path.exists(path_files):
+            raise ValueError, "no path to files: %s" %(path_files)
+        
+        self.grammar_dir = grammar_dir
+        
+        for line in open(paht_file):
+            
+            name = line.strip()
+            if not name: continue
+            
+            path = os.path.join(grammar_dir, name)
+            if not os.path.exists(path):
+                raise ValueError, "no path to scores: %s" %(path)
+            
+            self.append("grammar = " + path + ":max-span=%d" %(max_span))
+
+class TreeGrammar(UserList.UserList):
+    
+    def __init__(self, grammar_dir="", max_span=15):
+        
+        UserList.UserList.__init__(self)
+        
+        path_files = os.path.join(grammar_dir, 'files');
+        
+        if not os.path.exists(path_files):
+            raise ValueError, "no path to files: %s" %(path_files)
+        
+        self.grammar_dir = grammar_dir
+        
+        for line in open(paht_file):
+            
+            name = line.strip()
+            if not name: continue
+            
+            path = os.path.join(grammar_dir, name)
+            if not os.path.exists(path):
+                raise ValueError, "no path to scores: %s" %(path)
+            
+            self.append("tree-grammar = " + path)
+
+(options, args) = opt_parser.parse_args()
