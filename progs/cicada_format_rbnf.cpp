d988944b633c4f083891fb97083aea07e95b7319
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-08-13 12:41:35 +0900

    added --resource option to dump rules defined in ICU

--- progs/cicada_format_rbnf.cpp
+++ progs/cicada_format_rbnf.cpp
@@ -9,6 +9,11 @@
 #include <unicode/unistr.h>
 #include <unicode/locid.h>
 #include <unicode/bytestream.h>
+#include <unicode/resbund.h>
+#include <unicode/ures.h>
+#include <unicode/udata.h>
+
+#define U_ICUDATA_RBNF U_ICUDATA_NAME U_TREE_SEPARATOR_STRING "rbnf"
 
 #include "utils/compress_stream.hpp"
 
@@ -41,6 +46,8 @@ path_type output_file = "-";
 std::string locale;
 std::string rule;
 
+bool resource = false;
+
 int debug = 0;
 
 void options(int argc, char** argv);
@@ -57,12 +64,44 @@ int main(int argc, char** argv)
     
     if (loc.isBogus())
       throw std::runtime_error("invalid ocale: " + locale);
+
+    if (resource) {
+      const char* rules_tag = "RBNFRules";
+      const char* fmt_tag   = "SpelloutRules";
+
+      utils::compress_ostream os(output_file);
+      ostream_sink sink(os);
+      
+      UErrorCode status = U_ZERO_ERROR;
+      icu::ResourceBundle bundle(U_ICUDATA_RBNF, loc, status);
+      
+      if (U_SUCCESS(status)) {
+	icu::ResourceBundle bundle_rbnf = bundle.getWithFallback(rules_tag, status);
+        if (U_FAILURE(status))
+	  throw std::runtime_error("no rbnf?");
+	
+	icu::ResourceBundle bundle_rules = bundle_rbnf.getWithFallback(fmt_tag, status);
+        if (U_FAILURE(status))
+	  throw std::runtime_error("no rules?");
+	
+	while (bundle_rules.hasNext() && U_SUCCESS(status)) {
+	  bundle_rules.getNextString(status).toUTF8(sink);
+	  sink.write('\n');
+	}
+	
+	if (U_FAILURE(status))
+	  throw std::runtime_error("no string?");
+      } else 
+	throw std::runtime_error("no RBNF rules?");
+      
+      return 0;
+    }
     
     UErrorCode status = U_ZERO_ERROR;
     std::auto_ptr<RuleBasedNumberFormat> formatter(new RuleBasedNumberFormat(URBNF_SPELLOUT, loc, status));
     if (U_FAILURE(status))
       throw std::runtime_error(std::string("RuleBasedNumberFormat::spell_out: ") + u_errorName(status));
-
+    
     if (! rule.empty()) {
       UErrorCode status = U_ZERO_ERROR;
       formatter->setDefaultRuleSet(UnicodeString(rule.c_str()), status);
@@ -79,9 +118,6 @@ int main(int argc, char** argv)
 	throw std::runtime_error("unsupported rule set: " + rule);
       }
     }
-
-    
-	
     
     const bool flush_output = (output_file == "-"
                                || (boost::filesystem::exists(output_file)
@@ -118,8 +154,9 @@ void options(int argc, char** argv)
     ("input",  po::value<path_type>(&input_file)->default_value(input_file),   "input file")
     ("output", po::value<path_type>(&output_file)->default_value(output_file), "output")
     
-    ("locale", po::value<std::string>(&locale), "locale")
-    ("rule",   po::value<std::string>(&rule),   "rule set")
+    ("locale",   po::value<std::string>(&locale), "locale")
+    ("rule",     po::value<std::string>(&rule),   "rule set")
+    ("resource", po::bool_switch(&resource),      "dump resource")
     
     ("debug", po::value<int>(&debug)->implicit_value(1), "debug level")
         
