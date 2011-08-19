55c6cb93ca7c8f1870fa246557dccde3b59c65c6
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-08-19 18:43:59 +0900

    updated to use smaller buffer size + gzip compression when bcasting weights

--- progs/cicada_learn_online_mpi.cpp
+++ progs/cicada_learn_online_mpi.cpp
@@ -1335,7 +1335,7 @@ void send_weights(const int rank, const weight_set_type& weights)
   
   boost::iostreams::filtering_ostream os;
   os.push(boost::iostreams::gzip_compressor());
-  os.push(utils::mpi_device_sink(rank, weights_tag, 1024 * 1024));
+  os.push(utils::mpi_device_sink(rank, weights_tag, 4096));
   
   for (feature_type::id_type id = 0; id < weights.size(); ++ id)
     if (! feature_type(id).empty() && weights[id] != 0.0) {
@@ -1354,7 +1354,7 @@ void reduce_weights(const int rank, weight_set_type& weights)
   
   boost::iostreams::filtering_istream is;
   is.push(boost::iostreams::gzip_decompressor());
-  is.push(utils::mpi_device_source(rank, weights_tag, 1024 * 1024));
+  is.push(utils::mpi_device_source(rank, weights_tag, 4096));
   
   std::string line;
   
@@ -1560,7 +1560,8 @@ void bcast_weights(const int rank, weight_set_type& weights)
   
   if (mpi_rank == rank) {
     boost::iostreams::filtering_ostream os;
-    os.push(utils::mpi_device_bcast_sink(rank, 1024));
+    os.push(boost::iostreams::gzip_compressor());
+    os.push(utils::mpi_device_bcast_sink(rank, 4096));
     
     static const weight_set_type::feature_type __empty;
     
@@ -1581,7 +1582,8 @@ void bcast_weights(const int rank, weight_set_type& weights)
     weights.allocate();
     
     boost::iostreams::filtering_istream is;
-    is.push(utils::mpi_device_bcast_source(rank, 1024));
+    is.push(boost::iostreams::gzip_decompressor());
+    is.push(utils::mpi_device_bcast_source(rank, 4096));
     
     std::string feature;
     std::string value;
