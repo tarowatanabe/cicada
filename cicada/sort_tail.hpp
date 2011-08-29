c782ec6762bc8f861c26a8585bc21742ae7b4c76
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-08-29 17:02:23 +0900

    simplified sort-tail

--- cicada/sort_tail.hpp
+++ cicada/sort_tail.hpp
@@ -7,6 +7,7 @@
 #define __CICADA__SORT_TAIL__HPP__ 1
 
 #include <vector>
+#include <algorithm>
 
 #include <cicada/hypergraph.hpp>
 
@@ -29,7 +30,6 @@ namespace cicada
     {
       typedef std::vector<symbol_type, std::allocator<symbol_type> > rhs_type;
       typedef std::vector<id_type, std::allocator<id_type> >         tails_type;
-      typedef std::vector<int, std::allocator<int> >                 index_type;
 
       graph = x;
       
@@ -37,43 +37,38 @@ namespace cicada
       
       rhs_type   rhs;
       tails_type tails;
-      index_type index;
       
       hypergraph_type::edge_set_type::iterator eiter_end = graph.edges.end();
       for (hypergraph_type::edge_set_type::iterator eiter = graph.edges.begin(); eiter != eiter_end; ++ eiter) {
 	edge_type& edge = *eiter;
 
-	if (edge.tails.size() <= 1) continue;
+	if (edge.tails.empty()) continue;
 	
 	rhs.clear();
 	tails.clear();
-	index.clear();
 	
 	const symbol_type& lhs = edge.rule->lhs;
 	rhs.insert(rhs.end(), edge.rule->rhs.begin(), edge.rule->rhs.end());
-	tails.insert(tails.end(), edge.tails.begin(), edge.tails.end());
-	index.resize(tails.size());
+	tails.resize(edge.tails.size());
 	
 	int pos = 0;
 	rhs_type::iterator riter_end = rhs.end();
 	for (rhs_type::iterator riter = rhs.begin(); riter != riter_end; ++ riter)
 	  if (riter->is_non_terminal()) {
 	    const int non_terminal_pos = riter->non_terminal_index();
+	    const int antecedent_index = utils::bithack::branch(non_terminal_pos == 0, pos, non_terminal_pos - 1);
 	    
-	    index[utils::bithack::branch(non_terminal_pos == 0, pos, non_terminal_pos - 1)] = pos;
+	    tails[pos] = edge.tails[antecedent_index];
 	    
-	    *riter = riter->non_terminal(pos + 1);
+	    *riter = riter->non_terminal();
 	    ++ pos;
 	  }
-	
-	// sort tail wrt index...
-	for (size_t i = 0; i != tails.size(); ++ i)
-	  tails[i] = edge.tails[index[i]];
 	
 	// re-assign edge's rule and edge's tails...
 	
 	edge.rule = rule_type::create(rule_type(lhs, rhs.begin(), rhs.end()));
-	edge.tails.assign(tails.begin(), tails.end());
+	
+	std::copy(tails.begin(), tails.end(), edge.tails.begin());
       }
     }    
   };
