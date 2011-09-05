f29322b14c7902293458dbbf3d0224400e064a7d
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-09-05 12:52:46 +0900

    started implementing dependency feature

---
+++ cicada/feature/dependency.hpp
@@ -0,0 +1,17 @@
+// -*- mode: c++ -*-
+//
+//  Copyright(C) 2011 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+#ifndef __CICADA__FEATURE__DEPENDENCY__HPP__
+#define __CICADA__FEATURE__DEPENDENCY__HPP__ 1
+
+namespace cicada
+{
+  namespace feature
+  {
+    
+  };
+};
+
+#endif
