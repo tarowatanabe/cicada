dd926fc6dc62fb0e18b715f2c038d9f90caa552f
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-09-15 14:34:32 +0900

    added dependency filter

--- progs/cicada_filter_dependency.cpp
+++ progs/cicada_filter_dependency.cpp
@@ -106,8 +106,69 @@ int main(int argc, char** argv)
       throw std::runtime_error("# of dependency files do not match");
     if (target_files.size() != dependency_files.size())
       throw std::runtime_error("# of dependency files do not match");
-      
     
+    utils::compress_ostream os(output_file, 1024 * 1024);
+    
+    typedef boost::spirit::istream_iterator iiter_type;
+    
+    sentence_parser<iiter_type>    parser;
+
+    for (size_t i = 0; i != source_files.size(); ++ i) {
+      utils::compress_istream is_src(source_files[i], 1024 * 1024);
+      utils::compress_istream is_trg(target_files[i], 1024 * 1024);
+      utils::compress_istream is_align(alignment_files[i], 1024 * 1024);
+      utils::compress_istream is_dep(dependency_files[i], 1024 * 1024);
+      
+      is_src.unsetf(std::ios::skipws);
+      is_trg.unsetf(std::ios::skipws);
+      
+      iiter_type siter(is_src);
+      iiter_type titer(is_trg);
+      iiter_type siter_end;
+      iiter_type titer_end;
+      
+      sentence_type source;
+      sentence_type target;
+      alignment_type alignment;
+      dependency_type dependency;
+      dependency_type projected;
+      
+      for (size_t line_no = 0; siter != siter_end && titer != titer_end; ++ line_no) {
+	source.clear();
+	target.clear();
+	alignment.clear();
+	dependency.clear();
+	
+	if (! boost::spirit::qi::phrase_parse(siter, siter_end, parser, boost::spirit::standard::blank, source))
+	  throw std::runtime_error("source sentence parsing failed at # " + utils::lexical_cast<std::string>(line_no));
+	if (! boost::spirit::qi::phrase_parse(titer, titer_end, parser, boost::spirit::standard::blank, target))
+	  throw std::runtime_error("target sentence parsing failed at # " + utils::lexical_cast<std::string>(line_no));
+	
+	if (! (is_align >> alignment))
+	  throw std::runtime_error("no alignment?");
+	if (! (is_dep >> dependency))
+	  throw std::runtime_error("no dependency?");
+	
+	const int source_size = source.size();
+	const int target_size = target.size();
+
+	if (source_size != static_cast<int>(dependency.size()))
+	  throw std::runtime_error("source size do not match with dependency size at # " + utils::lexical_cast<std::string>(line_no));
+	
+	if (source_size == 0 || target_size == 0) {
+	  os << '\n';
+	  continue;
+	};
+	
+	projected.clear();
+	projected.resize(target_size, -1);
+	
+	// perform projection....
+	
+	
+	os << projected << '\n';
+      }
+    }
   }
   catch(std::exception& err) {
     std::cerr << "error: " << err.what() << std::endl;
