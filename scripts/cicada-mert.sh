cee4e3f13a94838e75c974122a32e5f78104be5f
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-09-29 15:47:42 +0900

    bugfix for openmpi path

--- scripts/cicada-mert.sh
+++ scripts/cicada-mert.sh
@@ -256,9 +256,9 @@ fi
 if test "$openmpi" != ""; then
   openmpi=`echo "${openmpi}/" | sed -e 's/\/\/$/\//'`
   
-  if test ! -e $openmpi/mpirun; then
-    openmpi=$openmpi/bin
-    if test ! -e $openmpi/mpirun; then
+  if test ! -e ${openmpi}mpirun; then
+    openmpi=${openmpi}bin/
+    if test ! -e ${openmpi}mpirun; then
       echo "no mpirun?" >&2
     exit 1
     fi
