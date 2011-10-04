c280eedfe119f92d24edd66919c1dcec869a48ec
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-10-04 15:32:01 +0900

    use of simple-vector, instead of std::vector for the storage of malloced area

--- cicada/model.cpp
+++ cicada/model.cpp
@@ -8,6 +8,8 @@
 
 #include "model.hpp"
 
+#include "utils/simple_vector.hpp"
+
 namespace cicada
 {
   class StateAllocator : public std::allocator<char>
@@ -20,15 +22,22 @@ namespace cicada
     typedef ptrdiff_t           difference_type;
   
     typedef std::allocator<char> allocator_type;
-    typedef std::vector<pointer, std::allocator<pointer> > state_set_type;
+    typedef utils::simple_vector<pointer, std::allocator<pointer> > state_set_type;
   
   public:
     static const size_type chunk_size  = 1024 * 16;
     static const size_type chunk_mask  = chunk_size - 1;
   
   public:
+
+    StateAllocator()
+      : states(), state_iterator(0),
+	cache(0),
+	state_size(0),
+	state_alloc_size(0),
+	state_chunk_size(0) {}
   
-    StateAllocator(size_type __state_size = 0)
+    StateAllocator(size_type __state_size)
       : states(), state_iterator(0),
 	cache(0),
 	state_size(__state_size),
@@ -89,6 +98,8 @@ namespace cicada
       if (chunk_pos == 0) {
 	states.push_back(allocator_type::allocate(state_chunk_size));
 	std::uninitialized_fill(states.back(), states.back() + state_chunk_size, 0);
+
+	//std::cerr << "allocated: " << ((void*) states.back()) << " size: " << states.size() << std::endl;
       }
     
       ++ state_iterator;
@@ -118,9 +129,13 @@ namespace cicada
   
     void clear()
     {
+      //std::cerr << "allocated states size: " << states.size() << std::endl;
+
       state_set_type::iterator siter_end = states.end();
-      for (state_set_type::iterator siter = states.begin(); siter != siter_end; ++ siter)
+      for (state_set_type::iterator siter = states.begin(); siter != siter_end; ++ siter) {
+	//std::cerr << "de-allocated: " << ((void*) *siter) << std::endl;
 	allocator_type::deallocate(*siter, state_chunk_size);
+      }
     
       states.clear();
       state_iterator = 0;
@@ -310,13 +325,18 @@ namespace cicada
     offsets.reserve(models.size());
     offsets.resize(models.size());
     states_size = 0;
+
+    const size_t pointer_size = sizeof(void*);
+    const size_t pointer_mask = ~(pointer_size - 1);
     
     for (size_t i = 0; i != models.size(); ++ i) {
       offsets[i] = states_size;
       
       //std::cerr << "offset: " << i << " = " << offsets[i] << std::endl;
       
-      states_size += models[i]->state_size();
+      // multiple of pointer size...
+      if (models[i]->state_size())
+	states_size += (models[i]->state_size() + pointer_size - 1) & pointer_mask;
       
       if (feature_names.find(models[i]->feature_name()) != feature_names.end())
 	throw std::runtime_error("you have already registered feature: " + static_cast<const std::string&>(models[i]->feature_name()));
