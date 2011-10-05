dd9f06fbdf99e0ebbc7e5031292226d122648b57
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-10-05 13:18:00 +0900

    "assign" should be defined for other assingmnets

--- cicada/feature/scorer.hpp
+++ cicada/feature/scorer.hpp
@@ -24,6 +24,13 @@ namespace cicada
       typedef score_type::score_ptr_type score_ptr_type;
       
       // this is scorer specific assign
+      virtual void assign(const size_type& id,
+			  const hypergraph_type& hypergraph,
+			  const lattice_type& lattice,
+			  const span_set_type& spans,
+			  const sentence_set_type& targets,
+			  const ngram_count_set_type& ngram_counts) = 0;
+
       virtual void assign(const score_ptr_type& score) = 0;
     };
     
