146326a8744f815cacb2d440b19758c364589f9d
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-10-13 11:42:23 +0900

    removed weight normalizer...

--- progs/cicada_learn_block_impl.hpp
+++ progs/cicada_learn_block_impl.hpp
@@ -437,7 +437,7 @@ struct LearnSVM : public LearnBase
 	if (*aiter > 0.0) {
 	  sample_set_type::value_type::const_iterator fiter_end = features[i][j].end();
 	  for (sample_set_type::value_type::const_iterator fiter = features[i][j].begin(); fiter != fiter_end; ++ fiter)
-	    weights[fiter->first] += (*aiter) * fiter->second * positions.size();
+	    weights[fiter->first] += (*aiter) * fiter->second;
 	}
 	
 	alphas[i][j] = *aiter;
