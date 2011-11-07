711aac922393e3fb7d04d5c5ff2035dcc45889cf
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-11-07 10:37:30 +0900

    allow revise: moses-config to weights

--- progs/cicada_filter_config_moses.cpp
+++ progs/cicada_filter_config_moses.cpp
@@ -95,165 +95,236 @@ int main(int argc, char** argv)
 {
   try {
     options(argc, argv);
-
-    if (weights_file.empty() || ! boost::filesystem::exists(weights_file))
-      throw std::runtime_error("no weight file? " + weights_file.string());
-
-    feature_map_type feature_map;
-    feature_map["d"]   = "weight-d";
-    feature_map["lr"]  = "weight-lr";
-    feature_map["g"]   = "weight-generation";
-    feature_map["I"]   = "weight-i";
-    feature_map["lm"]  = "weight-l";
-    feature_map["l"]   = "weight-lex";
-    feature_map["tm"]  = "weight-t";
-    feature_map["w"]   = "weight-w";
-    feature_map["u"]   = "weight-u";
-    feature_map["e"]   = "weight-e";
-    feature_map["slm"] = "weight-slm";
-
-    feature_weight_set_type feature_weights;
-    feature_weight_type feature_weight;
-      
-    {
-      typedef boost::spirit::istream_iterator iter_type;
-      
-      utils::compress_istream is(weights_file, 1024 * 1024);
-      is.unsetf(std::ios::skipws);
-      
-      feature_weight_parser<iter_type> parser;
-      iter_type iter(is);
-      iter_type iter_end;
-      
-      while (iter != iter_end) {
-	boost::fusion::get<0>(feature_weight).clear();
-	
-	if (! boost::spirit::qi::parse(iter, iter_end, parser, feature_weight))
-	  if (iter != iter_end)
-	    throw std::runtime_error("weight parsing failed");
-	
-	const std::string& feature = boost::fusion::get<0>(feature_weight);
-	const int&         pos     = boost::fusion::get<1>(feature_weight);
-	const double&      value   = boost::fusion::get<2>(feature_weight);
-
-	if (feature.empty()) continue;
-
-	std::string feat = feature;
-	feature_map_type::const_iterator miter = feature_map.find(feature);
-	if (miter != feature_map.end())
-	  feat = miter->second;
-	
-	weight_set_type& weights = feature_weights[feat];
-	if (pos >= weights.size())
-	  weights.resize(pos + 1);
-	weights[pos] = value;
-      }
-    }
     
-    if (! features_biased.empty()) {
-      feature_weight_parser<std::string::const_iterator> parser;
+    if (weights_file.empty()) {
+      // we will extract feature value!
+      
+      feature_map_type feature_map;
+      feature_map["weight-d"]          = "d";
+      feature_map["weight-lr"]         = "lr";
+      feature_map["weight-generation"] = "g";
+      feature_map["weight-i"]          = "I";
+      feature_map["weight-l"]          = "lm";
+      feature_map["weight-lex"]        = "l";
+      feature_map["weight-t"]          = "tm";
+      feature_map["weight-w"]          = "w";
+      feature_map["weight-u"]          = "u";
+      feature_map["weight-e"]          = "e";
+      feature_map["weight-slm"]        = "slm";
+      
+      utils::compress_istream is(input_file);
+      utils::compress_ostream os(output_file);
+      os.precision(10);
+    
+      namespace xpressive = boost::xpressive;
 
-      for (feature_set_type::const_iterator fiter = features_biased.begin(); fiter != features_biased.end(); ++ fiter) {
-	const std::string line = *fiter + " " + utils::lexical_cast<std::string>(weight_biased);
-	
-	std::string::const_iterator iter = line.begin();
-	std::string::const_iterator end  = line.end();
-
-	boost::fusion::get<0>(feature_weight).clear();
-
-	if (! boost::spirit::qi::parse(iter, end, parser, feature_weight))
-	  if (iter != end)
-	    throw std::runtime_error("weight parsing failed");
-	
-	const std::string& feature = boost::fusion::get<0>(feature_weight);
-	const int&         pos     = boost::fusion::get<1>(feature_weight);
-	const double&      value   = boost::fusion::get<2>(feature_weight);
-	
-	if (feature.empty()) continue;
-	
-	std::string feat = feature;
-	feature_map_type::const_iterator miter = feature_map.find(feature);
-	if (miter != feature_map.end())
-	  feat = miter->second;
-	
-	weight_set_type& weights = feature_weights[feat];
-	if (pos >= weights.size())
-	  weights.resize(pos + 1);
-	weights[pos] = value;
-      }
-    }
-    
-    
-    utils::compress_istream is(input_file);
-    utils::compress_ostream os(output_file);
-    os.precision(10);
-
-    feature_consumed_type features_consumed;
-    
-    namespace xpressive = boost::xpressive;
-
-    xpressive::sregex pattern_comment = *xpressive::_s >> '#' >> *xpressive::_;
-    xpressive::sregex pattern_empty = *xpressive::_s;
-    
-    xpressive::sregex pattern_section_weight = (*xpressive::_s
-						>> '[' >> (xpressive::s1= "weight-" >> +(~xpressive::_s)) >> ']'
-						>> *xpressive::_s);
-    
-    xpressive::sregex pattern_section = (*xpressive::_s
-					 >> '[' >> (xpressive::s1= +(~xpressive::_s)) >> ']'
-					 >> *xpressive::_s);
-    
-    std::string line;
-    bool consumed = std::getline(is, line);
-    while (consumed) {
-      xpressive::smatch what;
+      xpressive::sregex pattern_comment = *xpressive::_s >> '#' >> *xpressive::_;
+      xpressive::sregex pattern_empty = *xpressive::_s;
       
-      if (! xpressive::regex_match(line, what, pattern_section_weight)) {
-	os << line << '\n';
-	consumed = std::getline(is, line);
-	continue;
-      }
+      xpressive::sregex pattern_section_weight = (*xpressive::_s
+						  >> '[' >> (xpressive::s1= "weight-" >> +(~xpressive::_s)) >> ']'
+						  >> *xpressive::_s);
       
-      // this is the section for "weight"
-      os << line << '\n';
+      xpressive::sregex pattern_section = (*xpressive::_s
+					   >> '[' >> (xpressive::s1= +(~xpressive::_s)) >> ']'
+					   >> *xpressive::_s);
       
-      const std::string section = what[1];
-      feature_weight_set_type::const_iterator witer = feature_weights.find(section);
+      std::string weight_prefix;
+      std::string line;
+      bool consumed = std::getline(is, line);
+      while (consumed) {
+	xpressive::smatch what;
       
-      static const weight_set_type weights_empty;
-      
-      const weight_set_type& weights = (witer != feature_weights.end() ? witer->second : weights_empty);
-      
-      features_consumed.insert(section);
-      
-      int pos = 0;
-      do {
-	consumed = std::getline(is, line);
-	if (! consumed || xpressive::regex_match(line, what, pattern_section)) break;
-	
-	if (xpressive::regex_match(line, what, pattern_comment) || xpressive::regex_match(line, what, pattern_empty))
-	  os << line << '\n';
-	else {
-	  if (pos < static_cast<int>(weights.size()))
-	    os << weights[pos] << '\n';
-	  else
-	    os << 0.0 << '\n';
-	  ++ pos;
+	if (! xpressive::regex_match(line, what, pattern_section_weight)) {
+	  //os << line << '\n';
+	  consumed = std::getline(is, line);
+	  continue;
 	}
-      } while (consumed);
-    }
-    
-    // write all additional parameters...
-    
-    feature_weight_set_type::const_iterator fiter_end = feature_weights.end();
-    for (feature_weight_set_type::const_iterator fiter = feature_weights.begin(); fiter != fiter_end; ++ fiter)
-      if (features_consumed.find(fiter->first) == features_consumed.end()) {
-	os << '\n' << '[' << fiter->first << ']' << '\n';
-	for (size_t pos = 0; pos != fiter->second.size(); ++ pos)
-	  os << fiter->second[pos] << '\n';
-	os << '\n';
+	
+	// this is the section for "weight"
+	//os << line << '\n';
+	
+	const std::string section = what[1];
+	
+	feature_map_type::const_iterator fiter = feature_map.find(section);
+	
+	weight_prefix = (fiter == feature_map.end() ? std::string("") : fiter->second);
+	
+	int pos = 0;
+	do {
+	  consumed = std::getline(is, line);
+	  if (! consumed || xpressive::regex_match(line, what, pattern_section)) break;
+	  
+	  if (xpressive::regex_match(line, what, pattern_comment) || xpressive::regex_match(line, what, pattern_empty)) {
+	    //os << line << '\n';
+	  } else {
+	    if (! weight_prefix.empty())
+	      os << weight_prefix << ':' << utils::lexical_cast<std::string>(pos) << ' ' << line << '\n';
+	    
+	    ++ pos;
+	  }
+	} while (consumed);
+      }
+      
+    } else {
+      if (weights_file != "-" && ! boost::filesystem::exists(weights_file))
+	throw std::runtime_error("no weight file? " + weights_file.string());
+
+      feature_map_type feature_map;
+      feature_map["d"]   = "weight-d";
+      feature_map["lr"]  = "weight-lr";
+      feature_map["g"]   = "weight-generation";
+      feature_map["I"]   = "weight-i";
+      feature_map["lm"]  = "weight-l";
+      feature_map["l"]   = "weight-lex";
+      feature_map["tm"]  = "weight-t";
+      feature_map["w"]   = "weight-w";
+      feature_map["u"]   = "weight-u";
+      feature_map["e"]   = "weight-e";
+      feature_map["slm"] = "weight-slm";
+
+      feature_weight_set_type feature_weights;
+      feature_weight_type feature_weight;
+      
+      {
+	typedef boost::spirit::istream_iterator iter_type;
+      
+	utils::compress_istream is(weights_file, 1024 * 1024);
+	is.unsetf(std::ios::skipws);
+      
+	feature_weight_parser<iter_type> parser;
+	iter_type iter(is);
+	iter_type iter_end;
+      
+	while (iter != iter_end) {
+	  boost::fusion::get<0>(feature_weight).clear();
+	
+	  if (! boost::spirit::qi::parse(iter, iter_end, parser, feature_weight))
+	    if (iter != iter_end)
+	      throw std::runtime_error("weight parsing failed");
+	
+	  const std::string& feature = boost::fusion::get<0>(feature_weight);
+	  const int&         pos     = boost::fusion::get<1>(feature_weight);
+	  const double&      value   = boost::fusion::get<2>(feature_weight);
+
+	  if (feature.empty()) continue;
+
+	  std::string feat = feature;
+	  feature_map_type::const_iterator miter = feature_map.find(feature);
+	  if (miter != feature_map.end())
+	    feat = miter->second;
+	
+	  weight_set_type& weights = feature_weights[feat];
+	  if (pos >= weights.size())
+	    weights.resize(pos + 1);
+	  weights[pos] = value;
+	}
       }
     
+      if (! features_biased.empty()) {
+	feature_weight_parser<std::string::const_iterator> parser;
+
+	for (feature_set_type::const_iterator fiter = features_biased.begin(); fiter != features_biased.end(); ++ fiter) {
+	  const std::string line = *fiter + " " + utils::lexical_cast<std::string>(weight_biased);
+	
+	  std::string::const_iterator iter = line.begin();
+	  std::string::const_iterator end  = line.end();
+
+	  boost::fusion::get<0>(feature_weight).clear();
+
+	  if (! boost::spirit::qi::parse(iter, end, parser, feature_weight))
+	    if (iter != end)
+	      throw std::runtime_error("weight parsing failed");
+	
+	  const std::string& feature = boost::fusion::get<0>(feature_weight);
+	  const int&         pos     = boost::fusion::get<1>(feature_weight);
+	  const double&      value   = boost::fusion::get<2>(feature_weight);
+	
+	  if (feature.empty()) continue;
+	
+	  std::string feat = feature;
+	  feature_map_type::const_iterator miter = feature_map.find(feature);
+	  if (miter != feature_map.end())
+	    feat = miter->second;
+	
+	  weight_set_type& weights = feature_weights[feat];
+	  if (pos >= weights.size())
+	    weights.resize(pos + 1);
+	  weights[pos] = value;
+	}
+      }
+    
+    
+      utils::compress_istream is(input_file);
+      utils::compress_ostream os(output_file);
+      os.precision(10);
+
+      feature_consumed_type features_consumed;
+    
+      namespace xpressive = boost::xpressive;
+
+      xpressive::sregex pattern_comment = *xpressive::_s >> '#' >> *xpressive::_;
+      xpressive::sregex pattern_empty = *xpressive::_s;
+    
+      xpressive::sregex pattern_section_weight = (*xpressive::_s
+						  >> '[' >> (xpressive::s1= "weight-" >> +(~xpressive::_s)) >> ']'
+						  >> *xpressive::_s);
+    
+      xpressive::sregex pattern_section = (*xpressive::_s
+					   >> '[' >> (xpressive::s1= +(~xpressive::_s)) >> ']'
+					   >> *xpressive::_s);
+    
+      std::string line;
+      bool consumed = std::getline(is, line);
+      while (consumed) {
+	xpressive::smatch what;
+      
+	if (! xpressive::regex_match(line, what, pattern_section_weight)) {
+	  os << line << '\n';
+	  consumed = std::getline(is, line);
+	  continue;
+	}
+      
+	// this is the section for "weight"
+	os << line << '\n';
+      
+	const std::string section = what[1];
+	feature_weight_set_type::const_iterator witer = feature_weights.find(section);
+      
+	static const weight_set_type weights_empty;
+      
+	const weight_set_type& weights = (witer != feature_weights.end() ? witer->second : weights_empty);
+      
+	features_consumed.insert(section);
+      
+	int pos = 0;
+	do {
+	  consumed = std::getline(is, line);
+	  if (! consumed || xpressive::regex_match(line, what, pattern_section)) break;
+	
+	  if (xpressive::regex_match(line, what, pattern_comment) || xpressive::regex_match(line, what, pattern_empty))
+	    os << line << '\n';
+	  else {
+	    if (pos < static_cast<int>(weights.size()))
+	      os << weights[pos] << '\n';
+	    else
+	      os << 0.0 << '\n';
+	    ++ pos;
+	  }
+	} while (consumed);
+      }
+    
+      // write all additional parameters...
+    
+      feature_weight_set_type::const_iterator fiter_end = feature_weights.end();
+      for (feature_weight_set_type::const_iterator fiter = feature_weights.begin(); fiter != fiter_end; ++ fiter)
+	if (features_consumed.find(fiter->first) == features_consumed.end()) {
+	  os << '\n' << '[' << fiter->first << ']' << '\n';
+	  for (size_t pos = 0; pos != fiter->second.size(); ++ pos)
+	    os << fiter->second[pos] << '\n';
+	  os << '\n';
+	}
+    }
   }
   catch (const std::exception& err) {
     std::cerr << "error: " << err.what() << std::endl;
