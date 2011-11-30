110081932ef05dc55503dea75a16653141f1ae7b
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-11-30 17:30:21 +0900

    updated doc :-) + remove commented out code

--- progs/cicada_learn_kbest_mpi.cpp
+++ progs/cicada_learn_kbest_mpi.cpp
@@ -2243,8 +2243,7 @@ struct OptimizeMCP
 	for (hypothesis_set_type::const_iterator kiter = kbests[id].begin(); kiter != kiter_end; ++ kiter, ++ pos_kbest) {
 	  const hypothesis_type& kbest = *kiter;
 
-	  //const double margin = cicada::dot_product(weights, kbest.features.begin(), kbest.features.end(), kbest.loss);
-	  //const double margin = cicada::dot_product(weights, kbest.features.begin(), kbest.features.end(), 0.0);
+	  //const double margin = cicada::dot_product(weights, features_kbest[pos_kbest].begin(), features_kbest[pos_kbest].end(), kbest.loss);
 	  const double margin = cicada::dot_product(weights, features_kbest[pos_kbest].begin(), features_kbest[pos_kbest].end(), 0.0);
 	  
 	  if (! kbests_hyp[seg] || margin > kbests_margin[seg]) {
@@ -2278,12 +2277,6 @@ struct OptimizeMCP
 	  if (oracle.loss > kbest_loss) continue;
 	  //if (oracle.loss > oracles_loss[seg]) continue;
 	  
-	  //
-	  // we will find oracles which is very close to kbest...
-	  //
-	  
-	  //const double margin = cicada::dot_product(weights, oracle.features.begin(), oracle.features.end(), - oraacle.loss);
-	  //const double margin = cicada::dot_product(weights, oracle.features.begin(), oracle.features.end(), 0.0);
 	  //const double margin = cicada::dot_product(weights, features_oracle[pos_oracle].begin(), features_oracle[pos_oracle].end(), 0.0);
 	  const double margin = cicada::dot_product(weights, features_oracle[pos_oracle].begin(), features_oracle[pos_oracle].end(), - oracle.loss);
 	  
@@ -2335,16 +2328,6 @@ struct OptimizeMCP
 	sample_set_type::value_type::const_iterator oiter_end = features_oracle[oracles_pos[seg]].end();
 	for (sample_set_type::value_type::const_iterator oiter = features_oracle[oracles_pos[seg]].begin(); oiter != oiter_end; ++ oiter)
 	  acc[oiter->first] += oiter->second * factor;
-	
-#if 0
-	hypothesis_type::feature_set_type::const_iterator kiter_end = kbests_hyp[seg]->features.end();
-	for (hypothesis_type::feature_set_type::const_iterator kiter = kbests_hyp[seg]->features.begin(); kiter != kiter_end; ++ kiter)
-	  acc[kiter->first] -= kiter->second * factor;
-	
-	hypothesis_type::feature_set_type::const_iterator oiter_end = oracles_hyp[seg]->features.end();
-	for (hypothesis_type::feature_set_type::const_iterator oiter = oracles_hyp[seg]->features.begin(); oiter != oiter_end; ++ oiter)
-	  acc[oiter->first] += oiter->second * factor;
-#endif
       }
     
     return std::make_pair((loss - margin) * factor, score);
@@ -2539,8 +2522,7 @@ struct OptimizeMCP
 	for (hypothesis_set_type::const_iterator kiter = kbests[id].begin(); kiter != kiter_end; ++ kiter, ++ pos_kbest) {
 	  const hypothesis_type& kbest = *kiter;
 
-	  //const double margin = cicada::dot_product(weights, kbest.features.begin(), kbest.features.end(), kbest.loss);
-	  //const double margin = cicada::dot_product(weights, kbest.features.begin(), kbest.features.end(), 0.0);
+	  //const double margin = cicada::dot_product(weights, features_kbest[pos_kbest].begin(), features_kbest[pos_kbest].end(), kbest.loss);
 	  const double margin = cicada::dot_product(weights, features_kbest[pos_kbest].begin(), features_kbest[pos_kbest].end(), 0.0);
 	  
 	  if (! kbests_hyp[seg] || margin > kbests_margin[seg]) {
@@ -2571,8 +2553,6 @@ struct OptimizeMCP
 	  if (oracle.loss > kbest_loss) continue;
 	  //if (oracle.loss > oracles_loss[seg]) continue;
 	  
-	  //const double margin = cicada::dot_product(weights, oracle.features.begin(), oracle.features.end(), - oracle.loss);
-	  //const double margin = cicada::dot_product(weights, oracle.features.begin(), oracle.features.end(), 0.0);
 	  //const double margin = cicada::dot_product(weights, features_oracle[pos_oracle].begin(), features_oracle[pos_oracle].end(), 0.0);
 	  const double margin = cicada::dot_product(weights, features_oracle[pos_oracle].begin(), features_oracle[pos_oracle].end(), - oracle.loss);
 	  
