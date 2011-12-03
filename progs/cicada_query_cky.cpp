defda53e1cb71f4f660327be972bd57dbdb87ebb
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-12-03 14:40:54 +0900

    added query for cky composition

--- progs/cicada_query_cky.cpp
+++ progs/cicada_query_cky.cpp
@@ -24,10 +24,10 @@
 
 
 #include "cicada/lattice.hpp"
+#include "cicada/sentence.hpp"
 #include "cicada/hypergraph.hpp"
-#include "cicada/query_tree.hpp"
+#include "cicada/query_cky.hpp"
 #include "cicada/grammar.hpp"
-#include "cicada/tree_grammar.hpp"
 
 #include "utils/lockfree_list_queue.hpp"
 #include "utils/json_string_generator.hpp"
@@ -38,13 +38,12 @@
 typedef boost::filesystem::path path_type;
 
 typedef std::vector<std::string, std::allocator<std::string> > grammar_file_set_type;
-typedef cicada::HyperGraph      hypergraph_type;
+
+typedef cicada::Lattice  lattice_type;
+typedef cicada::Sentence sentence_type;
 
 typedef cicada::Grammar         grammar_type;
-typedef cicada::TreeGrammar     tree_grammar_type;
-
 typedef cicada::Transducer      transducer_type;
-typedef cicada::TreeTransducer  tree_transducer_type;
 
 typedef transducer_type::feature_set_type   feature_set_type;
 typedef transducer_type::attribute_set_type attribute_set_type;
@@ -52,10 +51,6 @@ typedef transducer_type::attribute_set_type attribute_set_type;
 typedef transducer_type::rule_type           rule_type;
 typedef transducer_type::rule_ptr_type       rule_ptr_type;
 typedef transducer_type::rule_pair_type      rule_pair_type;
-
-typedef tree_transducer_type::rule_type      tree_rule_type;
-typedef tree_transducer_type::rule_ptr_type  tree_rule_ptr_type;
-typedef tree_transducer_type::rule_pair_type tree_rule_pair_type;
 
 struct rule_pair_string_type
 {
@@ -78,26 +73,6 @@ struct rule_pair_string_type
   }
 };
 
-struct tree_rule_pair_string_type
-{
-  std::string source;
-  std::string target;
-  
-  feature_set_type   features;
-  attribute_set_type attributes;
-  
-  tree_rule_pair_string_type() {}
-
-  void clear()
-  {
-    source.clear();
-    target.clear();
-    features.clear();
-    attributes.clear();
-  }
-
-};
-
 struct less_rule_pair
 {
   bool operator()(const rule_pair_string_type& x, const rule_pair_string_type& y) const
@@ -114,25 +89,8 @@ struct less_rule_pair
   }
 };
 
-struct less_tree_rule_pair
-{
-  bool operator()(const tree_rule_pair_string_type& x, const tree_rule_pair_string_type& y) const
-  {
-    return (x.source < y.source
-	    || (!(y.source < x.source)
-		&& (x.target < y.target
-		    || (!(y.target < x.target)
-			&& (x.features < y.features
-			    || (!(y.features < x.features)
-				&& x.attributes < y.attributes))))));
-  }
-};
-
-typedef std::set<rule_pair_string_type, less_rule_pair, std::allocator<rule_pair_string_type> >                rule_pair_unique_type;
-typedef std::set<tree_rule_pair_string_type, less_tree_rule_pair, std::allocator<tree_rule_pair_string_type> > tree_rule_pair_unique_type;
-
-typedef std::vector<rule_pair_type, std::allocator<rule_pair_type> >                rule_pair_set_type;
-typedef std::vector<tree_rule_pair_type, std::allocator<tree_rule_pair_type> > tree_rule_pair_set_type;
+typedef std::set<rule_pair_string_type, less_rule_pair, std::allocator<rule_pair_string_type> > rule_pair_unique_type;
+typedef std::vector<rule_pair_type, std::allocator<rule_pair_type> >                            rule_pair_set_type;
 
 template <typename Iterator>
 struct features_generator : boost::spirit::karma::grammar<Iterator, feature_set_type()>
@@ -189,14 +147,16 @@ struct attributes_generator : boost::spirit::karma::grammar<Iterator, attribute_
 
 
 path_type input_file = "-";
-path_type output_tree_file = "-";
-path_type output_rule_file = "-";
+path_type output_file = "-";
 
 grammar_file_set_type grammar_files;
 bool grammar_list = false;
 
-grammar_file_set_type tree_grammar_files;
-bool tree_grammar_list = false;
+bool input_sentence_mode = false;
+bool input_lattice_mode = false;
+
+bool treebank_mode = false;
+bool pos_mode = false;
 
 int threads = 1;
 
@@ -208,64 +168,41 @@ struct Task
   typedef utils::lockfree_list_queue<std::string, std::allocator<std::string> > queue_type;
   
   Task(queue_type& __queue,
-       const tree_grammar_type& __tree_grammar,
-       const grammar_type& __grammar) : queue(__queue), tree_grammar(__tree_grammar), grammar(__grammar) {}
+       const grammar_type& __grammar) : queue(__queue), grammar(__grammar) {}
   
   void operator()()
   {
-    tree_grammar_type tree_grammar_local(tree_grammar.clone());
     grammar_type      grammar_local(grammar.clone());
 
-    cicada::QueryTree query(tree_grammar_local, grammar_local);
-
-    tree_rule_pair_set_type tree_rules;
-    rule_pair_set_type      rules;
+    cicada::QueryCKY query(grammar_local, treebank_mode, pos_mode);
     
-    tree_rule_pair_string_type tree_rule_string;
+    rule_pair_set_type    rules;
     rule_pair_string_type rule_string;
     
     boost::iostreams::filtering_ostream os_source;
     boost::iostreams::filtering_ostream os_target;
+
+    lattice_type lattice;
+    sentence_type sentence;
     
-    hypergraph_type graph;
     std::string line;
     for (;;) {
       queue.pop_swap(line);
       if (line.empty()) break;
       
-      graph.assign(line);
+      if (input_lattice_mode)
+	lattice.assign(line);
+      else {
+	sentence.assign(line);
+	lattice = lattice_type(sentence);
+      }
       
-      tree_rules.clear();
       rules.clear();
       
-      query(graph, std::back_inserter(tree_rules), std::back_inserter(rules));
+      query(lattice, std::back_inserter(rules));
 
       if (debug)
-	std::cerr << "# of tree rules: " << tree_rules.size()
-		  << " # of rules: " << rules.size()
-		  << std::endl;
-      
-      tree_rule_pair_set_type::iterator titer_end = tree_rules.end();
-      for (tree_rule_pair_set_type::iterator titer = tree_rules.begin(); titer != titer_end; ++ titer) 
-	if (titer->source || titer->target) {
-	  tree_rule_string.clear();
-	  
-	  os_source.push(boost::iostreams::back_inserter(tree_rule_string.source));
-	  os_target.push(boost::iostreams::back_inserter(tree_rule_string.target));
-	  
-	  if (titer->source)
-	    os_source << *(titer->source);
-	  if (titer->target)
-	    os_target << *(titer->target);
-	  
-	  os_source.pop();
-	  os_target.pop();
-	  
-	  tree_rule_string.features.swap(titer->features);
-	  tree_rule_string.attributes.swap(titer->attributes);
-	  
-	  tree_rules_unique.insert(tree_rule_string);
-	}
+	std::cerr << "# of rules: " << rules.size() << std::endl;
       
       rule_pair_set_type::iterator riter_end = rules.end();
       for (rule_pair_set_type::iterator riter = rules.begin(); riter != riter_end; ++ riter)
@@ -293,12 +230,10 @@ struct Task
     }
     
   }
-
+  
   queue_type&              queue;
-  const tree_grammar_type& tree_grammar;
   const grammar_type&      grammar;
   
-  tree_rule_pair_unique_type tree_rules_unique;
   rule_pair_unique_type      rules_unique;
 };
 
@@ -314,10 +249,10 @@ int main(int argc, char** argv)
       return 0;
     }
     
-    if (tree_grammar_list) {
-      std::cout << tree_grammar_type::lists();
-      return 0;
-    }
+    if (int(input_lattice_mode) + input_sentence_mode == 0)
+      input_sentence_mode = true;
+    if (int(input_lattice_mode) + input_sentence_mode > 1)
+      throw std::runtime_error("either lattice or sentence input");
     
     threads = utils::bithack::max(1, threads);
 
@@ -325,16 +260,12 @@ int main(int argc, char** argv)
     grammar_type grammar(grammar_files.begin(), grammar_files.end());
     if (debug)
       std::cerr << "grammar: " << grammar.size() << std::endl;
-    
-    tree_grammar_type tree_grammar(tree_grammar_files.begin(), tree_grammar_files.end());
-    if (debug)
-      std::cerr << "tree grammar: " << tree_grammar.size() << std::endl;
 
     typedef Task task_type;
     typedef std::vector<task_type, std::allocator<task_type> > task_set_type;
     
     task_type::queue_type queue(threads);
-    task_set_type tasks(threads, task_type(queue, tree_grammar, grammar));
+    task_set_type tasks(threads, task_type(queue, grammar));
     
     boost::thread_group workers;
     for (int i = 0; i != threads; ++ i)
@@ -352,17 +283,9 @@ int main(int argc, char** argv)
     
     workers.join_all();
 
-    tree_rule_pair_unique_type tree_rules_unique;
-    rule_pair_unique_type      rules_unique;
+    rule_pair_unique_type rules_unique;
     
     for (int i = 0; i != threads; ++ i) {
-      if (tree_rules_unique.empty())
-	tree_rules_unique.swap(tasks[i].tree_rules_unique);
-      else
-	tree_rules_unique.insert(tasks[i].tree_rules_unique.begin(), tasks[i].tree_rules_unique.end());
-      
-      tasks[i].tree_rules_unique.clear();
-      
       if (rules_unique.empty())
 	rules_unique.swap(tasks[i].rules_unique);
       else
@@ -372,36 +295,16 @@ int main(int argc, char** argv)
     }
     tasks.clear();
     
-
     typedef std::ostream_iterator<char> oiter_type;
 
     features_generator<oiter_type>   generate_features;
     attributes_generator<oiter_type> generate_attributes;
     
-    if (! output_tree_file.empty()) {
+    if (! output_file.empty()) {
       namespace karma = boost::spirit::karma;
       namespace standard = boost::spirit::standard;
       
-      utils::compress_ostream os(output_tree_file, 1024 * 1024);
-      
-      tree_rule_pair_unique_type::const_iterator iter_end = tree_rules_unique.end();
-      for (tree_rule_pair_unique_type::const_iterator iter = tree_rules_unique.begin(); iter != iter_end; ++ iter) {
-	karma::generate(oiter_type(os), standard::string << " ||| " << standard::string, iter->source, iter->target);
-	
-	if (! iter->features.empty())
-	  karma::generate(oiter_type(os), generate_features, iter->features);
-	
-	if (! iter->attributes.empty())
-	  karma::generate(oiter_type(os), generate_attributes, iter->attributes);
-	os << '\n';
-      }
-    }
-    
-    if (! output_rule_file.empty()) {
-      namespace karma = boost::spirit::karma;
-      namespace standard = boost::spirit::standard;
-      
-      utils::compress_ostream os(output_rule_file, 1024 * 1024);
+      utils::compress_ostream os(output_file, 1024 * 1024);
       
       rule_pair_unique_type::const_iterator iter_end = rules_unique.end();
       for (rule_pair_unique_type::const_iterator iter = rules_unique.begin(); iter != iter_end; ++ iter) {
@@ -431,15 +334,18 @@ void options(int argc, char** argv)
   
   po::options_description opts_config("configuration options");
   opts_config.add_options()
-    ("input",       po::value<path_type>(&input_file)->default_value(input_file),   "input file")
-    ("output-tree", po::value<path_type>(&output_tree_file)->default_value(output_tree_file), "output tree-rule file")
-    ("output-rule", po::value<path_type>(&output_rule_file)->default_value(output_rule_file), "output rule file")
+    ("input",  po::value<path_type>(&input_file)->default_value(input_file),   "input file")
+    ("output", po::value<path_type>(&output_file)->default_value(output_file), "output file")
+    
+    ("input-sentence",   po::bool_switch(&input_sentence_mode),   "sentence input")
+    ("input-lattice",    po::bool_switch(&input_lattice_mode),    "lattice input")
+    
+    ("treebank", po::bool_switch(&treebank_mode), "assume treebank style grammar")
+    ("pos",      po::bool_switch(&pos_mode),      "POS annotated input")
     
     // grammar
     ("grammar",           po::value<grammar_file_set_type >(&grammar_files)->composing(),      "grammar specification(s)")
     ("grammar-list",      po::bool_switch(&grammar_list),                                      "list of available grammar specifications")
-    ("tree-grammar",      po::value<grammar_file_set_type >(&tree_grammar_files)->composing(), "tree grammar specification(s)")
-    ("tree-grammar-list", po::bool_switch(&tree_grammar_list),                                 "list of available grammar specifications")
     ;
   
   po::options_description opts_command("command line options");
