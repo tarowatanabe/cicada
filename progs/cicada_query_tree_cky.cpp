dcee2d535082a544e60517a762dfa5dd2bd8c9e6
Taro Watanabe <taro.watanabe@nict.go.jp>
2011-12-03 23:02:13 +0900

    use of symbols

--- progs/cicada_query_tree_cky.cpp
+++ progs/cicada_query_tree_cky.cpp
@@ -56,6 +56,9 @@ typedef transducer_type::rule_type           rule_type;
 typedef transducer_type::rule_ptr_type       rule_ptr_type;
 typedef transducer_type::rule_pair_type      rule_pair_type;
 
+typedef rule_type::symbol_type     symbol_type;
+typedef rule_type::symbol_set_type symbol_set_type;
+
 typedef tree_transducer_type::rule_type      tree_rule_type;
 typedef tree_transducer_type::rule_ptr_type  tree_rule_ptr_type;
 typedef tree_transducer_type::rule_pair_type tree_rule_pair_type;
@@ -64,9 +67,9 @@ struct rule_pair_string_type
 {
   typedef cicada::FeatureVectorCompact feature_set_type;
   
-  std::string lhs;
-  std::string source;
-  std::string target;
+  symbol_type lhs;
+  symbol_set_type source;
+  symbol_set_type target;
   
   feature_set_type   features;
   attribute_set_type attributes;
@@ -75,7 +78,7 @@ struct rule_pair_string_type
 
   void clear()
   {
-    lhs.clear();
+    lhs = symbol_type();
     source.clear();
     target.clear();
     features.clear();
@@ -291,16 +294,10 @@ struct Task
 	
 	  rule_string.lhs = (riter->source ? riter->source->lhs : riter->target->lhs);
 	
-	  os_source.push(boost::iostreams::back_inserter(rule_string.source));
-	  os_target.push(boost::iostreams::back_inserter(rule_string.target));
-	
 	  if (riter->source)
-	    os_source << riter->source->rhs;
+	    rule_string.source = riter->source->rhs;
 	  if (riter->target)
-	    os_target << riter->target->rhs;
-	  
-	  os_source.pop();
-	  os_target.pop();
+	    rule_string.target = riter->target->rhs;
 	  
 	  rule_string.features = riter->features;
 	  rule_string.attributes.swap(riter->attributes);
@@ -430,7 +427,7 @@ int main(int argc, char** argv)
       rule_pair_unique_type::const_iterator iter_end = rules_unique.end();
       for (rule_pair_unique_type::const_iterator iter = rules_unique.begin(); iter != iter_end; ++ iter) {
 	karma::generate(oiter_type(os),
-			standard::string << " ||| " << standard::string << " ||| " << standard::string,
+			standard::string << " ||| " << -(standard::string % ' ') << " ||| " << -(standard::string % ' '),
 			iter->lhs, iter->source, iter->target);
 	
 	if (! iter->features.empty()) {
