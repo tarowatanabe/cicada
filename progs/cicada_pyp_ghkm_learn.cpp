3301b8bb628eef092786d8f34d5b8682e20b553b
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-03-23 10:35:14 +0900

    added only comment!

---
+++ progs/cicada_pyp_ghkm_learn.cpp
@@ -0,0 +1,19 @@
+//
+//  Copyright(C) 2012 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+// PYP-GHKM!
+
+// @InProceedings{cohn-blunsom:2009:EMNLP,
+//   author    = {Cohn, Trevor  and  Blunsom, Phil},
+//   title     = {A {Bayesian} Model of Syntax-Directed Tree to String Grammar Induction},
+//   booktitle = {Proceedings of the 2009 Conference on Empirical Methods in Natural Language Processing},
+//   month     = {August},
+//   year      = {2009},
+//   address   = {Singapore},
+//   publisher = {Association for Computational Linguistics},
+//   pages     = {352--361},
+//   url       = {http://www.aclweb.org/anthology/D/D09/D09-1037}
+// }
+//
+
