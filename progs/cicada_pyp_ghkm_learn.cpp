a8907d4e0c6c01106cbc0d1ee4cae7947043572b
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-03-26 14:52:22 +0900

    started ghkm learner

--- progs/cicada_pyp_ghkm_learn.cpp
+++ progs/cicada_pyp_ghkm_learn.cpp
@@ -17,3 +17,47 @@
 // }
 //
 
+#include <map>
+#include <iterator>
+#include <numeric>
+
+#include <cicada/sentence.hpp>
+#include <cicada/symbol.hpp>
+#include <cicada/vocab.hpp>
+#include <cicada/semiring/logprob.hpp>
+#include <cicada/hypergraph.hpp>
+#include <cicada/tree_rule.hpp>
+#include <cicada/tree_rule_compact.hpp>
+#include <cicada/rule.hpp>
+
+#include "utils/chunk_vector.hpp"
+#include "utils/array_power2.hpp"
+#include "utils/resource.hpp"
+#include "utils/program_options.hpp"
+#include "utils/compress_stream.hpp"
+#include "utils/mathop.hpp"
+#include "utils/bithack.hpp"
+#include "utils/lockfree_list_queue.hpp"
+#include "utils/chinese_restaurant_process.hpp"
+#include "utils/unordered_map.hpp"
+#include "utils/unordered_set.hpp"
+#include "utils/dense_hash_set.hpp"
+#include "utils/compact_trie_dense.hpp"
+#include "utils/sampler.hpp"
+#include "utils/repository.hpp"
+#include "utils/packed_device.hpp"
+#include "utils/packed_vector.hpp"
+#include "utils/succinct_vector.hpp"
+#include "utils/simple_vector.hpp"
+
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/fusion/tuple.hpp>
+
+typedef cicada::Vocab      vocab_type;
+typedef cicada::Sentence   sentence_type;
+typedef cicada::Symbol     word_type;
+typedef cicada::HyperGraph hypergraph_type;
+
