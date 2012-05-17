c0d8cbc6fb8e1a7926a9662ec06a14254ffe9e87
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-05-17 14:56:15 +0900

    do not increment/decrement

--- progs/cicada_pyp_pialign_learn.cpp
+++ progs/cicada_pyp_pialign_learn.cpp
@@ -490,8 +490,8 @@ struct PYPLexicon
     if (source.empty() && target.empty())
       throw std::runtime_error("invalid phrase pair");
 
-    increment(source, target, tables_source_target, *lexicon_source_target, parameter_source_target, sampler, temperature);
-    increment(target, source, tables_target_source, *lexicon_target_source, parameter_target_source, sampler, temperature);
+    //increment(source, target, tables_source_target, *lexicon_source_target, parameter_source_target, sampler, temperature);
+    //increment(target, source, tables_target_source, *lexicon_target_source, parameter_target_source, sampler, temperature);
   }
   
   template <typename Sampler>
@@ -533,8 +533,8 @@ struct PYPLexicon
     if (source.empty() && target.empty())
       throw std::runtime_error("invalid phrase pair");
     
-    decrement(source, target, tables_source_target, sampler);
-    decrement(target, source, tables_target_source, sampler);
+    //decrement(source, target, tables_source_target, sampler);
+    //decrement(target, source, tables_target_source, sampler);
   }
   
   template <typename Sampler>
