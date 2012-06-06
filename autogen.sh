4a80bdacf707ae6f16a79adc9b3a37d6cfd2d47b
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-06-07 01:12:09 +0900

    use of autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-system=`uname -s`
-
-if test $system = Darwin; then
-  aclocal -I config && autoconf && autoheader && glibtoolize -f && automake -a -f
-else
-  aclocal -I config && autoconf && autoheader && libtoolize -f && automake -a -f
-fi
+exec autoreconf -I config -vifs
