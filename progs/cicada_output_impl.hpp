7e7531706594372ffe7579142154a7cdc5daeca1
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-10-04 09:30:29 +0900

    more safe directory preparation

--- progs/cicada_output_impl.hpp
+++ progs/cicada_output_impl.hpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 #include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
 
 #include "utils/filesystem.hpp"
 
@@ -19,11 +20,22 @@ void prepare_directory(const boost::filesystem::path& path)
     boost::filesystem::create_directories(path);
   }
   
-  boost::filesystem::directory_iterator iter_end;
-  for (boost::filesystem::directory_iterator iter(path); iter != iter_end; ++ iter)
-    utils::filesystem::remove_all(*iter);  
+  while (! boost::filesystem::exists(path)) {
+    ::sync();
+    boost::thread::yield();
+  }
   
-  ::sync();
+  boost::filesystem::directory_iterator iter_end;
+  for (boost::filesystem::directory_iterator iter(path); iter != iter_end; ++ iter) {
+    const boost::filesystem::path subdir = *iter;
+    
+    utils::filesystem::remove_all(subdir);
+    
+    while (boost::filesystem::exists(subdir)) {
+      ::sync();
+      boost::thread::yield();
+    }
+  }
 }
 
 #endif
