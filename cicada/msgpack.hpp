d313436f77b72a966d8bca663c64b344ec2b54fa
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-10-17 15:13:08 +0900

    added a generic header to include all the msgpacks

---
+++ cicada/msgpack.hpp
@@ -0,0 +1,26 @@
+// -*- mode: c++ -*-
+//
+//  Copyright(C) 2012 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+#ifndef __CICADA__MSGPACK__HPP__
+#define __CICADA__MSGPACK__HPP__ 1
+
+#include <msgpack/alignment.hpp>
+#include <msgpack/attribute.hpp>
+#include <msgpack/attribute_vector.hpp>
+#include <msgpack/dependency.hpp>
+#include <msgpack/feature.hpp>
+#include <msgpack/feature_vector.hpp>
+#include <msgpack/hypergrpah.hpp>
+#include <msgpack/lattice.hpp>
+#include <msgpack/rule.hpp>
+#include <msgpack/sentence.hpp>
+#include <msgpack/sentence_vector.hpp>
+#include <msgpack/span_vector.hpp>
+#include <msgpack/symbol.hpp>
+#include <msgpack/symbol_vector.hpp>
+#include <msgpack/tree_rule.hpp>
+#include <msgpack/weight_vector.hpp>
+
+#endif
