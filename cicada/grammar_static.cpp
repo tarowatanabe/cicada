74661a562be03b745bccec60aff2dbe94da17232
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-10-26 12:48:42 +0900

    halve the caches ize

--- cicada/grammar_static.cpp
+++ cicada/grammar_static.cpp
@@ -265,7 +265,7 @@ namespace cicada
     typedef std::vector<score_set_type, std::allocator<score_set_type> > score_db_type;
     
     // caching...
-    typedef utils::arc_list<size_type, rule_pair_set_type, 8,
+    typedef utils::arc_list<size_type, rule_pair_set_type, 4,
 			    std::equal_to<size_type>,
 			    std::allocator<std::pair<size_type, rule_pair_set_type> > > cache_rule_set_type;
 
