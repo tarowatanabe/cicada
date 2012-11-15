655ec4792c65870de004d00d4d1621b095d812e7
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-11-15 17:25:02 +0900

    use of larger caching

--- progs/cicada_lexicon_impl.hpp
+++ progs/cicada_lexicon_impl.hpp
@@ -563,7 +563,7 @@ struct atable_type
   
   typedef utils::simple_vector<cache_type, std::allocator<cache_type> > cache_set_type;
   
-  typedef utils::array_power2<cache_set_type, 64, std::allocator<cache_set_type> > cache_static_type;
+  typedef utils::array_power2<cache_set_type, 128, std::allocator<cache_set_type> > cache_static_type;
   typedef std::deque<cache_set_type, std::allocator<cache_set_type> > cache_mutable_type;
   
   atable_type(const double __prior=0.1, const double __smooth=1e-20)
