df8ffafb7c91057295e123d4e75391d9a32b39ae
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-12-23 17:01:23 +0900

    update makefile.am !

--- eigen/makefile.sh
+++ eigen/makefile.sh
@@ -5,6 +5,10 @@ echo
 echo "nobase_eigen_include_HEADERS = \\"
 
 for file in `find Eigen -type f`; do
+  basename=`basename $file`
+  if test $basename = "CMakeLists.txt"; then
+    continue
+  fi
   echo "$file \\"
 done
 echo
