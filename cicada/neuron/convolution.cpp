3b9a52803a53d00684c8bd2dc047c2b764d99b92
Taro Watanabe <taro.watanabe@nict.go.jp>
2012-12-24 16:39:58 +0900

    correctly implemented convlution, but actually, it is sub-sampling

--- cicada/neuron/convolution.cpp
+++ cicada/neuron/convolution.cpp
@@ -16,18 +16,18 @@ namespace cicada
       data_output.resize(frame_output, n_frame_output);
       
       for (size_type k = 0; k != n_frame_output; ++ k)
-	data_output.col(k) = data_input.block(, ).colwise().sum() * weight + bias;
+	data_output.col(k) = data_input.block(0, k * dW, data_input.rows(), kW).rowwise().sum().array() * weight.array() + bias.array();
     }
     
     void Convolution::backward(const tensor_type& data_input, const tensor_type& gradient_output)
     {
       gradient_input.resizeLike(data_input);
+      gradient_input.setZero();
       
       const size_type n_frame_output = gradient_output.cols();
       
-      for (size_type k = 0; k != n_frame_output; ++ k) {
-	
-      }
+      for (size_type k = 0; k != n_frame_output; ++ k)
+	gradient_input.block(0, k * dW, data_input.rows(), kW) += (gradient_output.col(k).transpose() * weight).replicate(1, kW);
     }
   }
 }
