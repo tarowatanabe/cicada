c7e17c2363928ddb1a8dfdb3e9ec41db4c996513
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-01-04 14:41:48 +0900

    use of 32 bit version

--- utils/hashmurmur_main.cpp
+++ utils/hashmurmur_main.cpp
@@ -7,9 +7,8 @@ int main(int argc, char** argv)
 {
   std::string line;
 
-  utils::hashmurmur<uint64_t> hasher;
+  utils::hashmurmur<uint32_t> hasher;
   
   while (std::getline(std::cin, line))
     std::cout << hasher(line.begin(), line.end(), 0) << std::endl;
-    
 }
