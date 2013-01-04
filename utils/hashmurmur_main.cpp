b2e76f3b3480b2afb0444441a178340cdacbbc4c
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-01-04 14:43:25 +0900

    added 64-bit and 32-bit version

--- utils/hashmurmur_main.cpp
+++ utils/hashmurmur_main.cpp
@@ -7,8 +7,12 @@ int main(int argc, char** argv)
 {
   std::string line;
 
-  utils::hashmurmur<uint32_t> hasher;
+  utils::hashmurmur<uint64_t> hasher64;
+  utils::hashmurmur<uint32_t> hasher32;
   
   while (std::getline(std::cin, line))
-    std::cout << hasher(line.begin(), line.end(), 0) << std::endl;
+    std::cout << hasher64(line.begin(), line.end(), 0)
+	      << ' '
+	      << hasher32(line.begin(), line.end(), 0)
+	      << std::endl;
 }
