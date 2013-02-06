0f0a2ad7ae5c671224425c17127e32431f2f13a6
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-02-06 13:06:33 +0900

    added uspatent parser

---
+++ utils/parse-uspatent.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+entities = {}
+
+pattern = re.compile(r"^\s*\"(.+?)\",\s+\"(.+?)\"")
+
+
+for line in sys.stdin:
+    
+    result = pattern.search(line)
+    if not result: continue
+    
+    entity = result.group(1)
+    mapped = result.group(2)
+
+    if mapped[0] != '.' or mapped[-1] != '.': continue
+    if entity[0] != '&' or entity[-1] != ';': continue
+
+    mapped = mapped.replace('\\', '')
+    
+    entities[entity] = mapped
+
+names = entities.keys()
+
+def compare(x, y):
+    if len(x) != len(y):
+        return cmp(len(y), len(x))
+    else:
+        return cmp(x, y)
+
+names.sort(compare)
+
+for name in names:
+    sys.stdout.write("\"'%s' <> '%s'\",\n" %(name, entities[name]))
+    
