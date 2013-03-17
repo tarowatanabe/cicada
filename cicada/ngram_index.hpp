40ea2e01846ba3ec98e392c1c79fbe4abbb6da1b
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-03-17 16:44:08 +0900

    simpler decrement

--- cicada/ngram_index.hpp
+++ cicada/ngram_index.hpp
@@ -414,10 +414,7 @@ namespace cicada
       
       if (first == last || first + 1 == last) return std::make_pair(first, last);
       
-      // decrement first...
-      -- last;
-      
-      return std::make_pair(first, next(state_type(), first, last).second + 1);
+      return std::make_pair(first, next(state_type(), first, last - 1).second + 1);
     }
 
     template <typename Iterator>
