0220cc507223270fe36a13daaa5bee4a632723a1
Taro Watanabe <taro.watanabe@nict.go.jp>
2013-06-30 13:32:06 +0900

    ported from expgram

---
+++ cicada/ngram_scorer.hpp
@@ -0,0 +1,265 @@
+// -*- mode: c++ -*-
+//
+//  Copyright(C) 2013 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+// ngram scorer inspired by kenlm's left.hh
+
+#ifndef __EXPGRAM__NGRAM_SCORER__HPP__
+#define __EXPGRAM__NGRAM_SCORER__HPP__ 1
+
+#include <vector>
+
+#include <cicada/symbol.hpp>
+#include <cicada/vocab.hpp>
+
+#include <cicada/ngram.hpp>
+#include <cicada/ngram_state.hpp>
+#include <cicada/ngram_state_chart.hpp>
+
+namespace cicada
+{
+
+  struct NGramScorer
+  {
+    typedef size_t             size_type;
+    typedef ptrdiff_t          difference_type;
+    
+    typedef Symbol             symbol_type;
+    typedef Symbol             word_type;
+    typedef Vocab              vocab_type;
+    typedef float              logprob_type;
+
+    typedef NGram           ngram_type;
+    typedef NGramStateChart ngram_state_type;
+
+    typedef std::vector<char, std::allocator<char> > buffer_type;
+
+    NGramScore(const ngram_type& ngram)
+      : ngram_state_(ngram.index.order()), ngram_(ngram), state_(0), prob_(0.0), complete_(false)
+    {
+      buffer1_.reserve(ngram_state_.suffix_.buffer_size());
+      buffer2_.reserve(ngram_state_.suffix_.buffer_size());
+      
+      buffer1_.resize(ngram_state_.suffix_.buffer_size());
+      buffer2_.resize(ngram_state_.suffix_.buffer_size());      
+    }
+    
+    NGramScore(const ngram_type& ngram, void* state)
+      : ngram_state_(ngram.index.order()), ngram_(ngram), state_(state), prob_(0.0), complete_(false)
+    {
+      ngram_state_.length_prefix(state_) = 0;
+      ngram_state_.length_suffix(state_) = 0;
+      ngram_state_.complete(state_) = false;
+      
+      buffer1_.reserve(ngram_state_.suffix_.buffer_size());
+      buffer2_.reserve(ngram_state_.suffix_.buffer_size());
+      
+      buffer1_.resize(ngram_state_.suffix_.buffer_size());
+      buffer2_.resize(ngram_state_.suffix_.buffer_size());
+    }
+    
+    void assign(void* state)
+    {
+      state_    = state;
+      prob_     = 0.0;
+      complete_ = false;
+      
+      ngram_state_.length_prefix(state_) = 0;
+      ngram_state_.length_suffix(state_) = 0;
+      ngram_state_.complete(state_) = false;
+    }
+    
+    void initial_bos()
+    {
+      const word_type::id_type bos_id = ngram_.index.vocab()[vocab_type::BOS];
+      
+      ngram_.lookup_context(&bos_id, (&bos_id) + 1, ngram_state_.suffix(state_));
+      complete_ = true;
+    }
+    
+    void initial_bos(const void* buffer)
+    {
+      // copy suffix state...
+      ngram_state_.suffix_.copy(buffer, ngram_state_.suffix(state_));
+      complete_ = true;
+    }
+    
+    template <typename Word_>
+    void terminal(const Word_& word)
+    {
+      void* state_prev = &(*buffer1.begin());
+      
+      ngram_state_.suffix_.copy(nram_state_.suffix(state_), state_prev);
+      
+      const ngram_type::result_type result = ngram_.ngram_score(state_prev, word, nram_state_.suffix(state_));
+      
+      if (complete_ || result.complete) {
+	prob_ += result.prob;
+	
+	complete_ = true;
+      } else {
+	prob_ += result.bound;
+	
+	ngram_state_.state(state_)[ngram_state_.length_prefix(state_)] = result.state;
+	++ ngram_state_.length_prefix(state_);
+	
+	// if not incremental, this is a complete state!
+	if (ngram_state_.length_suffix(state_) != ngram_state_.suffix_.length(state_prev) + 1)
+	  complete_ = true;
+      }
+    }
+
+    // special handling for left-most non-terminal
+    void initial_non_terminal(const void* antecedent)
+    {
+      ngram_state_.copy(antecedent, state_);
+      
+      complete_ = ngram_state_.complete(antecedent);
+    }
+    
+    void non_terminal(const void* antecedent)
+    {
+      // antecedent has no prefxi for scoring...
+      if (ngram_state_.length_prefix(antecedent) == 0) {
+	
+	// if this antecedent is complete, we will copy suffix from antecedent to state_.
+	// then, all the backoffs in the current suffix are accumulated.
+	if (ngram_state_.complete(antecedent)) {
+	  // score all the backoff
+	  const logprob_type* biter     = ngram_state_.backoff(state_);
+	  const logprob_type* biter_end = biter + ngram_state_.length_suffix(state_);
+	  for (/**/; biter < biter_end; ++ biter)
+	    prob_ += *biter;
+	  
+	  // copy suffix state
+	  ngram_state_.copy_suffix(antecedent, state_);
+	  
+	  // this is a complete scoring
+	  complete_ = true;
+	}
+	
+	return;
+      }
+      
+      // we have no context as a suffix
+      if (ngram_state_.length_suffix(state_) == 0) {
+	// copy suffix state from antecedent
+	ngram_state_.copy_suffix(antecedent, state_);
+	
+	if (complete_) {
+	  // since we are a complete state, all the bound scoring in the antecedent's prefix should
+	  // be upgraded to probability scoring, and complete it.
+	  
+	  prob_ += ngram_.ngram_score_update(ngram_state_.state(antecedent),
+					     ngram_state_.state(antecedent) + ngram_state_.length_prefix(antecedent),
+					     1);
+	} else if (ngram_state_.length_prefix(state_) == 0) {
+	  // if prefix is empty, we aill also copy from antecedent
+	  ngram_state_.copy_prefix(antecedent, state_);
+	  
+	  complete_ = ngram_state_.complete(antecedent);
+	} else
+	  complete_ = true;
+	
+	return;
+      }
+      
+      // temporary storage...
+      void* suffix_curr = &(*buffer1_.begin());
+      void* suffix_next = &(*buffer2_.begin());
+      
+      const ngram_type::state_type* states = ngram_state_.state(antecedent);
+      const size_type               states_length = ngram_state_.length_prefix(antecedent);
+      
+      for (size_type order = 1; order <= states_length; ++ order) {
+	const ngram_type::result_type result = ngram_.ngram_partial_score(order == 1 ? ngram_state_.suffix(state_) : suffix_curr,
+									  states[order - 1],
+									  order,
+									  suffix_next);
+	
+	if (complete_ || result.complete) {
+	  prob_ += result.prob;
+	  
+	  complete_ = true;
+	} else {
+	  prob_ += result.bound;
+	  
+	  ngram_state.state(state_)[ngram_state_.length_prefix(state_)] = result.state;
+	  ++ ngram_state_.length_prefix(state_);
+	}
+	
+	// do swap!
+	std::swap(suffix_curr, suffix_next);
+	
+	// the original suffix length and the current suffix length is different... an indication of completion
+	if (ngram_state_.suffix_.length(suffix_curr) != ngram_state_.length_suffix(state_)) {
+	  complete_ = true;
+	  
+	  // we have finished all the scoring... and we do not have to score for the rest of the states
+	  if (ngram_state_.suffix_.length(suffix_curr) == 0) {
+	    ngram_state_.copy_suffix(antecedent, state_);
+	    
+	    // adjust rest-cost from antecedent.states + order, antecedent.states + length!
+
+	    prob_ += ngram_.ngram_score_update(ngram_state_.state(antecedent) + order,
+					       ngram_state_.state(antecedent) + ngram_state_.length_prefix(antecedent),
+					       order + 1);
+	    
+	    return;
+	  }
+	}
+      }
+      
+      // antecedent is a complete state
+      if (ngram_state_.complete(antecedent)) {
+	// score all the backoff..
+	const logprob_type* biter     = ngram_state_.backoff(suffix_curr);
+	const logprob_type* biter_end = biter + ngram_state_.length_suffix(suffix_curr);
+	for (/**/; biter < biter_end; ++ biter)
+	  prob_ += *biter;
+	
+	// copy suffix state
+	ngram_state_.copy_suffix(antecedent, state_);
+	
+	// this is a complete scoring
+	complete_ = true;
+	
+	return;
+      }
+      
+      // minimum suffix is already computed in the antecedent, and this is already minimum wrt prefix which
+      // is already combined with the previous suffix...
+      if (ngram_state_.length_suffix(antecedent) < ngram_state_.length_prefix(antecedent)) {
+	ngram_state_.copy_suffix(antecedent, state_);
+	return;
+      }
+      
+      // append states...
+      ngram_state_.suffix_.append(ngram_state_.suffix(antecedent), suffix_curr, ngram_state_.suffix(state_));
+    }
+    
+    double complete()
+    {
+      // if the prefix is already reached the ngram order - 1, then, it is also complete
+      ngram_state_.complete(state_) = complete_ || (ngram_state_.length_prefix(state_) == ngram_.index.order() - 1);
+      
+      // fill the state
+      ngram_state_.fill(state_);
+      
+      return prob_;
+    }
+    
+    ngram_state_type  ngram_state_;
+    
+    const ngram_type& ngram_;
+    void*             state_;
+    double            prob_;
+    bool              complete_;
+    
+    buffer_type buffer1_;
+    buffer_type buffer2_;
+  };
+};
+
+#endif
---
+++ cicada/ngram_state_chart.hpp
@@ -0,0 +1,148 @@
+// -*- mode: c++ -*-
+//
+//  Copyright(C) 2009-2013 Taro Watanabe <taro.watanabe@nict.go.jp>
+//
+
+#ifndef __CICADA__NGRAM_STATE_CHART__HPP__
+#define __CICADA__NGRAM_STATE_CHART__HPP__ 1
+
+//
+// ngram state manager for chart
+//
+
+#include <algorithm>
+
+#include <cicada/ngram_index.hpp>
+#include <cicada/ngram_state.hpp>
+
+namespace cicada
+{
+  struct NGramStateChart
+  {
+    typedef size_t             size_type;
+    typedef ptrdiff_t          difference_type;
+    
+    typedef Symbol             symbol_type;
+    typedef Symbol             word_type;
+    typedef float              logprob_type;
+
+    typedef NGramIndex::state_type state_type;
+    typedef NGramState suffix_state_type;
+    
+    NGramStateChart(const size_type order) : suffix_(order) {}
+    
+    size_type buffer_size() const
+    {
+      return offset_suffix() + suffix_.buffer_size();
+    }
+
+    size_type offset_prefix() const
+    {
+      return 0;
+    }
+
+    size_type offset_suffix() const
+    {
+      return sizeof(size_type) * 2 + sizeof(state_type) * (suffix_.order_ - 1);
+    }
+
+    void* suffix(void* buffer) const
+    {
+      return (char*) buffer + offset_suffix();
+    }
+
+    const void* suffix(const void* buffer) const
+    {
+      return (const char*) buffer + offset_suffix();
+    }
+    
+    size_type& length_suffix(void* buffer) const
+    {
+      return suffix_.length(suffix(buffer));
+    }
+    
+    const size_type& length_suffix(const void* buffer) const
+    {
+      return suffix_.length(suffix(buffer));
+    }
+
+    size_type& length_prefix(void* buffer) const
+    {
+      return *reinterpret_cast<size_type*>(buffer);
+    }
+    
+    const size_type& length_prefix(const void* buffer) const
+    {
+      return *reinterpret_cast<const size_type*>(buffer);
+    }
+    
+    size_type& complete(void* buffer) const
+    {
+      return *reinterpret_cast<size_type*>((char*) buffer + sizeof(size_type));
+    }
+
+    const size_type& complete(const void* buffer) const
+    {
+      return *reinterpret_cast<const size_type*>((const char*) buffer + sizeof(size_type));
+    }
+
+    state_type* state(void* buffer) const
+    {
+      return reinterpret_cast<state_type*>((char*) buffer + sizeof(size_type) * 2);
+    }
+
+    const state_type* state(const void* buffer) const
+    {
+      return reinterpret_cast<const state_type*>((const char*) buffer + sizeof(size_type) * 2);
+    }
+    
+    word_type::id_type* context(void* buffer) const
+    {
+      return suffix_.context(suffix(buffer));
+    }
+    
+    const word_type::id_type* context(const void* buffer) const
+    {
+      return suffix_.context(suffix(buffer));
+    }
+
+    logprob_type* backoff(void* buffer) const
+    {
+      return suffix_.backoff(suffix(buffer));
+    }
+
+    const logprob_type* backoff(const void* buffer) const
+    {
+      return suffix_.backoff(suffix(buffer));
+    }
+    
+    void fill(void* buffer) const
+    {
+      const size_type len = length_prefix(buffer);
+      
+      std::fill(reinterpret_cast<char*>(state(buffer) + len), reinterpret_cast<char*>(state(buffer) + suffix_.order_ - 1), 0);
+      
+      suffix_.fill(suffix(buffer));
+    }
+
+    void copy(const void* buffer, void* copied)
+    {
+      std::copy((char*) buffer, ((char*) buffer) + buffer_size(), (char*) copied);
+    }
+
+    void copy_prefix(const void* buffer, void* copied)
+    {
+      std::copy((char*) buffer, ((char*) buffer) + offset_suffix(), (char*) copied);
+    }
+
+    void copy_suffix(const void* buffer, void* copied)
+    {
+      suffix_.copy(suffix(buffer), suffix(copied));
+    }
+    
+    suffix_state_type suffix_;
+  };
+
+};
+
+#endif
