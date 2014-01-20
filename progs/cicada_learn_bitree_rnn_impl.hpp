3f395f9c552e536647c9206545dec195430962ec
Taro Watanabe <taro.watanabe@nict.go.jp>
2014-01-20 15:36:54 +0900

    when selecting longer hypotheses, also preserve sufficient statistics

--- progs/cicada_learn_bitree_rnn_impl.hpp
+++ progs/cicada_learn_bitree_rnn_impl.hpp
@@ -1038,14 +1038,6 @@ struct Oracle
   
   typedef std::vector<const candidate_type*, std::allocator<const candidate_type*> > oracle_set_type;
   typedef std::vector<oracle_set_type, std::allocator<oracle_set_type> > oracle_map_type;
-
-  struct greater_sentence_length
-  {
-    bool operator()(const candidate_type* x, const candidate_type* y) const
-    {
-      return x->hypothesis_.sentence.size() > y->hypothesis_.sentence.size();
-    }
-  };
   
   template <typename Generator>
   std::pair<score_ptr_type, score_ptr_type>
@@ -1148,12 +1140,15 @@ struct Oracle
 	    
 	    objective_next = objective_sample;
 	    score_next     = score_sample;
-	  } else if (objective_sample == objective_next)
-	    oracles_next[id].push_back(&(*hiter));
+	  } else if (objective_sample == objective_next) {
+	    if (hiter->hypothesis_.sentence.size() > oracles_next[id].front()->hypothesis_.sentence.size()) {
+	      oracles_next[id].insert(oracles_next[id].begin(), &(*hiter));
+	      
+	      score_next = score_sample;
+	    } else
+	      oracles_next[id].push_back(&(*hiter));
+	  }
 	}
-
-	// heuristic: sort so that longer translations are selected!
-	std::sort(oracles_next[id].begin(), oracles_next[id].end(), greater_sentence_length());
       }
       
       if (objective_next > objective_best) {
